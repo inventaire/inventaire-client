inventaire/inventaire-client

commit 96d2303d44004925bbeb98cebfd203fa66f9d215

Backbone.Model::reqGrab: do not re-request an already grabed model

app/lib/global_libs_extender.coffee
@@ -37,6 +37,8 @@ module.exports = (_)->
     return attributes.map @get.bind(@)
 
   Backbone.Model::reqGrab = (request, id, name)->
+    if @[name]? then return Promise.resolve @[name]
+
     app.request request, id
     .then @grab.bind(@, name)
     .catch _.ErrorRethrow("reqGrab #{request} #{id} #{name}")

commit d95dd57d07d68da91a8b2740373e4fe55d8724d9

reqGrab: take a refresh flag
96d2303d broke entity_value_editor valueEntity refreshing by changing
reqGreb behavior to not re-request a model already grabed

app/lib/global_libs_extender.coffee
@@ -36,8 +36,8 @@ module.exports = (_)->
       throw new Error 'gets expects attributes as different arguments'
     return attributes.map @get.bind(@)
 
-  Backbone.Model::reqGrab = (request, id, name)->
-    if @[name]? then return Promise.resolve @[name]
+  Backbone.Model::reqGrab = (request, id, name, refresh)->
+    if not refresh and @[name]? then return Promise.resolve @[name]
 
     app.request request, id
     .then @grab.bind(@, name)

app/modules/entities/models/property_value.coffee
@@ -18,7 +18,7 @@ module.exports = Backbone.Model.extend
     if value? and editorType in haveValueEntity
 
       if regex_.EntityUri.test value
-        @reqGrab 'get:entity:model', value, 'valueEntity'
+        @reqGrab 'get:entity:model', value, 'valueEntity', true
       else if _.isObject(value) and value.claims?
         # Allow to pass an entity draft as an object of the form:
         # { labels: {}, claims: {} }