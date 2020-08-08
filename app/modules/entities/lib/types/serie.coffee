inventaire/inventaire-client

commit 85d7c1a4b14344c7b00d6ea68e6ec4c8131347d2

serie: fetchPartsData: check graph change tokens

app/modules/entities/lib/types/serie.coffee
@@ -22,7 +22,7 @@ specificMethods = _.extend {}, commonsSerieWork,
     refresh = @getRefresh refresh
     if not refresh and @waitForParts? then return @waitForParts
 
-    @fetchPartsData { refresh }
+    @fetchPartsData refresh
     .then initPartsCollections.bind(@, refresh, fetchAll)
     .then importDataFromParts.bind(@)
 

commit 0650a2ee20f01dcb58022dd226508d7985dbd6d5

serie model: fix fetchPartsData call from initSerieParts
following a bug introduced by 85d7c1a4 that was probably due to a rushed rebase

app/modules/entities/lib/types/serie.coffee
@@ -10,10 +10,10 @@ module.exports = ->
 specificMethods = _.extend {}, commonsSerieWork,
   fetchPartsData: (options = {})->
     { refresh } = options
+    refresh = @getRefresh refresh
     if not refresh and @waitForPartsData? then return @waitForPartsData
 
     uri = @get 'uri'
-
     @waitForPartsData = _.preq.get app.API.entities.serieParts(uri, refresh)
       .then (res)=> @partsData = res.parts
 
@@ -22,7 +22,7 @@ specificMethods = _.extend {}, commonsSerieWork,
     refresh = @getRefresh refresh
     if not refresh and @waitForParts? then return @waitForParts
 
-    @fetchPartsData refresh
+    @fetchPartsData { refresh }
     .then initPartsCollections.bind(@, refresh, fetchAll)
     .then importDataFromParts.bind(@)
 