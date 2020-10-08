87970f27664299a5f04cdd56a939694fa14472c0
maxlath <k@maxlath.eu>
2020-10-08 18:33:38 +0200

    remove Internet Explorer polyfills

--- app/initialize.js
+++ app/initialize.js
@@ -4,15 +4,14 @@ import 'backbone.marionette'
 import 'lib/handlebars_helpers/init'
 import 'lib/global_libs_extender'
 import 'modules/general/lib/querystring_actions'
-import initApp from './init_app'
 import 'lib/env_config'
 import initPolyfills from 'lib/polyfills'
 import testVideoInput from 'lib/has_video_input'
 import initUnhandledErrorLogger from 'lib/unhandled_error_logger'
+import initApp from './init_app'
 
 // Init handler error before the app so that it can catch any error happenig there
 initUnhandledErrorLogger()
 testVideoInput()
-
 initPolyfills()
-.then(initApp)
+initApp()
--- app/lib/polyfills.js
+++ app/lib/polyfills.js
@@ -1,33 +1,5 @@
-export default async function () {
-  ISODatePolyFill()
-  DateNowPolyFill()
+export default function () {
   startsWithPolyFill()
-
-  if (!window.Promise) {
-    window.Promise = await import('promise-polyfill')
-  }
-}
-
-// from https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Date/toISOString
-const ISODatePolyFill = function () {
-  if (Date.prototype.toISOString == null) {
-    const pad = function (number) {
-      if (number < 10) return '0' + number
-      return number
-    }
-
-    // eslint-disable-next-line no-extend-native
-    Date.prototype.toISOString = function () {
-      return this.getUTCFullYear() +
-        '-' + pad(this.getUTCMonth() + 1) +
-        '-' + pad(this.getUTCDate()) +
-        'T' + pad(this.getUTCHours()) +
-        ':' + pad(this.getUTCMinutes()) +
-        ':' + pad(this.getUTCSeconds()) +
-        '.' + (this.getUTCMilliseconds() / 1000).toFixed(3).slice(2, 5) +
-        'Z'
-    }
-  }
 }
 
 // Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/startsWith#Polyfill
@@ -39,8 +11,4 @@ const startsWithPolyFill = function () {
       return this.substr(start, search.length) === search
     }
   }
-}
-
-const DateNowPolyFill = function () {
-  if (Date.now == null) Date.now = () => new Date().getTime()
 }
