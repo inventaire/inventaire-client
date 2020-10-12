cb38c1541a58314a7b5f3643ee891cc16f6e7909
maxlath <k@maxlath.eu>
2020-10-12 15:02:49 +0200

    handlebars: replace {{partial}} helper by Handlebars native partial syntax
    Liquidating the hack initiated by 143a7cc (2014-06-01 :D)
    Replacing it by a new hack: Handlebars.registerPartial is called in inventaire/parcel-plugin-handlebars-precompile#845da47
    See https://github.com/inventaire/parcel-plugin-handlebars-precompile/blob/master/handlebars-asset.js

--- app/lib/handlebars_helpers/init.js
+++ app/lib/handlebars_helpers/init.js
@@ -2,7 +2,6 @@ import Handlebars from 'handlebars/dist/handlebars.runtime'
 import blocks from './blocks'
 import misc from './misc'
 import utils from './utils'
-import partials from './partials'
 import claims from './claims'
 import userContent from './user_content'
 import * as icons from './icons'
@@ -10,11 +9,14 @@ import images from './images'
 import input from './input'
 import linkify from './linkify'
 
-const API = _.extend({ linkify }, blocks, misc, utils, partials, claims, userContent, icons, images, input)
+const API = _.extend({ linkify }, blocks, misc, utils, claims, userContent, icons, images, input)
 
 for (const name in API) {
   const fn = API[name]
   // Registering partials using the code here
   // https://github.com/brunch/handlebars-brunch/issues/10#issuecomment-38155730
   Handlebars.registerHelper(name, fn.bind(API))
+
+  // Partials are registered by https://github.com/inventaire/parcel-plugin-handlebars-precompile/blob/master/handlebars-asset.js
+  // allowing to create new partials without having to explictly register them here
 }
