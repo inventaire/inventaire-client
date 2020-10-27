a99ac3b0738c564affec9270f648805c53e0b483
maxlath <k@maxlath.eu>
2020-10-27 18:03:34 +0100

    bundle: extract_css: use content hash in filenames

--- bundle/plugins/extract_css.cjs
+++ bundle/plugins/extract_css.cjs
@@ -1,3 +1,5 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-module.exports = new MiniCssExtractPlugin()
+module.exports = new MiniCssExtractPlugin({
+  filename: '[name].[contenthash:8].css'
+})
