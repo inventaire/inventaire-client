1b99ad83a979f5d9827119b2f9fea0c40e6950d3
maxlath <k@maxlath.eu>
2020-10-30 15:55:30 +0100

    remove unused files
    detected by unused-webpack-plugin

--- app/modules/entities/lib/inv/create_inv_entity.js
+++
@@ -1,9 +0,0 @@
-import log_ from 'lib/loggers'
-import preq from 'lib/preq'
-
-export default function (params) {
-  const { labels, claims, createOnWikidata } = params
-  const prefix = createOnWikidata === true ? 'wd' : 'inv'
-  return preq.post(app.API.entities.create, { prefix, labels, claims })
-  .catch(log_.ErrorRethrow(`create ${prefix} entity err`))
-};
--- app/modules/general/regions/common_el.js
+++
@@ -1,7 +0,0 @@
-export default Marionette.Region.extend({
-  attachHtml (view) {
-    // uses the fake region el to have it's own el
-    // inserted just after the fake region
-    return $(view.el).insertAfter(this.$el)
-  }
-})
