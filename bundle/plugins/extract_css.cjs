970c1c7dc77af3e8bea5d4ae8f662920cbfa6ceb
maxlath <k@maxlath.eu>
2020-10-31 15:29:47 +0100

    bundle: extract_css: harmonize filename and chunkFilename
    it didn't seem to change anyting to the generated files though

--- bundle/plugins/extract_css.cjs
+++ bundle/plugins/extract_css.cjs
@@ -1,5 +1,6 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = new MiniCssExtractPlugin({
-  filename: '[name].[contenthash:8].css'
+  filename: '[name].[contenthash:8].css',
+  chunkFilename: '[name].[contenthash:8].css',
 })
