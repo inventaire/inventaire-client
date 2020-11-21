8e34502dd364e4f70ba7f476dbbfc518813a9d10
maxlath <k@maxlath.eu>
2020-11-21 12:33:48 +0100

    use node-config to allow customization of the webpack config
    using a to-be-released version of node-config to benefit from support of .cjs files
    
    addressing https://github.com/inventaire/inventaire-client/issues/241

--- bundle/webpack.config.dev.cjs
+++ bundle/webpack.config.dev.cjs
@@ -1,7 +1,7 @@
 const mode = 'development'
-const config = require('./webpack.config.common.cjs')(mode)
+const webpackConfig = require('./webpack.config.common.cjs')(mode)
 
-Object.assign(config, {
+Object.assign(webpackConfig, {
   mode,
   // Override 'browserlist' value (deduced from the presence of a browserslist in package.json)
   // as that disables HMR, see https://github.com/webpack/webpack-dev-server/issues/2758
@@ -10,4 +10,4 @@ Object.assign(config, {
   devServer: require('./dev_server.cjs')
 })
 
-module.exports = config
+module.exports = webpackConfig
--- config/default.js
+++
@@ -1,9 +0,0 @@
-export default {
-  // transifex doesn't use API keys or anything, just the same credentials as for an in browser login
-  transifex: {
-    username: 'customize',
-    password: 'customize'
-  },
-  host: 'http://localhost:3006',
-  prerenderUrl: 'http://localhost:3000',
-}
