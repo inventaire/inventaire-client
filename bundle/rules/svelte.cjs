9dae2b1d5ee75b0c85d686c786a87538357ebf83
maxlath <k@maxlath.eu>
2021-01-28 23:30:52 +0100

    bundle: svelte: update comment on SCSS

--- bundle/rules/svelte.cjs
+++ bundle/rules/svelte.cjs
@@ -1,7 +1,7 @@
-// About using SCSS in Svelte components: it can work by adding a preprocessor such as
-// https://github.com/ls-age/svelte-preprocess-sass
-// but then style hot reload is broken
-// See also https://daveceddia.com/svelte-with-sass-in-vscode/
+// About using SCSS in Svelte components:
+// - it can work by adding a preprocessor such as https://github.com/ls-age/svelte-preprocess-sass
+//   but then style hot reload was broken. See also https://daveceddia.com/svelte-with-sass-in-vscode/
+// - maybe we don't need SCSS: nested rules are much less needed in components
 
 module.exports = mode => ({
   test: /\.svelte$/,
