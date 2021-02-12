inventaire/inventaire-client

commit 9240a6f1ca2378bec57fb8dc60947103fe14f110

post-decaffeinate: remove some unnecessary returns

app/modules/entities/views/serie_infobox.js
@@ -18,7 +18,7 @@ export default Marionette.LayoutView.extend({
 
   initialize () {
     this.waitForAuthors = this.model.getExtendedAuthorsModels()
-    return this.model.getWikipediaExtract()
+    this.model.getWikipediaExtract()
   },
 
   modelEvents: {
@@ -35,8 +35,8 @@ export default Marionette.LayoutView.extend({
     return attrs
   },
 
-  onRender () {
-    return this.waitForAuthors
-    .then(this.ifViewIsIntact(showAllAuthorsPreviewLists))
+  async onRender () {
+    await this.waitForAuthors
+    if (this.isIntact()) showAllAuthorsPreviewLists.call(this)
   }
 })

app/modules/entities/views/work_infobox.js
@@ -38,11 +38,11 @@ export default Marionette.LayoutView.extend({
     return attrs
   },
 
-  onRender () {
+  async onRender () {
     app.execute('uriLabel:update')
 
-    return this.waitForAuthors
-    .then(this.ifViewIsIntact(showAllAuthorsPreviewLists))
+    await this.waitForAuthors
+    if (this.isIntact()) showAllAuthorsPreviewLists.call(this)
   }
 })
 

commit b3aa43d16ffee282dc507ddfbb4b5f38eb9fedaa

showAllAuthorsPreviewLists: fix missing data
broken by 9240a6f1

app/modules/entities/lib/show_all_authors_preview_lists.js
@@ -11,7 +11,7 @@ const showAuthorsPreviewList = function (property, models) {
   if (models.length === 0) return
   const collection = new Backbone.Collection(models)
   const name = extendedAuthorsKeys[property]
-  return this[name].show(new AuthorsPreviewList({ collection, name }))
+  this[name].show(new AuthorsPreviewList({ collection, name }))
 }
 
 const extendedAuthorsKeys = {

app/modules/entities/views/serie_infobox.js
@@ -36,7 +36,7 @@ export default Marionette.LayoutView.extend({
   },
 
   async onRender () {
-    await this.waitForAuthors
-    if (this.isIntact()) showAllAuthorsPreviewLists.call(this)
+    const authorsPerProperty = await this.waitForAuthors
+    if (this.isIntact()) showAllAuthorsPreviewLists.call(this, authorsPerProperty)
   }
 })

app/modules/entities/views/work_infobox.js
@@ -41,8 +41,8 @@ export default Marionette.LayoutView.extend({
   async onRender () {
     app.execute('uriLabel:update')
 
-    await this.waitForAuthors
-    if (this.isIntact()) showAllAuthorsPreviewLists.call(this)
+    const authorsPerProperty = await this.waitForAuthors
+    if (this.isIntact()) showAllAuthorsPreviewLists.call(this, authorsPerProperty)
   }
 })
 

app/modules/inventory/views/item_show_layout.js
@@ -63,10 +63,11 @@ export default Marionette.LayoutView.extend({
     app.execute('modal:open', 'large')
   },
 
-  onRender () {
+  async onRender () {
     this.showItemData()
     this.showShelves()
-    this.waitForAuthors.then(showAllAuthorsPreviewLists.bind(this))
+    const authorsPerProperty = await this.waitForAuthors
+    if (this.isIntact()) showAllAuthorsPreviewLists.call(this, authorsPerProperty)
   },
 
   showItemData () {