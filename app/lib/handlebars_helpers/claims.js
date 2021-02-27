8e944b88a66c7badcba6f50879e81f64f07ea0a2
jums <jums@inventaire.io>
2021-02-27 13:28:51 +0100

    wip

--- app/lib/handlebars_helpers/claims.js
+++ app/lib/handlebars_helpers/claims.js
@@ -1,6 +1,7 @@
 import { isEntityUri, isImageHash } from 'lib/boolean_tests'
 import typeOf from 'lib/type_of'
 import { i18n } from 'modules/user/lib/i18n'
+import { localStorageProxy } from 'lib/local_storage'
 import * as icons_ from './icons'
 import wdLang from 'wikidata-lang'
 import commons_ from 'lib/wikimedia/commons'
@@ -97,6 +98,7 @@ export default API = {
 
   identifierClaim (...args) {
     const [ claims, prop ] = neutralizeDataObject(args)
+    if (!isUserPreference(prop)) return ''
     const firstId = claims?.[prop]?.[0]
     if (firstId != null) {
       const label = labelString(prop)
@@ -156,4 +158,8 @@ const imagePreview = imageHash => {
       <p class="image-hash">${imageHash}</p>
     </a>
   `
+}
+const isUserPreference = prop => {
+  const props = localStorageProxy.getItem('customIdentifiers')
+  return prop in props
 }
