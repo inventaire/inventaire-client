eb06f6f8556179e3f4911863178963183acfc02c
jums <jums@inventaire.io>
2021-05-20 18:33:42 +0200

    add a goto fn to scroll to the setting height
    on mobile screen when clicking on a nav link
    
    but it doesnt work as expected, somehow it already need a focus
    on the link in order to scroll as expected (so you need to double click
    on it), cant find out how to solve this for now

--- app/modules/settings/components/settings_layout.svelte
+++ app/modules/settings/components/settings_layout.svelte
@@ -1,29 +1,43 @@
 <script>
   import app from 'app/app'
+  import { wait } from 'lib/promises'
+  import { onMount } from 'svelte'
   export let section = 'profile'
   $: app.navigate(`/settings/${section}`)
+  let settingEl, settingHeight
+  // onMount runs after the component is first rendered
+  // to ensure variable is bound after its creation
+  onMount(() => settingHeight = settingEl.offsetTop)
+  const isSmallScreen = window.screen.width < 470
+  const goToSetting = async () => {
+    if (isSmallScreen) {
+      await wait(5) //
+      window.scrollTo({
+        top: settingHeight,
+        behavior: 'smooth'
+      })
+    }
+  }
 </script>
 
 <div class="wrapper">
   <nav class="navigation">
-    <a href='/settings/profile' class="{section === 'profile' ? 'active' : ''}" on:click={() => { section = 'profile' }}>
+    <a href='/settings/profile' class="{section === 'profile' ? 'active' : ''}" on:click={goToSetting} on:click={() => { section = 'profile' }}>
       Profile
     </a>
-    <a href='/settings/account' class="{section === 'account' ? 'active' : ''}" on:click={() => { section = 'account' }}>
+    <a href='/settings/account' class="{section === 'account' ? 'active' : ''}" on:click={goToSetting} on:click={() => { section = 'account' }}>
       Account
     </a>
-    <a href='/settings/notifications' class="{section === 'notifications' ? 'active' : ''}" on:click={() => { section = 'notifications' }}>
+    <a href='/settings/notifications' class="{section === 'notifications' ? 'active' : ''}" on:click={goToSetting} on:click={() => { section = 'notifications' }}>
       Notifications
     </a>
-    <a href='/settings/data' class="{section === 'data' ? 'active' : ''}" on:click={() => { section = 'data' }}>
+    <a href='/settings/data' class="{section === 'data' ? 'active' : ''}" on:click={goToSetting} on:click={() => { section = 'data' }}>
       Data
     </a>
   </nav>
-  <div class="setting">
+  <div class="setting" bind:this={settingEl}>
     {#if section === 'profile'}
-      <div id='profile'>
-        Profile
-      </div>
+      Profile
     {/if}
     {#if section === 'account'}
       Account
