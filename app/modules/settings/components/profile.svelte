4b3b5aa98edfda8d7767b328bc5eddc0c24a9d02
jums <jums@inventaire.io>
2021-05-21 13:48:40 +0200

    profile: add html/css skeleton
    naming: section is the new tab
    put scss inside an external file to be reused later by other setting section

---
+++ app/modules/settings/components/profile.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { i18n, I18n } from 'modules/user/lib/i18n'
+  export let user
+  const username = user.attributes.label
+  const bio = user.attributes.bio || ''
+</script>
+<h2 class="title first-title">{I18n('public profile')}</h2>
+
+<section>
+  <h3 class="label">{I18n('username')}</h3>
+  <input placeholder="{i18n('username')}..." value={username}>
+  <p class="note">{I18n('username_tip')}</p>
+  <button id="saveUsername" class="save light-blue-button">{I18n('update name')}</button>
+</section>
+
+<section>
+  <h3 class="label">{I18n('presentation')}</h3>
+  <textarea name="bio" id="bio" class="ctrlEnterClick" aria-label="{i18n('presentation')}">{bio}</textarea>
+  <div class="bioLimit"></div>
+  <p class="note">{I18n('a few words on you?')}</p>
+  <button id="saveBio" class="save light-blue-button">{I18n('update presentation')}</button>
+</section>
+
+<style lang="scss">
+  @import 'app/modules/settings/scss/section_settings_svelte';
+</style>
