25a801653c1c4d28b567842aa278b7c2247fc865
jums <jums@inventaire.io>
2021-05-23 12:57:10 +0200

    profile: add username and bio logic
    without any error notifications for the moment

--- app/modules/settings/components/profile.svelte
+++ app/modules/settings/components/profile.svelte
@@ -1,24 +1,67 @@
 <script>
   import { i18n, I18n } from 'modules/user/lib/i18n'
+  import preq from 'lib/preq'
   export let user
-  const username = user.attributes.label
-  const bio = user.attributes.bio || ''
+  let currentUsername = user.attributes.label
+  let bio = user.attributes.bio || ''
+  let requestedUsername
+
+  const updateUsername = async () => {
+    await app.execute('ask:confirmation', {
+      confirmationText: i18n('username_change_confirmation', { requestedUsername, currentUsername }),
+      // no need to show the warning if it's just a case change
+      warningText: !doesUsernameCaseChange() ? i18n('username_change_warning') : undefined,
+      action: updateUserReq('username', requestedUsername)
+    })
+    currentUsername = requestedUsername
+  }
+
+  const doesUsernameCaseChange = () => requestedUsername.toLowerCase() === currentUsername.toLowerCase()
+
+  const updateUserReq = (attribute, value) => {
+    app.request('user:update', {
+      attribute,
+      value
+    })
+  }
+
+  const onUsernameChange = async newUsername => {
+    if (currentUsername === newUsername) {
+      // username has been modfied back to its original state
+      // nothing to update and nothing to flash notify either
+      return
+    }
+
+    await preq.get(app.API.auth.usernameAvailability(newUsername))
+    .then(() => requestedUsername = newUsername)
+  }
+
+  const onBioChange = value => {
+    bio = value
+  }
+  const updateBio = async () => {
+    await updateUserReq('bio', bio)
+  }
 </script>
+
 <h2 class="title first-title">{I18n('public profile')}</h2>
 
 <section>
   <h3 class="label">{I18n('username')}</h3>
-  <input placeholder="{i18n('username')}..." value={username}>
+  <div class="text-zone">
+    <input placeholder="{i18n('username')}..." value={currentUsername} on:keyup="{e => onUsernameChange(e.target.value)}">
+  </div>
   <p class="note">{I18n('username_tip')}</p>
-  <button id="saveUsername" class="save light-blue-button">{I18n('update name')}</button>
+  <button id="updateUsername" class="save light-blue-button" on:click="{updateUsername}">{I18n('update username')}</button>
 </section>
 
 <section>
   <h3 class="label">{I18n('presentation')}</h3>
-  <textarea name="bio" id="bio" class="ctrlEnterClick" aria-label="{i18n('presentation')}">{bio}</textarea>
-  <div class="bioLimit"></div>
-  <p class="note">{I18n('a few words on you?')}</p>
-  <button id="saveBio" class="save light-blue-button">{I18n('update presentation')}</button>
+  <div class="text-zone">
+    <textarea name="bio" id="bio" aria-label="{i18n('presentation')}" on:keyup="{e => onBioChange(e.target.value)}">{bio}</textarea>
+  </div>
+  <p class="note">{I18n('a few words on you?')} ({bio.length}/1000)</p>
+  <button id="saveBio" class="save light-blue-button" on:click="{() => updateBio(bio)}">{I18n('update presentation')}</button>
 </section>
 
 <style lang="scss">
