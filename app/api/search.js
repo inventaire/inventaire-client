dffb120fda3c9a422fcad6e1eab4918119d923e2
maxlath <k@maxlath.eu>
2021-09-13 22:54:30 +0200

    search entities on remote instance

--- app/api/search.js
+++ app/api/search.js
@@ -1,11 +1,17 @@
 import { forceArray } from 'lib/utils'
 import { buildPath } from 'lib/location'
 import endpoint from './endpoint'
+import { customizeInstance } from './instance'
+
 const { base } = endpoint('search')
 
 export default function (types, search, limit = 10, exact = false) {
   const { lang } = app.user
-  types = forceArray(types).join('|')
+  types = forceArray(types)
+  const hasSocialTypes = types.includes('users') || types.includes('groups')
+  types = types.join('|')
   search = encodeURIComponent(search)
-  return buildPath(base, { types, search, lang, limit, exact })
+  const url = buildPath(base, { types, search, lang, limit, exact })
+  if (hasSocialTypes) return url
+  else return customizeInstance(url)
 }
