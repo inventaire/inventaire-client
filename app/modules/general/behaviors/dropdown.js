cf3f19d91d99c869d5742d33b3600f5c19c590b2
maxlath <k@maxlath.eu>
2021-10-28 14:15:26 +0200

    top_bar language picker: allow to select language by pressing enter [2.1.1]

--- app/modules/general/behaviors/dropdown.js
+++ app/modules/general/behaviors/dropdown.js
@@ -13,23 +13,23 @@ export default Marionette.Behavior.extend({
     const $dropdown = $hasDropdown.find('.dropdown')
     const isVisible = $dropdown.css('display') !== 'none'
     if (isVisible) {
-      return hide($dropdown)
+      hide($dropdown)
     } else if (!isDisabled) {
       show($dropdown)
       // Let a delay so that the toggle click itself isn't catched by the listener
-      return this.view.setTimeout(closeOnClick.bind(this, $dropdown), 100)
+      this.view.setTimeout(closeOnClick.bind(this, $dropdown), 100)
     }
   }
 })
 
 const hide = function ($dropdown) {
   $dropdown.hide()
-  return $dropdown.removeClass('hover')
+  $dropdown.removeClass('hover')
 }
 
 const show = function ($dropdown) {
   $dropdown.show()
-  return $dropdown.addClass('hover')
+  $dropdown.addClass('hover')
 }
 
 const closeOnClick = function ($dropdown) {
--- app/modules/general/views/top_bar.js
+++ app/modules/general/views/top_bar.js
@@ -7,6 +7,7 @@ import screen_ from 'lib/screen'
 import { currentRoute, currentSection } from 'lib/location'
 import languages from 'lib/languages_data'
 import topBarTemplate from './templates/top_bar.hbs'
+import enterClick from '../lib/enter_click'
 
 const mostCompleteFirst = (a, b) => b.completion - a.completion
 const languagesList = _.values(languages).sort(mostCompleteFirst)
@@ -81,7 +82,8 @@ export default Marionette.LayoutView.extend({
 
     'focus #topBarButtons': 'closeSearch',
     'focus #language-picker': 'closeSearch',
-    'click #language-picker .option a': 'selectLang'
+    'click #language-picker .option a': 'selectLang',
+    'keydown a[tabindex=0]': enterClick.button,
   },
 
   childEvents: {
