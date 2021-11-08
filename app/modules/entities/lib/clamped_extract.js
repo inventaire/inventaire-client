31a60bf48a9ebc660e8e591f56c8a51e43a7e1d9
maxlath <k@maxlath.eu>
2021-11-08 17:08:11 +0100

    entities: clamped_extract: add aria-expanded and aria-controls attributes [4.1.2]

--- app/modules/entities/lib/clamped_extract.js
+++ app/modules/entities/lib/clamped_extract.js
@@ -1,8 +1,11 @@
+import { uniqueId } from 'underscore'
+
 export default {
   setAttributes (attrs) {
     if (!attrs.extract) attrs.extract = attrs.description
     if (attrs.extract != null) {
       attrs.extractOverflow = attrs.extract.length > 600
     }
+    attrs.extractId = uniqueId('extract-')
   }
 }
