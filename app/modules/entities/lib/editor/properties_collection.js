360008fdbbfc75034b49878ea5beb9c0bef59900
maxlath <k@maxlath.eu>
2021-11-09 13:59:40 +0100

    multi_entity_edit: prevent showing a claim editor for the property linking the 2 entities being created

--- app/modules/entities/lib/editor/properties_collection.js
+++ app/modules/entities/lib/editor/properties_collection.js
@@ -2,19 +2,25 @@ import PropertyValues from 'modules/entities/collections/property_values'
 import properties from '../properties'
 import propertiesPerType from './properties_per_type'
 import error_ from 'lib/error'
+import { omit } from 'underscore'
 
 export default function (entityModel) {
   const propertiesCollection = new Backbone.Collection()
 
-  const { type } = entityModel
+  const { type, relation } = entityModel
   if (type == null) {
     throw error_.new('unknown entity type', entityModel)
   }
 
-  const typeProps = propertiesPerType[type]
+  let typeProps = propertiesPerType[type]
   if (typeProps == null) {
     throw error_.new(`no properties found for entity type: ${type}`, entityModel)
   }
+
+  // When the entity being created has a relation to another entity being created,
+  // do not offer to edit that relation property
+  // (typically wdt:P629 when creating a work and an edition )
+  if (relation) typeProps = omit(typeProps, relation)
 
   for (const prop in typeProps) {
     const typeCustomPropData = typeProps[prop]
