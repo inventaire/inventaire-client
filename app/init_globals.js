0f3c1594a2e4c9778097f0b2100f461e3eb17ead
maxlath <k@maxlath.eu>
2021-11-22 13:41:09 +0100

    dependencies: update jquery, backbone, and marionette

--- app/init_globals.js
+++ app/init_globals.js
@@ -1,7 +1,8 @@
 import $ from 'jquery'
 import _ from 'underscore'
 import Backbone from 'backbone'
-import Marionette from 'backbone.marionette'
+import Marionette from 'backbone.marionette/lib/backbone.marionette.esm'
+import AppRouter from 'marionette.approuter/lib/marionette.approuter.esm'
 // Sets Backbone.NestedModel
 import 'backbone-nested'
 
@@ -9,3 +10,4 @@ window._ = _
 window.jQuery = window.$ = $
 window.Backbone = Backbone
 window.Marionette = Marionette
+Marionette.AppRouter = AppRouter
