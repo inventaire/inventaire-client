213592cee440bd6417f9e4ed075aa57720bc2433
jums <jums@inventaire.io>
2022-01-28 16:48:38 +0100

    add edition cover to canditate

--- app/modules/inventory/components/list_item.svelte
+++ app/modules/inventory/components/list_item.svelte
@@ -1,6 +1,7 @@
 <script>
   import { guessUriFromIsbn } from '#inventory/lib/import_helpers'
   import { I18n, i18n } from '#user/lib/i18n'
+  import { imgSrc } from '#lib/handlebars_helpers/images'
   import getOriginalLang from '#entities/lib/get_original_lang'
   import getBestLangValue from '#entities/lib/get_best_lang_value'
   import EntityLogo from '#inventory/components/entity_source_logo.svelte'
@@ -45,115 +46,131 @@
   $: candidate.customAuthorName = customAuthorName
 </script>
 <div class="listItem">
-  <div class="column work">
-    <span class="label">{i18n('title')}:</span>
-    {#if work}
-      <span class="workTitle">
-        {findBestLang(work, editionLang)}
-        <EntityLogo uri="{work.uri}"/>
-      </span>
-    {:else}
-      <input
-        type="text"
-        name="customWorkTitle"
-        class="customInput"
-        bind:value={customWorkTitle}
-        placeholder="{I18n('write book title')}">
-    {/if}
-  </div>
-  <span class="column authors">
-    <span class="label">{i18n('authors')}:</span>
-    {#if authors}
-      {#each authors as author, id}
-        <span class="authorName">
-          {findBestLang(author)}
-          <EntityLogo uri="{author.uri}"/>
-          {#if id !== authors.length - 1},&nbsp;{/if}
+  {#if edition?.image?.url}
+    <div class="candidateCover">
+      <img src="{imgSrc(edition.image.url, 80)}" alt='{findBestLang(work, editionLang)} cover'>
+    </div>
+  {/if}
+  <div class="textWrapper">
+    <div class="column work">
+      <span class="label">{i18n('title')}:</span>
+      {#if work}
+        <span class="workTitle">
+          {findBestLang(work, editionLang)}
+          <EntityLogo uri="{work.uri}"/>
         </span>
-      {/each}
-    {:else}
-      <input
-        type="text"
-        name="customAuthorName"
-        class="customInput"
-        bind:value={customAuthorName}
-      >
-    {/if}
-  </span>
-  <div class="column isbn">
-    {#if rawIsbn}
-      <span class="label">ISBN:</span>
-      {rawIsbn}
-    {/if}
-  </div>
-  <div class="column status">
-    {#if isbnData?.isInvalid}
-      <span class="invalid-isbn">{I18n('invalid ISBN')}</span>
-    {/if}
-    {#if error}
-      <div>{I18n('error:')} {error.status_verbose}</div>
-    {/if}
-    {#if needInfo}
-      <div>{I18n('need more information')}</div>
-    {/if}
-    {#if warning}
-      <div>{I18n(warning)}</div>
-    {/if}
-    {#if confirmInfo}
-      <div>{I18n('edit incorrect information')}</div>
-    {/if}
-    {#if existingItemsCount && !error && !item}
-      <span class="existing-entity-items">
-        {@html I18n('existing_entity_items', { smart_count: existingItemsCount, pathname: existingItemsPathname })}
-      </span>
-    {/if}
+      {:else}
+        <input
+          type="text"
+          name="customWorkTitle"
+          class="customInput"
+          bind:value={customWorkTitle}
+          placeholder="{I18n('write book title')}">
+      {/if}
+    </div>
+    <span class="column authors">
+      <span class="label">{i18n('authors')}:</span>
+      {#if authors}
+        {#each authors as author, id}
+          <span class="authorName">
+            {findBestLang(author)}
+            <EntityLogo uri="{author.uri}"/>
+            {#if id !== authors.length - 1},&nbsp;{/if}
+          </span>
+        {/each}
+      {:else}
+        <input
+          type="text"
+          name="customAuthorName"
+          class="customInput"
+          bind:value={customAuthorName}
+        >
+      {/if}
+    </span>
+    <div class="column isbn">
+      {#if rawIsbn}
+        <span class="label">ISBN:</span>
+        {rawIsbn}
+      {/if}
+    </div>
+    <div class="column status">
+      {#if isbnData?.isInvalid}
+        <span class="invalid-isbn">{I18n('invalid ISBN')}</span>
+      {/if}
+      {#if error}
+        <div>{I18n('error:')} {error.status_verbose}</div>
+      {/if}
+      {#if needInfo}
+        <div>{I18n('need more information')}</div>
+      {/if}
+      {#if warning}
+        <div>{I18n(warning)}</div>
+      {/if}
+      {#if confirmInfo}
+        <div>{I18n('edit incorrect information')}</div>
+      {/if}
+      {#if existingItemsCount && !error && !item}
+        <span class="existing-entity-items">
+          {@html I18n('existing_entity_items', { smart_count: existingItemsCount, pathname: existingItemsPathname })}
+        </span>
+      {/if}
+    </div>
   </div>
 </div>
 <style lang="scss">
   @import 'app/modules/general/scss/utils';
-  .column{
-    flex: 20 0 0;
-    padding: 0.2em;
-  }
-  .listItem{
-    @include display-flex(row, center, flex-start);
-    flex: 1 0 0;
-    .isbn{
-      @include display-flex(column);
-      text-align: right;
-      flex: 5 0 0;
+    .column{
+      flex: 20 0 0;
+      padding: 0.2em;
     }
-    .status{
-      @include display-flex(column);
-      text-align: right;
-      flex: 7 0 0;
+    .listItem{
+      flex: 1 0 0;
+      @include display-flex(row, center);
+      .candidateCover{
+        margin-right: 1em;
+      };
+      .textWrapper{
+        @include display-flex(row, center);
+        flex: 1 0 0;
+        .isbn{
+          @include display-flex(column);
+          text-align: right;
+          flex: 5 0 0;
+        }
+        .status{
+          @include display-flex(column);
+          text-align: right;
+          flex: 7 0 0;
+        }
+        .label{
+          display: none;
+        }
+      }
     }
-    .label{
-      display: none;
-    }
-  }
-  .authors, .work{
-    @include display-flex(row, center, center, wrap);
-      width: 100%;
-    .customInput{
-      width: 100%;
+    .authors, .work{
+      @include display-flex(row, center, center, wrap);
+        width: 100%;
+      .customInput{
+        width: 100%;
     }
   }
 
   /*Small screens*/
   @media screen and (max-width: 45em) {
     .listItem{
-      @include display-flex(column);
-      .column{
-        text-align: left;
-      }
-      .isbn{
-        @include display-flex(row, center);
-      }
-      .label{
-        display: inline;
-        color: $grey;
-        margin-right: 0.5em;
+      .textWrapper{
+        @include display-flex(column, center);
+        .column{
+          text-align: left;
+        }
+        .isbn{
+          @include display-flex(row, center);
+        }
+        .label{
+          display: inline;
+          color: $grey;
+          margin-right: 0.5em;
+        }
       }
     }
   }
