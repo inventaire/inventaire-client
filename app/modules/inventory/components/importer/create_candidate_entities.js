596caba59a7e378f345a967173c208906bd86c6d
jums <jums@inventaire.io>
2022-02-05 18:43:08 +0100

    rework logic around createCandidateEntities
    fixing work creation to be able to create a work without author

--- app/modules/inventory/components/importer/create_candidate_entities.js
+++ app/modules/inventory/components/importer/create_candidate_entities.js
@@ -4,23 +4,27 @@ import entityDraft from '#entities/lib/entity_draft_model'
 
 export const createEntitiesByCandidate = async candidate => {
   const { customWorkTitle, customAuthorName } = candidate
-  let workEntity
   if (!candidate.authors && customAuthorName) {
     const authorDraft = entityDraft.createDraft({ type: 'human', label: customAuthorName, claims: {} })
     const authorEntity = await createEntity(authorDraft)
     if (authorEntity) candidate.authors = [ authorEntity ]
   }
-  if (!candidate.works && candidate.authors) {
-    // TODO: handle several authors and remove candidate_row warning accordingly
-    const { uri } = candidate.authors[0]
-    if (uri) {
-      const workClaims = {}
-      workClaims['wdt:P50'] = [ uri ]
-      const workDraft = entityDraft.createDraft({ type: 'work', label: customWorkTitle, claims: workClaims })
-      workEntity = await createEntity(workDraft)
-      if (workEntity) candidate.works = [ workEntity ]
+
+  let workEntity
+  if (!candidate.works && customWorkTitle) {
+    const workClaims = {}
+    if (candidate.authors) {
+      // TODO: handle several authors and remove candidate_row warning accordingly
+      const { uri } = candidate.authors[0]
+      if (uri) {
+        workClaims['wdt:P50'] = [ uri ]
+      }
     }
+    const workDraft = entityDraft.createDraft({ type: 'work', label: customWorkTitle, claims: workClaims })
+    workEntity = await createEntity(workDraft)
+    if (workEntity) candidate.works = [ workEntity ]
   }
+
   if (!candidate.edition && workEntity) {
     const editionDraft = await createWorkEditionDraft({ workEntity, isbnData: candidate.isbnData })
     const editionEntity = await createEntity(editionDraft)
