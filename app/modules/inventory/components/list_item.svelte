ec0643b0daefba4a7d203ab0200f47952187d1c1
jums <jums@inventaire.io>
2022-02-08 10:39:47 +0100

    extract editor input into its own component

--- app/modules/inventory/components/list_item.svelte
+++ app/modules/inventory/components/list_item.svelte
@@ -1,9 +1,10 @@
 <script>
-  import { I18n, i18n } from '#user/lib/i18n'
+  import { i18n } from '#user/lib/i18n'
   import { imgSrc } from '#lib/handlebars_helpers/images'
   import getOriginalLang from '#entities/lib/get_original_lang'
   import getBestLangValue from '#entities/lib/get_best_lang_value'
   import EntityLogo from '#inventory/components/entity_source_logo.svelte'
+  import ClaimEditor from '#inventory/components/claim_editor.svelte'
   export let isbnData
   export let edition
   export let works
@@ -32,70 +33,54 @@
   }
 </script>
 <div class="listItem">
-    <div class="editionCover">
-      {#if edition?.image?.url}
-        <img src="{imgSrc(edition.image.url, 80)}" alt='{findBestLang(work, editionLang)} cover'>
+  <div class="editionCover">
+    {#if edition?.image?.url}
+      <img src="{imgSrc(edition.image.url, 80)}" alt='{findBestLang(work, editionLang)} cover'>
+    {/if}
+  </div>
+  <div class="textWrapper">
+    <div class="column work">
+      {#if work}
+        <span class="label">{i18n('title')}:</span>
+        <span class="workTitle">
+          {findBestLang(work, editionLang)}&nbsp;
+          <EntityLogo entity="{work}"/>
+        </span>
+      {:else}
+        {#if withEditor}
+          <ClaimEditor type='work' bind:inputName={customWorkTitle}/>
+        {/if}
       {/if}
     </div>
-    <div class="textWrapper">
-      <div class="column work">
-        {#if work}
-          <span class="label">{i18n('title')}:</span>
-          <span class="workTitle">
-            {findBestLang(work, editionLang)}&nbsp;
-            <EntityLogo entity="{work}"/>
+    <span class="column authors">
+      {#if authors && authors.length > 0}
+        <span class="label">{i18n('authors')}:</span>
+        {#each authors as author, id}
+          <span class="authorName">
+            {findBestLang(author)}&nbsp;
+            <EntityLogo entity="{author}"/>
+            {#if id !== authors.length - 1},&nbsp;{/if}
           </span>
-        {:else}
-          {#if withEditor}
-            <span class="editorRow">
-              <span class="label">{i18n('title')}:</span>
-              <input
-                on:click|stopPropagation
-                type="text"
-                name="customWorkTitle"
-                class="customInput"
-                bind:value={customWorkTitle}
-                placeholder="{I18n('must have book title')}">
-            </span>
-          {/if}
+        {/each}
+      {:else}
+        <!-- do not display editor if work exists, as it may be a book without author -->
+        <!-- always displaying editor might over crowd the visual, known case: /add/import -->
+        {#if withEditor && !work}
+          <ClaimEditor type='author' bind:inputName={customAuthorsNames}/>
         {/if}
-      </div>
-      <span class="column authors">
-        {#if authors && authors.length > 0}
-          <span class="label">{i18n('authors')}:</span>
-          {#each authors as author, id}
-            <span class="authorName">
-              {findBestLang(author)}&nbsp;
-              <EntityLogo entity="{author}"/>
-              {#if id !== authors.length - 1},&nbsp;{/if}
-            </span>
-          {/each}
-        {:else}
-          {#if withEditor}
-            <span class="editorRow">
-              <span class="label">{i18n('authors')}:</span>
-              <input
-                on:click|stopPropagation
-                type="text"
-                name="customAuthorName"
-                class="customInput"
-                bind:value={customAuthorName}
-              >
-            </span>
-          {/if}
+      {/if}
+    </span>
+    <div class="column isbn">
+      {#if rawIsbn}
+        <span class="label">ISBN:</span>
+        {rawIsbn}
+        {#if edition}
+          &nbsp;
+          <EntityLogo entity="{edition}"/>
         {/if}
-      </span>
-      <div class="column isbn">
-        {#if rawIsbn}
-          <span class="label">ISBN:</span>
-          {rawIsbn}
-          {#if edition}
-            &nbsp;
-            <EntityLogo entity="{edition}"/>
-          {/if}
-        {/if}
-      </div>
+      {/if}
     </div>
+  </div>
 </div>
 <style lang="scss">
   @import '#general/scss/utils';
