5ce307cf5f6a45f5ac77e43eae9c62d2c9e3ee79
jums <jums@inventaire.io>
2022-02-08 10:39:47 +0100

    extract editor input into its own component

---
+++ app/modules/inventory/components/claim_editor.svelte
@@ -0,0 +1,39 @@
+<script>
+  import { I18n } from '#user/lib/i18n'
+  export let inputName
+  export let type
+  const options = {
+    work: {
+      label: 'title',
+      placeholder: 'must have book title'
+    },
+    author: {
+      label: 'authors',
+    }
+  }
+</script>
+<span class="editorRow">
+  <span class="label">{I18n(options[type].label)}:</span>
+  <input
+    on:click|stopPropagation
+    type="text"
+    class="customInput"
+    bind:value={inputName}
+    placeholder={I18n(options[type].placeholder)}>
+</span>
+<style lang="scss">
+  @import 'app/modules/general/scss/utils';
+  .editorRow{
+    @include display-flex(row, center);
+  }
+  .label{
+    display: inline;
+    color: $grey;
+    font-size: 90%;
+    margin-right: 0.5em;
+  }
+  .customInput{
+    margin-bottom: 0;
+    width: 100%;
+  }
+</style>
