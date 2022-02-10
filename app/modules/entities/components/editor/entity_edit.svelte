595b63684bcc69ee358ca8025aa9a8ff6b06bc2c
maxlath <k@maxlath.eu>
2022-02-10 16:31:43 +0100

    entity_edit: add link to main entity page in header

--- app/modules/entities/components/editor/entity_edit.svelte
+++ app/modules/entities/components/editor/entity_edit.svelte
@@ -3,16 +3,34 @@
   import LabelsEditor from './labels_editor.svelte'
   import propertiesPerType from 'modules/entities/lib/editor/properties_per_type'
   import PropertyClaimsEditor from './property_claims_editor.svelte'
+  import getBestLangValue from 'modules/entities/lib/get_best_lang_value'
+  import { loadInteralLink } from 'lib/utils'
 
   export let entity
 
-  const { type } = entity
+  const { uri, type, labels } = entity
   const typeProperties = propertiesPerType[type]
+  const hasMonolingualTitle = typeProperties['wdt:P1476'] != null
+  const title = hasMonolingualTitle ? entity.claims['wdt:P1476']?.[0] : null
+  let {
+    value: favoriteLabel,
+    lang: favoriteLabelLang,
+  } = getBestLangValue(app.user.lang, null, labels)
 </script>
 
 <div class="entity-edit">
-  <h2>{I18n(entity.type)}</h2>
-  <LabelsEditor {entity} />
+  <h2>
+    <a href="/entity/{uri}" on:click={loadInteralLink}>
+      {favoriteLabel || title}
+    </a>
+  </h2>
+  <p class="type">{I18n(entity.type)}</p>
+  <p class="uri">{uri}</p>
+
+  {#if !hasMonolingualTitle}
+    <LabelsEditor {entity} bind:favoriteLabel {favoriteLabelLang} />
+  {/if}
+
   {#each Object.keys(typeProperties) as property}
     <PropertyClaimsEditor
       {entity}
@@ -30,6 +48,17 @@
     margin: 0 auto;
   }
   h2{
-    text-align: center;
+    margin-bottom: 0;
+    a{
+      @include link-dark;
+    }
+  }
+  .type{
+    color: $grey;
+    font-size: 1rem;
+  }
+  .uri{
+    @include sans-serif;
+    font-size: 0.8rem;
   }
 </style>
