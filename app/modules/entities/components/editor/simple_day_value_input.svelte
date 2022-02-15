a16ff9ff64e6c5af9271140ccce73f810ea71e8d
maxlath <k@maxlath.eu>
2022-02-16 00:04:39 +0100

    simple_day_value_input: separate the labels from the inputs so that inputs can follow each others in tab order

--- app/modules/entities/components/editor/simple_day_value_input.svelte
+++ app/modules/entities/components/editor/simple_day_value_input.svelte
@@ -2,6 +2,7 @@
   import { simpleDay } from 'lib/utils'
   import { i18n } from 'modules/user/lib/i18n'
   import SimpleDayValueInputField from './simple_day_value_input_field.svelte'
+  import SimpleDayValueInputLabel from './simple_day_value_input_label.svelte'
 
   export let currentValue, getInputValue
 
@@ -34,38 +35,65 @@
 </script>
 
 <form>
-  <SimpleDayValueInputField
-    name='year'
-    bind:value={year}
-    min=-3000
-    max={nextYear}
-    placeholder={currentYear}
-    optional={false}
-  />
+  <!-- Separate the labels from the inputs so that inputs can follow each others in tab order -->
+  <div class="labels">
+    <SimpleDayValueInputLabel
+      name='year'
+      bind:value={year}
+      optional={false}
+    />
 
-  <SimpleDayValueInputField
-    name='month'
-    bind:value={month}
-    min=1
-    max=12
-    placeholder={currentMonth}
-    closeButtonTitle={i18n('Remove month precision')}
-  />
+    <SimpleDayValueInputLabel
+      name='month'
+      bind:value={month}
+      closeButtonTitle={i18n('Remove month precision')}
+    />
 
-  <SimpleDayValueInputField
-    name='day'
-    bind:value={day}
-    min=1
-    max=31
-    placeholder={currentDay}
-    closeButtonTitle={i18n('Remove day precision')}
-  />
+    <SimpleDayValueInputLabel
+      name='day'
+      bind:value={day}
+      closeButtonTitle={i18n('Remove day precision')}
+    />
+  </div>
+
+  <div class="inputs">
+    <SimpleDayValueInputField
+      name='year'
+      bind:value={year}
+      min=-3000
+      max={nextYear}
+      placeholder={currentYear}
+      optional={false}
+    />
+
+    <SimpleDayValueInputField
+      name='month'
+      bind:value={month}
+      min=1
+      max=12
+      placeholder={currentMonth}
+    />
+
+    <SimpleDayValueInputField
+      name='day'
+      bind:value={day}
+      min=1
+      max=31
+      placeholder={currentDay}
+    />
+  </div>
 </form>
 
 <style lang="scss">
   @import 'app/modules/general/scss/utils';
   form{
-    @include display-flex(row);
     margin-right: auto;
+  }
+
+  .labels, .inputs{
+    @include display-flex(row);
+  }
+  .labels{
+    margin-bottom: 0.1em;
   }
 </style>
---
+++ app/modules/entities/components/editor/simple_day_value_input_label.svelte
@@ -0,0 +1,40 @@
+<script>
+  import { I18n } from 'modules/user/lib/i18n'
+  export let value, name, optional = true, closeButtonTitle
+</script>
+
+{#if value || !optional}
+  <div>
+    <label for="{name}">{I18n(name)}</label>
+    {#if optional}
+      <button
+        class="close"
+        title={closeButtonTitle}
+        on:click={() => value = null}
+      >
+      &#215;
+      </button>
+    {/if}
+  </div>
+{/if}
+
+<style lang="scss">
+  @import 'app/modules/general/scss/utils';
+  div{
+    margin-bottom: 0.1em;
+    height: 1.5rem;
+    width: 5rem;
+    margin-right: 0.5em;
+    @include display-flex(row, center, flex-start);
+  }
+  .close{
+    font-size: 1.5rem;
+    margin: 0 0 0 auto;
+    padding: 0;
+    line-height: 0;
+    height: 100%;
+    width: 1.5rem;
+    @include shy(0.8);
+    @include bg-hover(white);
+  }
+</style>
