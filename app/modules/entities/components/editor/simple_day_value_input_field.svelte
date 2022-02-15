feb7f9c2f1ed3d5709dc188c1629e376728bca57
maxlath <k@maxlath.eu>
2022-02-16 00:04:39 +0100

    simple_day_value_input: separate the labels from the inputs so that inputs can follow each others in tab order

--- app/modules/entities/components/editor/simple_day_value_input_field.svelte
+++ app/modules/entities/components/editor/simple_day_value_input_field.svelte
@@ -1,6 +1,6 @@
 <script>
-  import { i18n, I18n } from 'modules/user/lib/i18n'
-  export let value, name, min, max, placeholder, optional = true, closeButtonTitle
+  import { i18n } from 'modules/user/lib/i18n'
+  export let value, name, min, max, placeholder, optional = true
 
   let inputEl
 
@@ -11,33 +11,19 @@
 </script>
 
 {#if value || !optional}
-  <fieldset>
-    <div>
-      <label for="{name}">{I18n(name)}</label>
-      {#if optional}
-        <button
-          class="close"
-          on:click={() => value = null}
-        >
-        &#215;
-        </button>
-      {/if}
-    </div>
-    <input
-      {name}
-      type="number"
-      {min}
-      {max}
-      step=1
-      value={value}
-      {placeholder}
-      bind:this={inputEl}
-    >
-  </fieldset>
+  <input
+    {name}
+    type="number"
+    {min}
+    {max}
+    step=1
+    bind:value={value}
+    {placeholder}
+    bind:this={inputEl}
+  >
 {:else}
   <button
     class="tiny-button"
-    title={closeButtonTitle}
     on:click={initValue}
     >
     + {i18n(name)}
@@ -46,40 +32,21 @@
 
 <style lang="scss">
   @import 'app/modules/general/scss/utils';
-  .tiny-button{
+  input{
+    margin: 0;
+  }
+  input, button{
+    width: 5rem;
+    margin-right: 0.5em;
+  }
+  button{
     font-weight: normal;
     white-space: nowrap;
     align-self: flex-end;
     height: 2.3em;
-    margin: 0 0.5em 0.1em 0;
     @include shy(0.9);
-  }
-  input, .tiny-button{
-    width: 5em;
-  }
-  input{
-    margin: 0;
   }
   input:invalid{
     border: 2px red solid;
-  }
-  fieldset{
-    margin-right: 0.5em;
-    height: 4em;
-  }
-  fieldset div{
-    margin-bottom: 0.1em;
-    height: 1.5rem;
-    @include display-flex(row, center, flex-start);
-  }
-  .close{
-    font-size: 1.5rem;
-    margin: 0 0 0 auto;
-    padding: 0;
-    line-height: 0;
-    height: 100%;
-    width: 1.5rem;
-    @include shy(0.8);
-    @include bg-hover(white);
   }
 </style>
