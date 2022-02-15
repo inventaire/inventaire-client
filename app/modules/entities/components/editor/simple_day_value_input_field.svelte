ecbff620542851d495a901f51d4fbf905661e4c3
maxlath <k@maxlath.eu>
2022-02-16 00:45:07 +0100

    simple_day_value_input: add keyboard controls

--- app/modules/entities/components/editor/simple_day_value_input_field.svelte
+++ app/modules/entities/components/editor/simple_day_value_input_field.svelte
@@ -1,23 +1,32 @@
 <script>
   import { i18n } from 'modules/user/lib/i18n'
-  export let value, name, min, max, placeholder, optional = true
+  import { createEventDispatcher } from 'svelte'
+  import { BubbleUpComponentEvent } from 'lib/utils'
+
+  export let value, name, min, max, placeholder, optional = true, componentId
 
   let inputEl
+
+  const dispatch = createEventDispatcher()
+  const bubbleUpEvent = BubbleUpComponentEvent(dispatch)
 
   function initValue () {
     value = value || 1
   }
+
   $: inputEl && inputEl.focus()
 </script>
 
 {#if value || !optional}
   <input
-    {name}
     type="number"
+    id="{componentId}-{name}"
+    {name}
     {min}
     {max}
     step=1
     bind:value={value}
+    on:keyup={bubbleUpEvent}
     {placeholder}
     bind:this={inputEl}
   >
@@ -26,7 +35,8 @@
     title={i18n(`Precise the date to the ${name}`)}
     class="tiny-button"
     on:click={initValue}
-    >
+    on:keyup={bubbleUpEvent}
+  >
     + {i18n(name)}
   </button>
 {/if}
