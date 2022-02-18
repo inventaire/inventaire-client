cde95af7f6bab313e199f626b8abdc6fc9e3e70d
maxlath <k@maxlath.eu>
2022-02-18 18:55:11 +0100

    find_uri: add tests

--- tests/112-find-uri.js
+++ tests/112-find-uri.js
@@ -1,4 +1,4 @@
-import 'should'
+import should from 'should'
 import findUri from '#modules/search/lib/find_uri'
 
 describe('findUri', () => {
@@ -7,5 +7,25 @@ describe('findUri', () => {
     findUri('Q1').should.equal('wd:Q1')
     findUri('wd:Q1').should.equal('wd:Q1')
     findUri('wd:q1').should.equal('wd:Q1')
+  })
+
+  it('should find an inv uri', () => {
+    const uri = 'inv:e7ecbb0a797eabca4e1706b33a000c71'
+    findUri(uri).should.equal(uri)
+  })
+
+  it('should find an isbn uri', () => {
+    findUri('9780552167741').should.equal('isbn:9780552167741')
+    findUri('978-0-552-16774-1').should.equal('isbn:9780552167741')
+    findUri('978-0552167741').should.equal('isbn:9780552167741')
+    findUri('0-552-16774-6').should.equal('isbn:0552167746')
+    findUri('0552167746').should.equal('isbn:0552167746')
+    findUri('isbn:9780552167741').should.equal('isbn:9780552167741')
+  })
+
+  it('should return nothing for non-uri', () => {
+    should(findUri('foo')).not.be.ok()
+    should(findUri('p1')).not.be.ok()
+    should(findUri('azQ123')).not.be.ok()
   })
 })
