4ee1a653f596b3faccf1689b30a1d4d235888e3e
jums <jums@inventaire.io>
2022-02-23 18:19:06 +0100

    create candidate: fetch entities: use the resolver before getting by uris
    notably to resolve with imported identifiers
    then extract resolved entry uris, and initiate get by uris with relatives as resolver to not answer any relatives entities
    
    in every case, guess edition uri and pass it to get by uris, in order to
    create unresolved entities from seed (which the resolver does not do)
    
    remove useless reassignation of redirects uri to normalizedIsbn, as
    canonical uri should be the edition entity already assign to candidate

--- app/modules/inventory/lib/import_helpers.js
+++ app/modules/inventory/lib/import_helpers.js
@@ -1,19 +1,32 @@
 import getBestLangValue from '#entities/lib/get_best_lang_value'
 import getOriginalLang from '#entities/lib/get_original_lang'
-import { isNonEmptyString } from '#lib/boolean_tests'
+import { isNonEmptyString, isNonEmptyArray } from '#lib/boolean_tests'
+import preq from '#lib/preq'
 
-export const createCandidate = (preCandidate, entitiesRes) => {
-  const { index, isbnData, customWorkTitle, customAuthorsNames, details, notes } = preCandidate
+export const createCandidate = (externalEntry, entitiesRes) => {
+  const {
+    index,
+    isbnData,
+    customWorkTitle,
+    customAuthorsNames,
+    details,
+    notes,
+    publicationDate,
+    numberOfPages,
+    goodReadsEditionId,
+    libraryThingWorkId
+  } = externalEntry
+
   const candidate = { index }
-  if (isbnData && isbnData.normalizedIsbn.length === 10) {
-    const redirectsUri = entitiesRes.redirects[guessUriFromIsbn({ preCandidate })]
-    if (redirectsUri) isbnData.normalizedIsbn = redirectsUri.replace(/isbn:/, '')
-  }
   if (isbnData) candidate.isbnData = isbnData
   if (isNonEmptyString(customWorkTitle)) candidate.customWorkTitle = customWorkTitle
   if (customAuthorsNames) candidate.customAuthorsNames = customAuthorsNames
-  if (details) candidate.details = details
-  if (notes) candidate.notes = notes
+  if (isNonEmptyString(details)) candidate.details = details
+  if (isNonEmptyString(notes)) candidate.notes = notes
+  if (isNonEmptyString(publicationDate)) candidate.publicationDate = publicationDate
+  if (numberOfPages) candidate.numberOfPages = numberOfPages
+  if (goodReadsEditionId) candidate.goodReadsEditionId = goodReadsEditionId
+  if (libraryThingWorkId) candidate.libraryThingWorkId = libraryThingWorkId
 
   if (!entitiesRes) return candidate
   const entities = Object.values(entitiesRes.entities).map(serializeEntity)
@@ -79,4 +92,62 @@ export const addExistingItemsCountToCandidate = counts => candidate => {
   const count = counts[uri]
   if (count != null) candidate.existingItemsCount = count
   return candidate
+}
+
+export const resolveEntryAndFetchEntities = async preCandidate => {
+  // take advantage of the file upload to update existing entities ASAP
+  // but do not create to let user verify unresolved entries information
+  const entry = serializeResolverEntry(preCandidate)
+  const { entries } = await preq.post(app.API.entities.resolve, {
+    entries: [ entry ],
+    update: true
+  })
+
+  const uris = []
+  const { edition, works, authors } = entries[0]
+  const pushUri = subEntry => { if (subEntry.uri) uris.push(subEntry.uri) }
+  pushUri(edition)
+  works.forEach(pushUri)
+  authors.forEach(pushUri)
+  if (!isNonEmptyArray(uris)) return
+
+  return preq.get(app.API.entities.getByUris(uris, false, editionRelatives))
+}
+
+const editionRelatives = [ 'wdt:P629', 'wdt:P50' ]
+
+export const serializeResolverEntry = data => {
+  const { isbn, customWorkTitle: title, lang, customAuthorsNames, normalizedIsbn } = data
+  const labelLang = lang || app.user.lang
+
+  const edition = {
+    isbn: isbn || normalizedIsbn,
+    claims: {
+      'wdt:P1476': [ title ]
+    }
+  }
+
+  const work = { labels: {}, claims: {} }
+  work.labels[labelLang] = title
+
+  if (data.publicationDate != null) edition.claims['wdt:P577'] = data.publicationDate
+  if (data.numberOfPages != null) edition.claims['wdt:P1104'] = data.numberOfPages
+  if (data.goodReadsEditionId != null) edition.claims['wdt:P2969'] = data.goodReadsEditionId
+  if (data.libraryThingWorkId != null) work.claims['wdt:P1085'] = data.libraryThingWorkId
+
+  let authors
+
+  if (customAuthorsNames) {
+    authors = customAuthorsNames.map(name => {
+      const labels = { [labelLang]: name }
+      return { labels }
+    })
+  }
+
+  return { edition, works: [ work ], authors }
+}
+
+export const getEditionEntitiesByUri = async isbn => {
+  const editionUri = `isbn:${isbn}`
+  return preq.get(app.API.entities.getByUris(editionUri, false, editionRelatives))
 }
