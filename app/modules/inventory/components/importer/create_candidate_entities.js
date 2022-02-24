47e7cc7a63a223f9a39dc0e37c7fcc3249a089e8
jums <jums@inventaire.io>
2022-02-24 09:57:26 +0100

    refactor: extract create_candidate_entities block into its own function
    to ease reviewer and future people lisibility
    most original code was done in commit 'display items results'

--- app/modules/inventory/components/importer/create_candidate_entities.js
+++ app/modules/inventory/components/importer/create_candidate_entities.js
@@ -8,41 +8,58 @@ export const createEntitiesByCandidate = async candidate => {
     customWorkTitle,
     customAuthorsNames,
     libraryThingWorkId,
+  } = candidate
+
+  if (!isNonEmptyArray(candidate.authors) && isNonEmptyArray(customAuthorsNames)) {
+    candidate.authors = await createAuthors(customAuthorsNames)
+  }
+
+  if (!isNonEmptyArray(candidate.works) && customWorkTitle) {
+    candidate.works = await createWorks(customWorkTitle, libraryThingWorkId)
+  }
+
+  if (!candidate.edition && isNonEmptyArray(candidate.works)) {
+    candidate.edition = await createEdition(candidate)
+  }
+  return candidate
+}
+
+const createAuthors = async customAuthorsNames => {
+  const createdAuthorsEntities = await Promise.all(customAuthorsNames.map(authorName => {
+    const authorDraft = entityDraft.createDraft({ type: 'human', label: authorName, claims: {} })
+    return createEntity(authorDraft)
+  }))
+  if (isNonEmptyArray(createdAuthorsEntities)) return createdAuthorsEntities
+}
+
+const createWorks = async (authors, customWorkTitle, libraryThingWorkId) => {
+  const workClaims = {}
+  if (isNonEmptyArray(authors)) {
+    const authorsUris = authors.map(_.property('uri'))
+    if (isNonEmptyArray(authorsUris)) workClaims['wdt:P50'] = authorsUris
+  }
+  if (libraryThingWorkId != null) workClaims['wdt:P1085'] = libraryThingWorkId
+
+  const workDraft = entityDraft.createDraft({ type: 'work', label: customWorkTitle, claims: workClaims })
+  const workEntity = await createEntity(workDraft)
+  if (workEntity) return [ workEntity ]
+}
+
+const createEdition = async candidate => {
+  const {
+    isbnData,
+    works,
     publicationDate,
     numberOfPages,
     goodReadsEditionId
   } = candidate
-  if (!isNonEmptyArray(candidate.authors) && isNonEmptyArray(customAuthorsNames)) {
-    const createdAuthorsEntities = await Promise.all(customAuthorsNames.map(authorName => {
-      const authorDraft = entityDraft.createDraft({ type: 'human', label: authorName, claims: {} })
-      return createEntity(authorDraft)
-    }))
-    if (isNonEmptyArray(createdAuthorsEntities)) candidate.authors = createdAuthorsEntities
-  }
 
-  let workEntity
-  if (!isNonEmptyArray(candidate.works) && customWorkTitle) {
-    const workClaims = {}
-    if (isNonEmptyArray(candidate.authors)) {
-      const authorsUris = candidate.authors.map(_.property('uri'))
-      if (isNonEmptyArray(authorsUris)) workClaims['wdt:P50'] = authorsUris
-    }
-    if (libraryThingWorkId != null) workClaims['wdt:P1085'] = libraryThingWorkId
+  const editionClaims = {}
+  if (publicationDate != null) editionClaims['wdt:P577'] = publicationDate
+  if (numberOfPages != null) editionClaims['wdt:P1104'] = numberOfPages
+  if (goodReadsEditionId != null) editionClaims['wdt:P2969'] = goodReadsEditionId
 
-    const workDraft = entityDraft.createDraft({ type: 'work', label: customWorkTitle, claims: workClaims })
-    workEntity = await createEntity(workDraft)
-    if (workEntity) candidate.works = [ workEntity ]
-  }
-
-  if (!candidate.edition && isNonEmptyArray(candidate.works)) {
-    const editionClaims = {}
-    if (publicationDate != null) editionClaims['wdt:P577'] = publicationDate
-    if (numberOfPages != null) editionClaims['wdt:P1104'] = numberOfPages
-    if (goodReadsEditionId != null) editionClaims['wdt:P2969'] = goodReadsEditionId
-
-    const editionDraft = await createWorkEditionDraft({ workEntity, isbnData: candidate.isbnData, editionClaims })
-    const editionEntity = await createEntity(editionDraft)
-    if (editionEntity) candidate.edition = editionEntity
-  }
-  return candidate
+  const editionDraft = await createWorkEditionDraft({ workEntity: works[0], isbnData, editionClaims })
+  const editionEntity = await createEntity(editionDraft)
+  if (editionEntity) return editionEntity
 }
