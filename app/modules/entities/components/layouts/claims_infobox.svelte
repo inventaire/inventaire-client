d9827a89cf97cf9122e1c987f480c30da2c35215
jums <jums@inventaire.io>
2022-03-11 11:27:34 +0100

    reorder edition claims
    inspired by small editors websites
    ([libertalia](https://www.editionslibertalia.com/catalogue/ceux-d-en-bas/),
    [la fabrique](https://lafabrique.fr/les-patrons-de-la-presse-nationale/),
    [communs](https://www.editionsducommun.org/collections/all/products/se-faire-virer-manon-delatre)
    to name a few)
    which have a pattern of ordering information of an edition as follow:
    - date of publication
    - number of pages
    - isbn 13
    
    authors are also displayed all the time before those information, so
    translators and foreword/afterword authors are displayed first

--- app/modules/entities/components/layouts/claims_infobox.svelte
+++ app/modules/entities/components/layouts/claims_infobox.svelte
@@ -1,12 +1,13 @@
 <script>
   export let claims
+  export let claimsOrder = []
   import { formatClaim } from '#entities/components/lib/claims_helpers'
 
-  const props = Object.keys(claims)
+  const props = Object.keys(claims).sort((a, b) => claimsOrder.indexOf(a) - claimsOrder.indexOf(b))
 </script>
 <div class="claims">
   {#each props as prop}
-      {@html formatClaim(prop, claims)}
+    {@html formatClaim(prop, claims)}
   {/each}
 </div>
 <style lang="scss">
--- app/modules/entities/components/layouts/edition.svelte
+++ app/modules/entities/components/layouts/edition.svelte
@@ -1,10 +1,31 @@
 <script>
   import { I18n } from '#user/lib/i18n'
   import { imgSrc } from '#lib/handlebars_helpers/images'
+  import { isNonEmptyArray } from '#lib/boolean_tests'
   import ClaimsInfobox from './claims_infobox.svelte'
-  export let entity, standalone
+  export let entity, works, standalone
 
-  const { uri, image, label, claims } = entity
+  const { uri, image, label } = entity
+  let { claims } = entity
+
+  }
+
+  const claimsOrder = [
+    ...editionWorkProperties,
+    'wdt:P2679', // author of foreword
+    'wdt:P2680', // author of afterword
+    'wdt:P655', // translator
+    'wdt:P577', // date of publication
+    'wdt:P1104', // number of pages
+    'wdt:P123', // publisher
+    'wdt:P212', // isbn 13
+    'wdt:P957', // isbn 10
+    'wdt:P407', // language
+    'wdt:P629', // work
+    'wdt:P195', // collection
+    'wdt:P2635', // number of volumes
+    'wdt:P856', // official website
+  ]
 
   $: app.navigate(`/entity/${uri}`)
 </script>
@@ -20,7 +41,7 @@
     {/if}
     <div class="claims entity-data-box">
       <h2 class="edition-title">{entity.claims['wdt:P1476']}</h2>
-      <ClaimsInfobox {claims}/>
+      <ClaimsInfobox {claims} {claimsOrder}/>
       <!-- TODO: 'entities:edit_data' -->
     </div>
   </div>
