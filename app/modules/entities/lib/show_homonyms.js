54cd937bb272a9c97ecbdfe7994ea26f9d998eee
maxlath <k@maxlath.eu>
2022-03-13 21:14:01 +0100

    show_homonyms: search exact match with labels in other languages and aliases

--- app/modules/entities/lib/show_homonyms.js
+++ app/modules/entities/lib/show_homonyms.js
@@ -16,21 +16,47 @@ export default async params => {
 }
 
 const getHomonyms = async model => {
-  const [ uri, label ] = model.gets('uri', 'label')
+  const [ uri, labels, aliases ] = model.gets('uri', 'labels', 'aliases')
+  const terms = getTerms(labels, aliases)
   const { pluralizedType } = model
-  const { results } = await preq.get(app.API.search({
-    types: pluralizedType,
-    search: label,
-    limit: 100,
-    exact: true
-  }))
+  const responses = await Promise.all(terms.map(searchTerm(pluralizedType)))
+  const results = _.pluck(responses, 'results').flat()
   return parseSearchResults(uri, results)
 }
 
+const searchTerm = types => term => {
+  return preq.get(app.API.search({
+    types,
+    search: term,
+    limit: 100,
+    exact: true
+  }))
+}
+
 const parseSearchResults = async (uri, searchResults) => {
-  const uris = _.pluck(searchResults, 'uri')
+  const uris = _.uniq(_.pluck(searchResults, 'uri'))
+    .filter(result => result.uri !== uri)
   // Search results entities miss their claims, so we need to fetch the full entities
   const entities = await app.request('get:entities:models', { uris })
   // Re-filter out uris to omit as a redirection might have brought it back
   return entities.filter(entity => entity.get('uri') !== uri)
+}
+
+const getTerms = (labels, aliases) => {
+  let terms = Object.values(labels)
+    .concat(Object.values(aliases).flat())
+    // Order term words to not search both "foo bar" and "bar foo"
+    // as words order doesn't matter
+    .map(orderTermWordsAlphabetically)
+  return _.uniq(terms)
+}
+
+const orderTermWordsAlphabetically = term => {
+  return term
+  .toLowerCase()
+  // Remove characters known to create duplicates
+  .replace(/[.]/g, '')
+  .split(' ')
+  .sort((a, b) => a > b ? 1 : -1)
+  .join(' ')
 }
