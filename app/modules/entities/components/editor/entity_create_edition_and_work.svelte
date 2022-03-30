22faf1b80d6435aa84ff3e73422180acd10bcd99
maxlath <k@maxlath.eu>
2022-03-30 16:03:41 +0200

    entity_create_edition_and_work: allow to set more claims

--- app/modules/entities/components/editor/entity_create_edition_and_work.svelte
+++ app/modules/entities/components/editor/entity_create_edition_and_work.svelte
@@ -3,6 +3,8 @@
   import PropertyClaimsEditor from '#entities/components/editor/property_claims_editor.svelte'
   import wdLang from 'wikidata-lang'
   import preq from '#lib/preq'
+  import WrapToggler from '#components/wrap_toggler.svelte'
+  import propertiesPerType from '#entities/lib/editor/properties_per_type'
 
   export let edition
 
@@ -13,13 +15,16 @@
     claims: {}
   }
 
-  const workProperties = [
+  let showAllWorkProperties = false
+  let showAllEditionProperties = false
+
+  let workPropertiesShortlist = [
     'wdt:P50',
     'wdt:P136',
     'wdt:P921',
   ]
 
-  const editionProperties = [
+  let editionPropertiesShortlist = [
     'wdt:P212',
     'wdt:P1476', // required
     'wdt:P1680',
@@ -27,6 +32,17 @@
     'wdt:P123',
     'invp:P2',
   ]
+
+  const allWorkProperties = Object.keys(propertiesPerType.work)
+  const allEditionProperties = _.without(Object.keys(propertiesPerType.edition), 'wdt:P629')
+  const isShortlisted = shortlist => property => shortlist.includes(property)
+
+  // Regenerate shortlists from propertiesPerType properties to preserve order
+  workPropertiesShortlist = allWorkProperties.filter(isShortlisted(workPropertiesShortlist))
+  editionPropertiesShortlist = allEditionProperties.filter(isShortlisted(editionPropertiesShortlist))
+
+  $: displayedWorkProperties = showAllWorkProperties ? allWorkProperties : workPropertiesShortlist
+  $: displayedEditionProperties = showAllEditionProperties ? allEditionProperties : editionPropertiesShortlist
 
   // TODO: add required properties
 
@@ -55,26 +71,39 @@
     {i18n('No data could be found for that ISBN.')}
     {I18n('can you tell us more about this work and this particular edition?')}
   </p>
+
   <section class="work">
     <h2>{I18n('work')}</h2>
     <p class="help">{i18n('Data common to all editions of this book')}</p>
-    {#each workProperties as property}
+    {#each displayedWorkProperties as property (property)}
       <PropertyClaimsEditor
         bind:entity={work}
         {property}
       />
     {/each}
+    <WrapToggler
+      bind:show={showAllWorkProperties}
+      moreText={I18n('add more details')}
+      lessText={I18n('show only main properties')}
+    />
   </section>
+
   <section class="edition">
     <h2>{I18n('edition')}</h2>
     <p class="help">{i18n('Data specific to that particular edition')}</p>
-    {#each editionProperties as property}
+    {#each displayedEditionProperties as property (property)}
       <PropertyClaimsEditor
         bind:entity={edition}
         {property}
       />
     {/each}
+    <WrapToggler
+      bind:show={showAllEditionProperties}
+      moreText={I18n('add more details')}
+      lessText={I18n('show only main properties')}
+    />
   </section>
+
   <button
     on:click={create}
     class="success-button"
@@ -109,5 +138,10 @@
   }
   .success-button{
     margin: 0 auto;
+  }
+  section{
+    :global(.wrap-toggler) {
+      margin: 0 auto;
+    }
   }
 </style>
---
+++ app/modules/general/components/wrap_toggler.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { icon } from '#lib/handlebars_helpers/icons'
+  export let show, moreText, lessText
+</script>
+
+{#if show}
+  {#if lessText}
+    <button
+      class="wrap-toggler"
+      on:click={() => show = false}
+      >
+      {@html icon('chevron-up')}
+      {lessText}
+    </button>
+  {/if}
+{:else}
+  <button
+    class="wrap-toggler"
+    on:click={() => show = true}
+    >
+    {@html icon('chevron-down')}
+    {moreText}
+  </button>
+{/if}
+
+<style lang="scss">
+  @import '#general/scss/utils';
+  button{
+    display: block;
+    @include shy;
+  }
+</style>
