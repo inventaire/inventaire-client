13e1d560161c4ec8dad47d6544e8d175d9c5b2a9
maxlath <k@maxlath.eu>
2022-05-13 20:29:47 +0200

    remove decaffeinate scripts
    we are unlikely to need them anymore ;)

--- bulk-decaffeinate.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  decaffeinateArgs: [ '--loose', '--optional-chaining', '--disable-suggestion-comment' ],
-  useJSModules: true,
-  mochaEnvFilePattern: '^.test.js$',
-  eslintPath: '~/code/inventaire/inventaire/client/node_modules/.bin/eslint',
-  decaffeinatePath: '~/.nvm/versions/node/v12.18.4/bin/decaffeinate',
-  numWorkers: 8
-  // "skipVerify": true
-}
