7e39b249670c50e246d636ed0683f3924b28b2d1
maxlath <k@maxlath.eu>
2022-05-14 20:24:12 +0200

    fixup! default suggestions: + wdt:P50

--- app/modules/entities/components/editor/lib/suggestions/wdt_P50.js
+++ app/modules/entities/components/editor/lib/suggestions/wdt_P50.js
@@ -1,7 +1,10 @@
+import { difference } from 'underscore'
+
 export default async function ({ entity }) {
   const serieUri = entity.claims['wdt:P179']?.[0]
   if (serieUri == null) return
-
   const serie = await app.request('get:entity:model', serieUri)
-  return serie.getExtendedAuthorsUris()
+  const authorsUris = entity.claims['wdt:P50'] || []
+  const serieAuthorsUris = await serie.getExtendedAuthorsUris()
+  return difference(serieAuthorsUris, authorsUris)
 }
