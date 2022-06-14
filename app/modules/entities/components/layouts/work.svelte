4323e2075d3c8382585cfaecf01732115d49cb55
jums <jums@inventaire.io>
2022-06-14 10:38:45 +0200

    add empty work layout

---
+++ app/modules/entities/components/layouts/work.svelte
@@ -0,0 +1,68 @@
+<script>
+  import BaseLayout from './base_layout.svelte'
+  import AuthorsInfo from './authors_info.svelte'
+
+  export let entity, standalone
+
+  const { uri } = entity
+
+  $: claims = entity.claims
+  $: notOnlyP31 = Object.keys(claims).length > 1
+  $: app.navigate(`/entity/${uri}`)
+</script>
+
+<BaseLayout
+  bind:entity={entity}
+  {standalone}
+>
+  <div class="entity-layout" slot="entity">
+    <div class="top-section">
+      {#if notOnlyP31}
+        <div class="work-section">
+          <AuthorsInfo
+            {claims}
+          />
+        </div>
+      {/if}
+  </div>
+</BaseLayout>
+
+<style lang="scss">
+  @import '#general/scss/utils';
+  $entity-max-width: 650px;
+  .entity-layout{
+    @include display-flex(column, center);
+    width: 100%;
+  }
+  .top-section{
+    @include display-flex(row, flex-start, center);
+    width: 100%;
+  }
+  .work-section{
+    @include display-flex(column, flex-start);
+    flex: 1 0 0;
+    margin: 0 1em;
+  }
+
+  /*Small screens*/
+  @media screen and (max-width: $small-screen) {
+    .work-section{
+      margin-left: 0;
+      margin-right: 1em;
+    }
+  }
+
+  /*Smaller screens*/
+  @media screen and (max-width: $smaller-screen) {
+    .top-section{
+      @include display-flex(column);
+    }
+  }
+
+  /*Small screens*/
+  @media screen and (max-width: $smaller-screen) {
+    .entity-layout{
+      @include display-flex(column);
+    }
+  }
+</style>
