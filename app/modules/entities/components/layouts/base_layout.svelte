c6cb05061809c636c6a1bd1ae3e416c3f382ed19
jums <jums@inventaire.io>
2022-06-14 10:37:54 +0200

    introduction svelte layout
    which destroy previous backbone entities layout, this is because we cant
    guess entity type from a uri, and entity object is directly formatted as backbone
    model.
    Alternative could have been to debackbonify entity model, but its way
    easier to start from scratch and bonfire backbone stuff at the end of
    the branch, but its necessarly need to replace *all* types of entity layouts
    (luckily that's what we want to do)
    
    this commit is keeping feature parity: fetching edition's works while
    displaying edition layout, but works in edition layout will be
    introducted later

---
+++ app/modules/entities/components/layouts/base_layout.svelte
@@ -0,0 +1,73 @@
+<script>
+  import EditDataActions from './edit_data_actions.svelte'
+  import EntityHeader from '../entity_header.svelte'
+  import preq from '#lib/preq'
+
+  export let entity
+  export let standalone
+
+  const refreshEntity = async () => {
+    const { entities } = await preq.get(app.API.entities.getByUris(entity.uri, true))
+    entity = Object.values(entities)[0]
+  }
+</script>
+
+<div class="layout">
+  <div class="header-wrapper">
+    <div class="header-title">
+      <EntityHeader {entity} {standalone}/>
+    </div>
+    <div class="header">
+      <EditDataActions
+        {entity}
+        on:refreshEntity={refreshEntity}
+      />
+    </div>
+  </div>
+  <div class="entity-wrapper">
+    <slot name="entity" />
+  </div>
+</div>
+
+<style lang="scss">
+  @import '#general/scss/utils';
+  .layout{
+    @include display-flex(column, stretch, center);
+    margin: 0 auto;
+    max-width: 70em;
+    padding: 0 1em;
+    background-color: white;
+  }
+  .header-title{
+    @include display-flex(row, center);
+    width: 100%;
+  }
+  .header-wrapper{
+    display: flex;
+    margin: 1em 0;
+  }
+  .header{
+    @include display-flex(row, center, center);
+    width: 100%;
+  }
+  .entity-wrapper{
+    @include display-flex(column, flex-start);
+    margin-bottom: 2em;
+  }
+  /*Very Small screens*/
+  @media screen and (max-width: $very-small-screen) {
+    .layout{
+      padding: 0;
+    }
+  }
+  /*Small screens*/
+  @media screen and (max-width: $small-screen) {
+    .header-wrapper{
+      @include display-flex(row, center, space-between);
+      padding-bottom: 1em;
+    }
+    .entity-wrapper{
+      @include display-flex(column, flex-start);
+    }
+  }
+</style>
