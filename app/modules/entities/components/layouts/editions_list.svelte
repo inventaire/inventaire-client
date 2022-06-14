3352edeadd1b240abf7a67feb6fbfef376f44081
jums <jums@inventaire.io>
2022-06-14 12:12:41 +0200

    add work editions list

---
+++ app/modules/entities/components/layouts/editions_list.svelte
@@ -0,0 +1,89 @@
+<script>
+  import { I18n } from '#user/lib/i18n'
+  import { createEventDispatcher } from 'svelte'
+  import { getLang, hasSelectedLang } from '#entities/components/lib/claims_helpers'
+  import EditionsListActions from './editions_list_actions.svelte'
+  import EntitiesList from './entities_list.svelte'
+
+  const dispatch = createEventDispatcher()
+
+  export let someInitialEditions,
+    someEditions,
+    editions,
+    publishersByUris,
+    initialEditions,
+    usersSize
+
+  let userLang = app.user.lang
+  let selectedLangs = [ userLang ]
+  let editionsLangs
+
+  const filterEditionByLang = _ => {
+    if (selectedLangs.length === editionsLangs.length) {
+      return editions = initialEditions
+    }
+    editions = initialEditions.filter(hasSelectedLang(selectedLangs))
+  }
+
+  const prioritizeMainUserLang = langs => {
+    if (langs.includes(userLang)) {
+      const userLangIndex = langs.indexOf(userLang)
+      langs.splice(userLangIndex, 1)
+      langs.unshift(userLang)
+    }
+    return langs
+  }
+
+  $: {
+    if (initialEditions) {
+      let rawEditionsLangs = _.compact(_.uniq(initialEditions.map(getLang)))
+      editionsLangs = prioritizeMainUserLang(rawEditionsLangs)
+    }
+  }
+  $: selectedLangs && filterEditionByLang(initialEditions)
+</script>
+<div class="editions-section">
+  <div class="editions-list-title">
+    <h5>
+      {I18n('editions')}
+    </h5>
+  </div>
+  <EditionsListActions
+    bind:selectedLangs={selectedLangs}
+    bind:editionsLangs={editionsLangs}
+    {someInitialEditions}
+    {someEditions}
+    {usersSize}
+    on:scrollToItemsList={() => dispatch('scrollToItemsList')}
+    on:showMap={() => { dispatch('showMap') }}
+  />
+  {#if someEditions}
+    <EntitiesList
+      entities={editions}
+      relatedEntities={publishersByUris}
+      type='editions'
+    />
+  {:else}
+    <div class="no-edition-wrapper">
+      {I18n('no editions found')}
+    </div>
+  {/if}
+</div>
+<style lang="scss">
+  @import '#general/scss/utils';
+
+  .editions-section{
+    @include display-flex(column, center);
+    @include radius;
+    padding: 0.5em;
+    background-color: $off-white;
+  }
+  .editions-list-title{
+    @include display-flex(row, center, center);
+  }
+  .no-edition-wrapper{
+    @include display-flex(row, center, center);
+    color: $grey;
+    margin-top: 1em;
+  }
+</style>
--- app/modules/entities/components/layouts/work.svelte
+++ app/modules/entities/components/layouts/work.svelte
@@ -3,6 +3,7 @@
   import { isNonEmptyArray } from '#lib/boolean_tests'
   import { I18n } from '#user/lib/i18n'
   import { getSubEntities } from '../lib/entities'
+  import { getEntitiesAttributesByUris } from '#entities/lib/entities'
   import { getWorkProperties } from '#entities/components/lib/claims_helpers'
   import BaseLayout from './base_layout.svelte'
   import AuthorsInfo from './authors_info.svelte'
@@ -10,13 +11,20 @@
   import WikipediaExtract from './wikipedia_extract.svelte'
   import Ebooks from './ebooks.svelte'
   import ItemsLists from './items_lists.svelte'
+  import EditionsList from './editions_list.svelte'
 
-  export let entity, standalone, mapToShow
+  export let entity, standalone
+
+  let showMap
 
   const omitAuthorsProperties = true
   const { uri } = entity
   let editionsUris
+  let editions = []
   let initialEditions = []
+  const userLang = app.user.lang
+  let publishersByUris
+  let usersSize = 0
 
   const workShortlist = [
     'wdt:P577',
@@ -24,12 +32,35 @@
     'wdt:P921',
   ]
 
-  const getEditions = async () => {
+  const getEditionsWithPublishers = async () => {
     initialEditions = await getSubEntities('work', uri)
+    const publishersUris = getPublishersUrisFromEditions(initialEditions)
+    const { entities } = await getEntitiesAttributesByUris({
+      uris: publishersUris,
+      attributes: [ 'labels' ],
+      lang: userLang
+    })
+    publishersByUris = entities
     editions = initialEditions
   }
 
-  let editions = getEditions()
+  let editionsWithPublishers = getEditionsWithPublishers()
+  const getPublishersUrisFromEditions = editions => {
+    return _.uniq(_.compact(_.flatten(editions.map(edition => {
+      return findFirstClaimValue(edition, 'wdt:P123')
+    }))))
+  }
+
+  const findFirstClaimValue = (entity, prop) => {
+    const values = entity?.claims[prop]
+    if (!values || !values[0]) return
+    return values[0]
+  }
+
+  let editionsList, windowScrollY
+  const scrollToItemsList = () => {
+    if (editionsList) { windowScrollY = editionsList.offsetTop }
+  }
 
   $: claims = entity.claims
   $: notOnlyP31 = Object.keys(claims).length > 1
@@ -38,8 +69,10 @@
     editionsUris = editions.map(_.property('uri'))
   }
   $: someEditions = editions && isNonEmptyArray(editions)
+  $: someInitialEditions = initialEditions && isNonEmptyArray(initialEditions)
 </script>
 
+<svelte:window bind:scrollY={windowScrollY} />
 <BaseLayout
   bind:entity={entity}
   {standalone}
@@ -61,11 +94,35 @@
           />
           <Ebooks
             {entity}
+            {userLang}
           />
         </div>
       {/if}
+      <div
+        class="editions-section-wrapper"
+        class:no-edition={!someEditions}
+      >
+        {#await editionsWithPublishers}
+          <div class="loading-wrapper">
+            <p class="loading">{I18n('looking for editions...')}
+              <Spinner/>
+            </p>
+          </div>
+        {:then}
+          <EditionsList
+            {someInitialEditions}
+            {someEditions}
+            bind:usersSize={usersSize}
+            {publishersByUris}
+            {initialEditions}
+            bind:editions={editions}
+            on:showMap={() => showMap = true}
+            on:scrollToItemsList={scrollToItemsList}
+          />
+        {/await}
+      </div>
     </div>
-    {#await editions}
+    {#await editionsWithPublishers}
       <div class="loading-wrapper">
         <p class="loading">{I18n('looking for editions...')}
           <Spinner/>
@@ -75,11 +132,14 @@
       {#if someEditions}
         <div
           class="editions-list"
+          bind:this={editionsList}
         >
           <!-- TODO: dont display items list if items owners are only main user items -->
           <ItemsLists
             {editionsUris}
-            bind:mapToShow={mapToShow}
+            bind:showMap={showMap}
+            bind:usersSize={usersSize}
+            on:scrollToItemsList={scrollToItemsList}
           />
         </div>
       {/if}
@@ -93,6 +153,14 @@
   .entity-layout{
     @include display-flex(column, center);
     width: 100%;
+  }
+  .editions-section-wrapper{
+    flex: 1 0 0;
+    max-width: 50%;
+  }
+  .no-edition{
+    flex: none;
+    width: 15em;
   }
   .top-section{
     @include display-flex(row, flex-start, center);
