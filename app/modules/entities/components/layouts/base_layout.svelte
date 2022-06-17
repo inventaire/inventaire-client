1c7b5c7abc2397dc734f014df9158403bf6e1add
jums <jums@inventaire.io>
2022-06-17 15:45:59 +0200

    add entities data section in footer

--- app/modules/entities/components/layouts/base_layout.svelte
+++ app/modules/entities/components/layouts/base_layout.svelte
@@ -1,4 +1,6 @@
 <script>
+  import { I18n } from '#user/lib/i18n'
+  import { buildAltUri } from '../lib/entities'
   import EditDataActions from './edit_data_actions.svelte'
   import EntityHeader from '../entity_header.svelte'
   import preq from '#lib/preq'
@@ -6,10 +8,14 @@
   export let entity
   export let standalone
 
+  const { uri, _id, type } = entity
+
   const refreshEntity = async () => {
-    const { entities } = await preq.get(app.API.entities.getByUris(entity.uri, true))
+    const { entities } = await preq.get(app.API.entities.getByUris(uri, true))
     entity = Object.values(entities)[0]
   }
+
+  const altUri = buildAltUri(uri, _id)
 </script>
 
 <div class="layout">
@@ -26,6 +32,15 @@
   </div>
   <div class="entity-wrapper">
     <slot name="entity" />
+  </div>
+  <div class="entity-data-wrapper">
+    <p class="uri">
+      {I18n(type)}
+      - {uri}
+      {#if altUri}
+         - {altUri}
+      {/if}
+    </p>
   </div>
 </div>
 
@@ -53,6 +68,9 @@
   .entity-wrapper{
     @include display-flex(column, flex-start);
     margin-bottom: 2em;
+  }
+  .entity-data-wrapper{
+    @include display-flex(column, center);
   }
   /*Very Small screens*/
   @media screen and (max-width: $very-small-screen) {
