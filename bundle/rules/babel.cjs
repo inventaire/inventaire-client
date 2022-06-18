b669473ddb13f841bfb2dc004444612fc35c4acb
maxlath <k@maxlath.eu>
2022-06-18 20:04:37 +0200

    dependencies: update babel

--- bundle/rules/babel.cjs
+++ bundle/rules/babel.cjs
@@ -5,6 +5,7 @@ module.exports = {
     // "export 'default' (imported as '_') was not found in 'underscore' (possible exports: )"
     exclude: /node_modules/,
     presets: [ '@babel/preset-env' ],
+    // Requires @babel/runtime
     // See https://github.com/babel/babel-loader#babel-is-injecting-helpers-into-each-file-and-bloating-my-code
     plugins: [ '@babel/plugin-transform-runtime' ],
     // See https://github.com/babel/babel-loader#babel-loader-is-slow
