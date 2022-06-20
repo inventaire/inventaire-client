d9b4a966477645beefe4fe276ab5daeddfcd45c0
maxlath <k@maxlath.eu>
2022-06-20 17:25:20 +0200

    bundle: run babel on svelte-generated js code
    See https://github.com/sveltejs/svelte-loader/issues/108

--- bundle/rules/babel.cjs
+++ bundle/rules/babel.cjs
@@ -1,9 +1,10 @@
 module.exports = {
   loader: 'babel-loader',
   options: {
-    // Required to avoid errors such as:
-    // "export 'default' (imported as '_') was not found in 'underscore' (possible exports: )"
-    exclude: /node_modules/,
+    // Only transpile modules that need it
+    // See https://stackoverflow.com/questions/54156617/why-would-we-exclude-node-modules-when-using-babel-loader
+    // and https://github.com/babel/babel-loader/issues/171
+    exclude: /node_modules\/(?!(svelte))/,
     presets: [ '@babel/preset-env' ],
     // Requires @babel/runtime
     // See https://github.com/babel/babel-loader#babel-is-injecting-helpers-into-each-file-and-bloating-my-code
--- bundle/rules/svelte.cjs
+++ bundle/rules/svelte.cjs
@@ -6,6 +6,16 @@ module.exports = mode => {
   const prod = mode === 'production'
   const dev = !prod
 
+  const svelteFilesRule = {
+    test: /\.svelte$/,
+    type: 'javascript/auto',
+    use: [],
+  }
+
+  if (prod) {
+    svelteFilesRule.use.push(require('./babel.cjs'))
+  }
+
   const preprocess = {
     style: sass({
       importer: [
@@ -17,25 +27,21 @@ module.exports = mode => {
     preprocess.script = reactivePreprocess().script
   }
 
-  return [
-    {
-      test: /\.svelte$/,
-      type: 'javascript/auto',
-      use: [
-        {
-          loader: 'svelte-loader',
-          options: {
-            compilerOptions: {
-              dev,
-            },
-            hotReload: dev,
-            // Only emit in production to get the page to reload on style change in development
-            emitCss: prod,
-            preprocess,
-          },
-        }
-      ],
+  svelteFilesRule.use.push({
+    loader: 'svelte-loader',
+    options: {
+      compilerOptions: {
+        dev,
+      },
+      hotReload: dev,
+      // Only emit in production to get the page to reload on style change in development
+      emitCss: prod,
+      preprocess,
     },
+  })
+
+  return [
+    svelteFilesRule,
     {
       // Required to prevent errors from Svelte on Webpack 5+
       // as recommended by https://github.com/sveltejs/svelte-loader#usage
