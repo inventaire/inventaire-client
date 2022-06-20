68afee2cba332da35a777cb158dc7fe04c2f1904
maxlath <k@maxlath.eu>
2022-06-20 17:23:00 +0200

    bundle: recover polyfills
    The previous assumption that they were automatically added by babel was wrong:
    the `useBuiltIns` parameter needs to be set and core-js is required

--- bundle/rules/babel.cjs
+++ bundle/rules/babel.cjs
@@ -1,3 +1,10 @@
+// Deduce the core-js version to use from the version installed
+// to pickup new feature versions automatically
+const coreJsVersion = require('core-js/package.json').version.split('.').slice(0, 2).join('.')
+if (coreJsVersion.split('.')[0] !== '3') {
+  throw new Error('Expected core-js 3.x to be installed')
+}
+
 module.exports = {
   loader: 'babel-loader',
   options: {
@@ -5,7 +12,24 @@ module.exports = {
     // See https://stackoverflow.com/questions/54156617/why-would-we-exclude-node-modules-when-using-babel-loader
     // and https://github.com/babel/babel-loader/issues/171
     exclude: /node_modules\/(?!(svelte))/,
-    presets: [ '@babel/preset-env' ],
+    presets: [
+      [
+        // Relies on browserslist to determine how far back in ECMAscript versions babel should go
+        '@babel/preset-env',
+        {
+          corejs: { version: coreJsVersion },
+          // Add polyfills per module, based on usage
+          // See https://babeljs.io/docs/en/babel-preset-env#usebuiltins
+          useBuiltIns: 'usage',
+          // Allows to use features that are still at the proposal stage
+          // but already implemented by some browsers
+          // See https://babeljs.io/docs/en/babel-preset-env#shippedproposals
+          shippedProposals: true,
+          // See https://babeljs.io/docs/en/babel-preset-env#bugfixes
+          bugfixes: true,
+        }
+      ]
+    ],
     // Requires @babel/runtime
     // See https://github.com/babel/babel-loader#babel-is-injecting-helpers-into-each-file-and-bloating-my-code
     plugins: [ '@babel/plugin-transform-runtime' ],
