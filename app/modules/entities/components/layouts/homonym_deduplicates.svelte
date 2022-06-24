0d8cd91c213a62f07d7bc1254e4f8daf13f2219a
jums <jums@inventaire.io>
2022-06-24 14:30:45 +0200

    homonyms deduplicate: reuse EntityListElement

--- app/modules/entities/components/layouts/homonym_deduplicates.svelte
+++ app/modules/entities/components/layouts/homonym_deduplicates.svelte
@@ -1,37 +1,16 @@
 <script>
-  import getFavoriteLabel from '#entities/lib/get_favorite_label'
   import { getHomonymsEntities } from '#entities/lib/show_homonyms'
   import Spinner from '#general/components/spinner.svelte'
-  import { isNonEmptyArray } from '#lib/boolean_tests'
-  import Flash from '#lib/components/flash.svelte'
   import { icon } from '#lib/utils'
   import { I18n, i18n } from '#user/lib/i18n'
-  import HomonymDeduplicate from './homonym_deduplicate.svelte'
+  import EntityListElement from './entity_list_element.svelte'
 
   export let entity
 
-  let homonyms = [], flash
+  let homonyms = []
   const { hasDataadminAccess } = app.user
 
   const getHomonymsPromise = async () => homonyms = await getHomonymsEntities(entity)
-
-  const checkAll = checked => homonyms = homonyms.map(homonym => ({ ...homonym, checked }))
-
-  const mergeSelectedHomonyms = async () => {
-    flash = null
-    if (selectedHomonyms.length === 0) {
-      flash = {
-        type: 'warning',
-        message: I18n('nothing')
-      }
-    }
-
-    const homonymsPromises = selectedHomonyms.map(homonym => homonym.childEvents.merge())
-    // TODO: merge sequentially 3 at a time
-    await Promise.all(homonymsPromises)
-  }
-
-  $: selectedHomonyms = homonyms.filter(_.property('checked'))
 </script>
 {#if hasDataadminAccess}
   {#await getHomonymsPromise()}
@@ -46,43 +25,13 @@
         {@html icon('compress')}
         {I18n('merge homonyms')}
       </h3>
-      {#if isNonEmptyArray(homonyms)}
-        <div class="merge-homonyms-controls">
-          <button
-            class="grey-button"
-            on:click={() => checkAll(true)}
-            name={I18n('select all')}
-          >
-            {I18n('select all')}
-          </button>
-          <button
-            class="grey-button"
-            on:click={() => checkAll(false) }
-            name={I18n('unselect all')}
-          >
-            {I18n('unselect all')}
-          </button>
-          <button
-            class="grey-button"
-            disabled={selectedHomonyms.length === 0}
-            title={selectedHomonyms.length === 0 ? I18n('no homonym selected') : ''}
-            on:click={mergeSelectedHomonyms}
-          >
-            {@html icon('compress')}
-            {I18n('merge selected suggestions')}
-          </button>
-          <Flash bind:state={flash}/>
-        </div>
-      {:else}
-        <p>{i18n('no result')}</p>
-      {/if}
-  	  <div class='merge-homonyms'>
+  	  <div class='homonyms'>
         {#each homonyms as homonym, i}
           {#if !homonym.merged}
-            <HomonymDeduplicate
-              bind:homonym={homonym}
-              {entity}
-              bind:this={homonyms[i].childEvents}
+            <EntityListElement
+              entity={homonym}
+              actionType={'merge'}
+              parentEntity={entity}
             />
           {/if}
         {:else}
@@ -100,11 +49,12 @@
     padding: 1em;
     margin: 1em 0;
   }
-  .merge-homonyms-controls{
-    padding: 1em;
-  }
-  .merge-homonyms{
-    @include display-flex(row, center, center, wrap);
+  .homonyms{
+    @include display-flex(column, center, space-between);
+    :global(.entity-list){
+      width: 100%;
+      max-width: 30em;
+    }
   }
   /*Large screens*/
   @media screen and (min-width: $small-screen) {
