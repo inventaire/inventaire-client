31b73c1fbcedf6357d5d5dfcad7b1d99bcfa60f0
maxlath <k@maxlath.eu>
2022-07-01 13:42:26 +0200

    refactor: extract edition layout functions

---
+++ app/modules/entities/components/lib/edition_layout_helpers.js
@@ -0,0 +1,10 @@
+import { aggregateWorksClaims, infoboxPropsLists } from '#entities/components/lib/claims_helpers'
+import { isNonEmptyArray } from '#lib/boolean_tests'
+
+export const addWorksClaims = (claims, works) => {
+  const worksClaims = aggregateWorksClaims(works)
+  const nonEmptyWorksClaims = _.pick(worksClaims, isNonEmptyArray)
+  return Object.assign(claims, nonEmptyWorksClaims)
+}
+
+export const filterClaims = (_, key) => infoboxPropsLists.edition.long.includes(key)
