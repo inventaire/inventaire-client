49248eb172fb78fdc3a6d017988f92934cccb419
maxlath <k@maxlath.eu>
2022-07-11 17:39:43 +0200

    select_shelves: refactor with checkbox bind:group
    - fixes the perf issue: there was a 1s delay between clicking a shelf
      and that shelf being displayed as selected, for some unknown reason
    - improving a11y with the use of fieldset, legend, and labels

--- app/modules/inventory/components/importer/select_shelves.svelte
+++ app/modules/inventory/components/importer/select_shelves.svelte
@@ -1,48 +1,61 @@
 <script>
   import { I18n } from '#user/lib/i18n'
-  import _ from 'underscore'
   import app from '#app/app'
   import Spinner from '#components/spinner.svelte'
   import { getShelvesByOwner } from '#shelves/lib/shelves'
-  import SelectShelf from '#inventory/components/importer/select_shelf.svelte'
+  import ShelfInfo from '#inventory/components/importer/select_info.svelte'
 
   export let shelvesIds
   let userShelves = []
 
   const waitForShelves = getShelvesByOwner(app.user.id)
     .then(res => userShelves = res)
-
-  $: shelvesIds = _.compact(userShelves.map(shelf => {
-    if (shelf.checked) return shelf._id
-  }))
 </script>
-{#await waitForShelves}
-  <label for="shelves-selector">
-    <p class="title">{I18n('shelves')}</p>
-    <p class="description">{I18n('shelves_importer_description')}</p>
-  </label>
-  <Spinner/>
-{:then}
-  {#if userShelves.length > 0}
-    <label for="shelves-selector">
-      <p class="title">{I18n('shelves')}</p>
-      <p class="description">{I18n('shelves_importer_description')}</p>
-      <!-- i18n suggestion: "shelves_importer_description": "Add the selected books to your shelf(ves)" -->
-    </label>
-    <ul id="shelves-selector" role="menu">
-      {#each userShelves as shelf}
-        <SelectShelf bind:shelf/>
-      {/each}
-    </ul>
-  {/if}
-{/await}
-<style>
+
+<div class="shelves-selector">
+  {#await waitForShelves}
+    <Spinner/><span class="title">{I18n('shelves')}</span>
+  {:then}
+    {#if userShelves.length > 0}
+      <fieldset>
+        <legend>
+          <p class="title">{I18n('shelves')}</p>
+          <p class="description">{I18n('shelves_importer_description')}</p>
+          <!-- i18n suggestion: "shelves_importer_description": "Add the selected books to your shelf(ves)" -->
+        </legend>
+        {#each userShelves as shelf}
+          <label title={I18n('select_shelf')}>
+            <input type="checkbox" bind:group={shelvesIds} value={shelf._id}>
+            <ShelfInfo bind:shelf />
+          </label>
+        {/each}
+      </fieldset>
+    {/if}
+  {/await}
+</div>
+
+<style lang="scss">
+  @import '#general/scss/utils';
   .description{
     font-size: 0.9rem;
     margin-bottom: 0.5em;
   }
-  #shelves-selector{
+  legend{
     max-height: 15em;
     overflow: auto;
+  }
+  .shelves-selector{
+    margin-top: 1em;
+  }
+  label{
+    color: $default-text-color;
+    @include bg-hover($off-white, 5%);
+    @include display-flex(row, center, flex-start);
+    @include radius;
+    cursor: pointer;
+    margin-bottom: 0.1em;
+  }
+  input[type="checkbox"]{
+    margin: 0.5em;
   }
 </style>
