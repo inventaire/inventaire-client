4bc964780b7c5b07585bbdfe36c1a196084d1b7c
maxlath <k@maxlath.eu>
2022-07-11 17:39:43 +0200

    select_shelves: refactor with checkbox bind:group
    - fixes the perf issue: there was a 1s delay between clicking a shelf
      and that shelf being displayed as selected, for some unknown reason
    - improving a11y with the use of fieldset, legend, and labels

---
+++ app/modules/inventory/components/importer/select_info.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { imgSrc } from '#lib/handlebars_helpers/images'
+  import { getColorSquareDataUri, getColorHexCodeFromModelId } from '#lib/images'
+
+  export let shelf
+  const shelfColor = getColorSquareDataUri(shelf.color || getColorHexCodeFromModelId(shelf._id))
+</script>
+
+<div class="shelf-info">
+  <img class="shelf-picture" src="{imgSrc(shelfColor)}" alt='{shelf.name}'>
+  <div class="shelf-text">
+    <p>{shelf.name}</p>
+    <p>{shelf.description}</p>
+  </div>
+</div>
+
+<style lang="scss">
+  @import '#general/scss/utils';
+  .shelf-picture{
+    flex: 0 0 auto;
+    width: 48px;
+    @include radius;
+  };
+  .shelf-info{
+    @include display-flex(row, center, flex-start);
+  }
+  .shelf-text{
+    margin: 0 0.5em;
+  }
+</style>
