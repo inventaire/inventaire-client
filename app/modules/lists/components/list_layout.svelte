65c50a7c62b6dbbe51ae884b5e0a04f2c368cb31
jums <jums@inventaire.io>
2022-07-17 07:43:45 +0200

    add List layout header, footer and router

---
+++ app/modules/lists/components/list_layout.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { I18n } from '#user/lib/i18n'
+  export let id
+</script>
+<div class="header">
+  <p>{I18n('list')}</p>
+</div>
+<div class="footer">
+  <p class="list-id">
+    {I18n('list')}
+      -
+    {id}
+  </p>
+</div>
+<style lang="scss">
+  @import '#general/scss/utils';
+  .header{
+    @include display-flex(column, center, center);
+    display: flex;
+    margin: 1em 0;
+    width: 100%;
+  }
+  .list-id{
+    @include display-flex(column, center);
+    font-size: small;
+  }
+ }
+</style>
