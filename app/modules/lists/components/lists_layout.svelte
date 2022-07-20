c46e76c2357f56f70115e8d09ce878e9710581f7
jums <jums@inventaire.io>
2022-07-20 08:35:15 +0200

    wip: pagination

--- app/modules/lists/components/lists_layout.svelte
+++ app/modules/lists/components/lists_layout.svelte
@@ -1,6 +1,8 @@
 <script>
   import { I18n } from '#user/lib/i18n'
   import { icon } from '#lib/utils'
+  import { isNonEmptyArray } from '#lib/boolean_tests'
+  import screen_ from '#lib/screen'
   import Spinner from '#general/components/spinner.svelte'
   import { getEntitiesByUris } from '#entities/lib/entities'
   import ListSelectionsCandidate from './list_selections_candidate.svelte'
@@ -11,14 +13,25 @@
   const listings = app.user.listings()
   let visibilityData = listings[visibility]
   let isEditable = creator === app.user.id
-  let entities
+  let entities = []
 
-  const getSelectionsEntities = async () => {
+  const paginationSize = 5
+  let offset = 5
+  let fetching
+  // fake
+  let scrollY = 0
+
+  const getSelectionsEntities = async selections => {
     const uris = selections.map(_.property('uri'))
-    entities = await getEntitiesByUris(uris)
+    return getEntitiesByUris(uris)
   }
 
-  const waitingForEntities = getSelectionsEntities()
+  const assignFirstSelectionsAsEntities = async () => {
+    const firstSelections = selections.slice(0, paginationSize)
+    entities = await getSelectionsEntities(firstSelections)
+  }
+
+  const waitingForEntities = assignFirstSelectionsAsEntities()
 
   const showEditor = async () => {
     const { default: ListEditor } = await import('#lists/components/list_editor.svelte')
@@ -33,6 +46,24 @@
     })
   }
 
+  const fetchMore = async () => {
+    if (fetching || hasMore === false) return
+    fetching = true
+    const nextBatchSelections = selections.slice(offset, paginationSize)
+    const nextEntities = await getSelectionsEntities(nextBatchSelections)
+    if (isNonEmptyArray(nextEntities)) {
+      offset += paginationSize
+      entities = [ entities, ...nextEntities ]
+    }
+    fetching = false
+  }
+
+  $: {
+    if (screen_.height() >= scrollY) {
+      fetchMore()
+    }
+  }
+  $: hasMore = entities.length > offset
   $: name = list.name
   $: description = list.description
 </script>
