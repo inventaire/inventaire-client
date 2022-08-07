feaa5d3e347eec3e45fd8f28be27d683b2fd6f17
maxlath <k@maxlath.eu>
2022-08-07 23:23:50 +0200

    list_layout: extract info box to list_info_box

--- app/modules/lists/components/list_layout.svelte
+++ app/modules/lists/components/list_layout.svelte
@@ -1,14 +1,14 @@
 <script>
   import { I18n } from '#user/lib/i18n'
-  import { icon } from '#lib/utils'
   import { isNonEmptyArray } from '#lib/boolean_tests'
   import screen_ from '#lib/screen'
   import Spinner from '#general/components/spinner.svelte'
   import { getEntitiesByUris } from '#entities/lib/entities'
   import ListSelections from './list_selections.svelte'
+  import ListInfoBox from '#modules/lists/components/list_info_box.svelte'
   export let list, selections
 
-  let { _id, description, creator } = list
+  let { _id, creator } = list
 
   // TODO: rebase and fix visibility once items/shelves visibility branches have been merged
   // let { visibility } = list
@@ -36,19 +36,6 @@
 
   const waitingForEntities = assignFirstSelectionsAsEntities()
 
-  const showEditor = async () => {
-    const { default: ListEditor } = await import('#lists/components/list_editor.svelte')
-    app.execute('modal:open')
-    const component = app.layout.showChildComponent('modal', ListEditor, {
-      props: {
-        list,
-      }
-    })
-    component.$on('listUpdated', event => {
-      list = event.detail.list
-    })
-  }
-
   const fetchMore = async () => {
     if (fetching || hasMore === false) return
     fetching = true
@@ -67,36 +54,21 @@
     }
   }
   $: hasMore = entities.length > offset
-  $: name = list.name
-  $: description = list.description
 </script>
-{#await waitingForEntities}
-  <p class="loading">Loading... <Spinner/></p>
-{:then}
-  <div class="header">
-	  <!-- <p>{I18n('list')}</p> -->
-	  <h3>{name}</h3>
-	  <p class="visibility">
-      <!-- {@html icon(visibilityData.icon)} {visibilityData.label} -->
-	  </p>
-	  <p>{description}</p>
-    {#if isEditable}
-      <button
-        id="showListEditor"
-        class="tiny-button"
-        on:click={showEditor}
-      >
-        {@html icon('pencil')}
-        {I18n('edit info')}
-      </button>
-    {/if}
+
+<div class="list-layout">
+  <ListInfoBox {list} />
+  {#await waitingForEntities}
+    <Spinner center={true} />
+  {:then}
     <ListSelections
       bind:entities
       listId={_id}
       {isEditable}
     />
-  </div>
-{/await}
+  {/await}
+</div>
+
 <div class="footer">
   <p class="list-id">
     {I18n('list')}
@@ -104,25 +76,14 @@
     {_id}
   </p>
 </div>
+
 <style lang="scss">
   @import '#general/scss/utils';
- .header{
-   @include display-flex(column, center, center);
-   display: flex;
-   margin: 1em 0;
-   width: 100%;
- }
- .visibility{
- 	color: #666;
+ .list-layout{
+   @include display-flex(column, center);
  }
  .list-id{
  	@include display-flex(column, center);
  	font-size: small;
- }
- #showListEditor{
-  margin: 1em 0;
-  padding: 0.3em;
-  white-space: nowrap;
-  width: 10em;
  }
 </style>
