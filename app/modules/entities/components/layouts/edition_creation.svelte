3d224099e258fe54316450ab00f68f867a89917a
maxlath <k@maxlath.eu>
2022-08-09 16:39:43 +0200

    edition_creation: harmonize buttons sizes and remove obsolete selector rules

--- app/modules/entities/components/layouts/edition_creation.svelte
+++ app/modules/entities/components/layouts/edition_creation.svelte
@@ -31,17 +31,16 @@
     if (key === 'esc') showForm = false
   }
 </script>
-<div class="edition-creation">
-  {#if showForm}
-    <div class="from-isbn">
-      <label for="isbn-group">
-      	{i18n('Add an edition by its ISBN')}
-      </label>
-      <form id="isbn-group" class="input-group">
-        <div class="input-box">
+<div class="wrapper">
+  <div class="edition-creation">
+    {#if showForm}
+      <div class="from-isbn">
+        <label for="isbn-group">
+          {i18n('Add an edition by its ISBN')}
+        </label>
+        <form class="isbn-group">
           <input
             type="text"
-            id="isbnField"
             name="isbn"
             class="has-alertbox enterClick"
             bind:value={userInput}
@@ -50,93 +49,69 @@
             aria-label="isbn"
             use:autofocus
           >
-        </div>
-        <button
-          id="isbnButton"
-          class="button grey postfix sans-serif"
-          on:click={onIsbnButtonClick}
+          <button
+            class="isbn-button tiny-button grey"
+            on:click={onIsbnButtonClick}
+          >
+            {@html icon('plus')}
+            {i18n('add')}
+          </button>
+        </form>
+      </div>
+      <a
+        href={addWithoutIsbnPath(work)}
+        class="without-isbn tiny-button grey"
+        on:click={loadInternalLink}
         >
-          {@html icon('plus')}
-          {i18n('add')}
-        </button>
-      </form>
-    </div>
-    <a
-      href={addWithoutIsbnPath(work)}
-      class="without-isbn tiny-button grey"
-      on:click={loadInternalLink}
+        {@html icon('plus')}
+        {i18n('add an edition without an ISBN')}
+      </a>
+    {:else}
+      <button
+        class="tiny-button show-form"
+        on:click={() => showForm = true}
       >
-      {@html icon('plus')}
-      {i18n('add an edition without an ISBN')}
-    </a>
-  {:else}
-    <button
-      class="tiny-button show-form"
-      on:click={() => showForm = true}
-    >
-      {@html icon('plus')}
-      {i18n('add missing edition')}
-    </button>
-  {/if}
+        {@html icon('plus')}
+        {i18n('add a missing edition')}
+      </button>
+    {/if}
+  </div>
   <Flash bind:state={flash}/>
 </div>
 <style lang="scss">
   @import '#general/scss/utils';
-  .edition-creation{
-    @include display-flex(row, flex-start, flex-start, wrap);
-    @include radius;
-    flex-direction: column;
-    align-items: stretch;
-    max-width: 20em;
-    padding: 0.5em;
-  }
   .wrapper{
-    @include display-flex(row, center, center);
+    border-top: 1px solid #ddd;
+    padding: 1em 0.5em 0.5em 0.5em;
+    margin-top: 0.5em;
+    align-self: stretch;
   }
-  #isbnButton{
-    @include display-flex(row, center, center);
+  .edition-creation{
+    @include display-flex(column, stretch, center);
+    @include radius;
+    max-width: 20em;
+    margin: 0 auto;
+  }
+  .isbn-group{
+    @include display-flex(row);
+    input{
+      @include radius-right(0);
+    }
+    .tiny-button{
+      @include radius-left(0);
+    }
+    margin-bottom: 0.5em;
+  }
+  .isbn-button{
     white-space: nowrap;
   }
   .without-isbn{
-    @include display-flex(row, center, center);
-    font-size: 0.9em;
     padding: 0.5em;
+    line-height: 1rem;
+    text-align: center;
   }
   .show-form{
     padding: 0.5em;
     max-width: 20em;
-    font-size: 0.9em;
-  }
-  .input-group{
-    width: 100%;
-    @include display-flex(row, left, flex-start);
-    margin-bottom: 0.5em;
-    button.postfix{
-      height: 2.2rem;
-      white-space: nowrap;
-    }
-  }
-  .input-box{
-    flex: 1 1 auto;
-    min-width: 60%;
-    margin-right: 0.4em;
-    input{
-      height: 2.2rem;
-      padding: 0 0.5em;
-      margin-bottom: 0;
-    }
-  }
-
-  /*Small screens*/
-  @media screen and (max-width: $very-small-screen) {
-    .input-group{
-      flex-direction: column;
-      button{
-        width: 100%;
-      }
-    }
-    .input-box{
-      width: 100%;
-    }
   }
 </style>
