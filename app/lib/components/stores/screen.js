6eb55fe8de908228dba73a78c3daed376509aca2
maxlath <k@maxlath.eu>
2022-09-02 22:59:00 +0200

    screen store: replace window.screen by window.visualViewport

--- app/lib/components/stores/screen.js
+++ app/lib/components/stores/screen.js
@@ -2,15 +2,15 @@ import { readable } from 'svelte/store'
 
 const getStoreValue = () => {
   return {
-    width: window.screen.width,
-    height: window.screen.height,
+    width: window.visualViewport.width,
+    height: window.visualViewport.height,
     isSmallerThan,
     isLargerThan,
   }
 }
 
-const isSmallerThan = maxWidth => window.screen.width < maxWidth
-const isLargerThan = maxWidth => window.screen.width > maxWidth
+const isSmallerThan = maxWidth => window.visualViewport.width < maxWidth
+const isLargerThan = maxWidth => window.visualViewport.width > maxWidth
 
 export const screen = readable(getStoreValue(), set => {
   const update = () => set(getStoreValue())
