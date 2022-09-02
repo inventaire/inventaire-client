1b1ba32e0830010b403d1fd390798cb3eb4add16
maxlath <k@maxlath.eu>
2022-09-02 09:37:37 +0200

    user_groups_store: filter-out groups that the main user requested to join

--- app/modules/user/user_groups_store.js
+++ app/modules/user/user_groups_store.js
@@ -7,23 +7,27 @@ export const userGroups = readable([], start)
 // The start function can not be async as it is supposed to return a stop function
 // and not a promise (see https://github.com/sveltejs/svelte/issues/4765#issuecomment-623092644=)
 function start (setStoreValue) {
+  const collection = app.groups.mainUserMember
+
   const update = async () => {
     await app.request('wait:for', 'groups')
-    const updateStoreValue = app.groups.map(model => model.toJSON())
+    const updateStoreValue = collection.map(model => model.toJSON())
     setStoreValue(updateStoreValue)
   }
 
-  app.groups.on('add', update)
-  app.groups.on('remove', update)
+  collection.on('add', update)
+  collection.on('remove', update)
 
   // Call immediately, in case app.groups was populated
   // before this start was called
   update()
 
   const stop = () => {
-    app.groups.off('add', update)
-    app.groups.off('remove', update)
+    collection.off('add', update)
+    collection.off('remove', update)
   }
 
   return stop
 }
+
+window.userGroups = userGroups
