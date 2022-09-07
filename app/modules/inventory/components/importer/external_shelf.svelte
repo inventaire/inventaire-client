66d27932ecd2e8ee3b81ad69b436123a9ac8cc19
maxlath <k@maxlath.eu>
2022-09-07 21:12:14 +0200

    replaces tabs by spaces

--- app/modules/inventory/components/importer/external_shelf.svelte
+++ app/modules/inventory/components/importer/external_shelf.svelte
@@ -7,16 +7,16 @@
   $: checked = externalShelf.checked
 </script>
 <li class='external-shelf' on:click={toggleCheckbox}>
-	<input type="checkbox" bind:checked name="{I18n('external_shelf')}">
-	<p>{externalShelf.name}</p>
+  <input type="checkbox" bind:checked name="{I18n('external_shelf')}">
+  <p>{externalShelf.name}</p>
 </li>
 <style lang="scss">
   @import '#general/scss/utils';
   .external-shelf{
-	  @include display-flex(row, center);
-  	cursor: pointer;
+    @include display-flex(row, center);
+    cursor: pointer;
   }
   input{
-  	margin-right: 0.5em;
+    margin-right: 0.5em;
   }
 </style>
