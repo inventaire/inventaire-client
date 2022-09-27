4429a6171aec0707f89a05924f53ef78651b212a
jums <jums@inventaire.io>
2022-09-27 20:51:19 +0200

    edition action: move an i18n key to shortkey
    untranslated key yet, fortunately

--- app/modules/entities/components/lib/edition_action_helpers.js
+++ app/modules/entities/components/lib/edition_action_helpers.js
@@ -1,6 +1,6 @@
 import { I18n } from '#user/lib/i18n'
 
-export const getCounterText = editionItems => I18n('users have this book', { users_size: getUsersSizePerEdition(editionItems) })
+export const getCounterText = editionItems => I18n('users_count_have_this_book', { smart_count: getUsersSizePerEdition(editionItems) })
 
 const getUsersSizePerEdition = editionItems => {
   const notMainUserOwners = editionItems.filter(notMainUserOwner)
