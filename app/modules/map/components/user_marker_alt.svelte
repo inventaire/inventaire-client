323a8a4ea462ea2e25686fa3ee59b1b77c049f33
maxlath <k@maxlath.eu>
2022-11-12 18:37:20 +0100

    public_users_nav: refactor map with a more Svelte-idiomatic approach
    inspired by https://imfeld.dev/writing/svelte_domless_components
    and https://github.com/ngyewch/svelte-leaflet

---
+++ app/modules/map/components/user_marker_alt.svelte
@@ -0,0 +1,27 @@
+<!-- TODO: merge with #map/components/user_marker.svelte -->
+<script>
+  import { I18n } from '#user/lib/i18n'
+  import { imgSrc } from '#lib/handlebars_helpers/images'
+  import { SelectInventoryOnClick } from '#users/components/lib/navs_helpers'
+
+  export let doc
+  const { username, picture } = doc
+  const pathname = `/users/${username}`
+
+  const onClick = SelectInventoryOnClick({ type: 'user', doc })
+</script>
+
+<a
+  href={pathname}
+  on:click={onClick}
+  title="{username} - {I18n('user')}"
+  class="objectMarker userMarker"
+>
+  <div
+    class="picture"
+    style="background-image: url({imgSrc(picture, 100)})"
+  />
+  <p class="username label">
+    {username}
+  </p>
+</a>
