3f575be3032b318687bf59fcfaf3db31613a1e9f
maxlath <k@maxlath.eu>
2022-11-12 18:37:20 +0100

    public_users_nav: refactor map with a more Svelte-idiomatic approach
    inspired by https://imfeld.dev/writing/svelte_domless_components
    and https://github.com/ngyewch/svelte-leaflet

---
+++ app/modules/map/components/group_marker.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { I18n } from '#user/lib/i18n'
+  import { icon } from '#lib/utils'
+  import { imgSrc } from '#lib/handlebars_helpers/images'
+  import { getGroupMembersCount, getGroupPathname, getGroupPicture } from '#groups/lib/groups'
+  import { SelectInventoryOnClick } from '#users/components/lib/navs_helpers'
+
+  export let doc
+  const { name } = doc
+  const picture = getGroupPicture(doc)
+  const pathname = getGroupPathname(doc)
+  const membersCount = getGroupMembersCount(doc)
+  const onClick = SelectInventoryOnClick({ type: 'group', doc })
+</script>
+
+<a
+  href={pathname}
+  on:click={onClick}
+  title="{name} - {I18n('group')}"
+  class="objectMarker groupMarker"
+>
+  <div class="picture" style="background-image: url({imgSrc(picture, 100)})"></div>
+  <p class="name label">{name}</p>
+  <div class="members-count">{@html icon('group')} {membersCount}</div>
+</a>
---
+++ app/modules/map/components/leaflet_map.svelte
@@ -0,0 +1,88 @@
+<!-- Inspired by https://imfeld.dev/writing/svelte_domless_components
+     and https://github.com/ngyewch/svelte-leaflet -->
+<script>
+  import { createEventDispatcher, setContext } from 'svelte'
+  import L from 'leaflet'
+  import 'leaflet/dist/leaflet.css'
+  import 'leaflet.markercluster/dist/MarkerCluster.css'
+  import 'leaflet.markercluster/dist/MarkerCluster.Default.css'
+  // Needs to be initialized after window.L was set
+  import 'leaflet.markercluster'
+  // TODO: split markers style between components
+  import '#map/scss/objects_markers.scss'
+  import mapConfig from '#map/lib/config.js'
+  import isMobile from '#lib/mobile_check'
+
+  mapConfig.init()
+
+  // Must set either bounds, or view and zoom.
+  export let bounds
+  export let view
+  export let zoom = 13
+  export let cluster = false
+
+  export let map
+
+  let clusterGroup
+
+  const dispatch = createEventDispatcher()
+
+  setContext('map', () => map)
+  setContext('layer', () => clusterGroup || map)
+
+  function createLeaflet (node) {
+    map = L.map(node)
+      .on('zoom', e => dispatch('zoom', e))
+      .on('moveend', e => dispatch('moveend', e))
+
+    if (bounds) {
+      map.fitBounds(bounds)
+    } else {
+      map.setView(view, zoom)
+    }
+
+    L.tileLayer(mapConfig.tileUrl, mapConfig.settings)
+    .addTo(map)
+
+    if (isMobile) map.scrollWheelZoom.disable()
+
+    if (cluster) {
+      clusterGroup = L.markerClusterGroup()
+      map.addLayer(clusterGroup)
+    }
+
+    return {
+      destroy () {
+        map.remove()
+        map = null
+      },
+    }
+  }
+
+  $: if (map) {
+    if (bounds) {
+      map.fitBounds(bounds)
+    } else {
+      map.setView(view, zoom)
+    }
+  }
+</script>
+
+<div use:createLeaflet>
+  {#if map}
+    <slot {map} />
+  {/if}
+</div>
+
+<style>
+  div{
+    height: 100%;
+    width: 100%;
+  }
+  :global(.leaflet-control-container){
+    position: static;
+  }
+  :global(.leaflet-marker-icon){
+    border: 0;
+  }
+</style>
