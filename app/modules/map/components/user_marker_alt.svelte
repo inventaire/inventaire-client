aafbe58c0a8688fc84715400b50681fccaabb02f
maxlath <k@maxlath.eu>
2022-11-17 18:27:53 +0100

    user_marker_alt: recover posibility to edit the position for the main user

--- app/modules/map/components/user_marker_alt.svelte
+++ app/modules/map/components/user_marker_alt.svelte
@@ -3,25 +3,37 @@
   import { I18n } from '#user/lib/i18n'
   import { imgSrc } from '#lib/handlebars_helpers/images'
   import { SelectInventoryOnClick } from '#users/components/lib/navs_helpers'
+  import { icon } from '#lib/handlebars_helpers/icons'
+  import { showMainUserPositionPicker } from '#map/lib/map'
 
   export let doc
-  const { username, picture } = doc
+  const { _id, username, picture } = doc
   const pathname = `/users/${username}`
+  const isMainUser = _id === app.user.id
 
   const onClick = SelectInventoryOnClick({ type: 'user', doc })
 </script>
 
-<a
-  href={pathname}
-  on:click={onClick}
-  title="{username} - {I18n('user')}"
-  class="objectMarker userMarker"
->
-  <div
-    class="picture"
-    style="background-image: url({imgSrc(picture, 100)})"
-  />
-  <p class="username label">
-    {username}
-  </p>
-</a>
+<div class="objectMarker userMarker">
+  <a
+    href={pathname}
+    on:click={onClick}
+    title="{username} - {I18n('user')}"
+  >
+    <div
+      class="picture"
+      style="background-image: url({imgSrc(picture, 100)})"
+    />
+    <p class="username label">
+      {username}
+    </p>
+  </a>
+  {#if isMainUser}
+    <button
+      id="showPositionPicker"
+      on:click={showMainUserPositionPicker}
+    >
+      {@html icon('pencil')}
+    </button>
+  {/if}
+</div>
