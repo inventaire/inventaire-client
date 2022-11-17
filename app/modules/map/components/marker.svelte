bd0d7812961c06cda17eb9a847d35c2528bd3ff7
maxlath <k@maxlath.eu>
2022-11-17 23:21:28 +0100

    public_users_nav: update main user marker position after position update

--- app/modules/map/components/marker.svelte
+++ app/modules/map/components/marker.svelte
@@ -1,31 +1,42 @@
 <script>
+  import { onChange } from '#lib/svelte/svelte'
   import L from 'leaflet'
-  import { getContext } from 'svelte'
+  import { getContext, onDestroy, onMount } from 'svelte'
 
   export let latLng
   const layer = getContext('layer')()
 
-  let marker
-  function createMarker (markerElement) {
+  let marker, markerElement
+
+  function createMarker () {
     let icon = L.divIcon({
       html: markerElement,
       // Let the children elements determine the size
       iconSize: [ 0, 0 ]
     })
     marker = L.marker(latLng, { icon }).addTo(layer)
-    return {
-      destroy () {
-        if (marker) {
-          marker.removeFrom(layer)
-          marker.remove()
-          marker = null
-        }
-      }
+  }
+
+  function updateMarker () {
+    if (marker) {
+      marker.setLatLng(latLng).update()
     }
   }
+
+  function destroyMarker () {
+    if (marker) {
+      marker.removeFrom(layer)
+      marker.remove()
+      marker = null
+    }
+  }
+
+  onMount(createMarker)
+  onDestroy(destroyMarker)
+  $: onChange(latLng, updateMarker)
 </script>
 
-<div use:createMarker>
+<div bind:this={markerElement}>
   {#if marker}
     <slot />
   {/if}
