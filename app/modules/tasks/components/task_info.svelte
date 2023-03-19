0c9e40c3769dc882633e2fe9a6966ce5e1a5ed5d
jums <jums@inventaire.io>
2023-03-19 14:42:53 +0100

    rewrite get by id and controls

---
+++ app/modules/tasks/components/task_info.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { i18n } from '#user/lib/i18n'
+
+  export let task
+
+  $: ({ reporter, clue } = task)
+</script>
+
+<li>
+  <strong>{i18n('reporter')}: </strong>
+  <a href="/users/{reporter}/contributions" class="link">{reporter}</a>
+
+</li>
+<li>
+  <strong>{i18n('clue')}: </strong>
+  <a href="/entity/isbn:{clue}" class="link">{clue}</a>
+</li>
