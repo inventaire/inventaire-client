778d0d7208617c477c8dc3047ba8204e77158e72
maxlath <k@maxlath.eu>
2023-05-24 11:03:16 +0200

    images_collage: remove null from imagesUrls array
    to prevent generating img elements with src=""

--- app/modules/general/components/images_collage.svelte
+++ app/modules/general/components/images_collage.svelte
@@ -1,8 +1,9 @@
 <script>
   import { imgSrc } from '#lib/handlebars_helpers/images'
+  import { compact } from 'underscore'
   export let imagesUrls, limit = 1, imageSize = 200
 
-  $: displayedImages = imagesUrls.slice(0, limit)
+  $: displayedImages = compact(imagesUrls).slice(0, limit)
 </script>
 
 <div
