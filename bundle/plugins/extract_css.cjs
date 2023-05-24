46d06ae8367af78c7876bfa7356551d603e77d01
maxlath <k@maxlath.eu>
2023-05-24 15:45:00 +0200

    webpack bundle: remove presumably unnecessary warnings
    Those "Conflicting order" warnings can be safely ignored in projects "where css ordering
    has been mitigated through consistent use of scoping or naming conventions", which should
    be our case with Svelte.
    
    Those warnings appeared in c6b864e71 due to the addition of a new dependency to works_browser_section.svelte:
      import WorkActions from '#entities/components/layouts/work_actions.svelte'
    
    Related issue: https://github.com/facebook/create-react-app/issues/5372#issuecomment-685932009

--- bundle/plugins/extract_css.cjs
+++ bundle/plugins/extract_css.cjs
@@ -3,4 +3,6 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 module.exports = new MiniCssExtractPlugin({
   filename: '[name].[contenthash:8].css',
   chunkFilename: '[name].[contenthash:8].css',
+  // See https://github.com/webpack-contrib/mini-css-extract-plugin#ignoreOrder
+  ignoreOrder: true,
 })
