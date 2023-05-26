68e323f133f0b2ff5015e4ed2dfe35ff282a4a64
maxlath <k@maxlath.eu>
2023-05-26 09:55:33 +0200

    edition_layout_helpers: lint

--- app/modules/entities/components/lib/edition_layout_helpers.js
+++ app/modules/entities/components/lib/edition_layout_helpers.js
@@ -1,12 +1,12 @@
 import { aggregateWorksClaims } from '#entities/components/lib/claims_helpers'
 import { isNonEmptyArray } from '#lib/boolean_tests'
+import { pick } from 'underscore'
 
 export const addWorksClaims = (claims, works) => {
   const worksClaims = aggregateWorksClaims(works)
-  const nonEmptyWorksClaims = _.pick(worksClaims, isNonEmptyArray)
+  const nonEmptyWorksClaims = pick(worksClaims, isNonEmptyArray)
   return Object.assign(claims, nonEmptyWorksClaims)
 }
 
-export const isSubentitiesTypeEdition = type => [
-  'collection',
-  'publisher' ].includes(type)
+const entitiesTypesWithEditionsSubentities = [ 'collection', 'publisher' ]
+export const isSubentitiesTypeEdition = type => entitiesTypesWithEditionsSubentities.includes(type)
