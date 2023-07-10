dab72eebccc752d62e50c4a790416ed915874b03
antalAkos <aztalos.akos@gmail.com>
2023-07-10 19:34:36 +0300

    basic result list for selection implemented

--- app/modules/map/components/location_search_input.svelte
+++ app/modules/map/components/location_search_input.svelte
@@ -1,63 +1,94 @@
 <script>
-    import getActionKey from "#lib/get_action_key";
+  import getActionKey from '#lib/get_action_key'
 
-    export let map;
+  export let map
 
-    let searchQuery = "";
+  let results
 
-    function searchLocation() {
-        fetch(
-            `https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(
-                searchQuery
-            )}&format=json`
-        )
-            .then((response) => response.json())
-            .then((data) => {
-                if (data.length > 0) {
-                    let mapZoom = 13;
-                    const result = data[0];
-                    const latLng = [
-                        parseFloat(result.lat),
-                        parseFloat(result.lon),
-                    ];
-                    map.setView(latLng, mapZoom);
-                    L.marker(latLng).addTo(map);
-                }
-            })
-            .catch((error) => {
-                console.error("Error searching location:", error);
-            });
-    }
+  let resultList
 
-    function searchKey(e) {
-        if (getActionKey(e) === "enter") {
-            searchLocation();
+  let searchQuery = ''
+
+  function searchLocation () {
+    fetch(
+      `https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(
+        searchQuery
+      )}&format=json`
+    )
+      .then(response => response.json())
+      .then(data => {
+        if (data.length > 0) {
+          results = data
         }
+      })
+      .catch(error => {
+        console.error('Error searching location:', error)
+      })
+  }
+
+  function setView (index) {
+    let mapZoom = 13
+    const latLng = [
+      parseFloat(results[index].lat),
+      parseFloat(results[index].lon),
+    ]
+    map.setView(latLng, mapZoom)
+    L.marker(latLng).addTo(map)
+    resultList.remove()
+  }
+
+  function searchKey (e) {
+    if (getActionKey(e) === 'enter') {
+      searchLocation()
     }
+  }
+
 </script>
 
 <div id="locationSearchInput">
-    <input
-        type="text"
-        bind:value={searchQuery}
-        on:keydown={searchKey}
-        placeholder="Search for a location"
-    />
-    <button on:click={searchLocation}>Search</button>
+  <input
+    type="text"
+    bind:value={searchQuery}
+    on:keydown={searchKey}
+    placeholder="Search for a location"
+  />
+  <button on:click={searchLocation}>Search</button>
+  {#if results}
+    <ul bind:this={resultList}>
+      {#each results as result, i}
+        <!-- {console.log(result)} -->
+        <li on:click={() => setView(i)} on:keypress={() => setView(i)}>{result.display_name}</li>
+      {/each}
+    </ul>
+  {/if}
 </div>
 
 <style>
-    #locationSearchInput {
-        position: absolute;
-        top: 0;
-        right: 0;
-        z-index: 1000;
-        padding: 10px;
+    #locationSearchInput{
+      position: absolute;
+      top: 0;
+      right: 0;
+      z-index: 1000;
+      padding: 10px;
+      width: 300px;
     }
 
-    #locationSearchInput button {
-        background-color: blue;
-        color: white;
-        font-weight: bold;
+    #locationSearchInput button{
+      background-color: blue;
+      color: white;
+      font-weight: bold;
+    }
+
+    #locationSearchInput ul{
+      background-color: aliceblue;
+      padding: 5px;
+    }
+
+    #locationSearchInput li{
+        padding-bottom: 5px;
+        border-bottom: 2px solid black;
+    }
+    #locationSearchInput li:hover{
+        cursor: pointer;
     }
 </style>
