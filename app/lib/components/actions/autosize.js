64e01c4e2336d459c0fbe44932c0c769832b0e5a
maxlath <k@maxlath.eu>
2023-08-25 18:05:51 +0200

    autosize: remove unnecessary call to autosize.udpate and document known short coming

--- app/lib/components/actions/autosize.js
+++ app/lib/components/actions/autosize.js
@@ -1,6 +1,9 @@
 import _autosize from 'autosize'
 
 export function autosize (node) {
+  // Automatically detects changes resulting from user input
+  // but it fails to detect changes triggered by JS setting the textarea value
   _autosize(node)
-  node.addEventListener('keyup', () => _autosize.update(node))
+  // Callin the update function doesn't seem to work
+  // node.addEventListener('change', () => _autosize.update(node))
 }
