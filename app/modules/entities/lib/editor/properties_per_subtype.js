bbd140a9381e875da256c7808642017fecf31b8a
maxlath <k@maxlath.eu>
2023-09-06 18:00:08 +0200

    entity_create/edit: customize displayed properties depending on serie type

--- app/modules/entities/lib/editor/properties_per_subtype.js
+++ app/modules/entities/lib/editor/properties_per_subtype.js
@@ -8,11 +8,18 @@ const illustratedBook = [ 'wdt:P58', 'wdt:P110', 'wdt:P6338' ]
 const allAuthorRoleProperties = classicBook.concat(illustratedBook)
 
 const preferredAuthorRolesPropertiesPerWorkType = {
+  // works
   'wd:Q1004': illustratedBook, // comics
   'wd:Q8274': illustratedBook, // manga
   'wd:Q562214': illustratedBook, // manhwa
   'wd:Q725377': illustratedBook, // graphic novel
   'wd:Q747381': illustratedBook, // light novel
+
+  // series
+  'wd:Q14406742': illustratedBook, // comic book series
+  'wd:Q21198342': illustratedBook, // manga series
+  'wd:Q74262765': illustratedBook, // manhwa series
+  'wd:Q104213567': illustratedBook, // light novel series
 }
 
 export function getWorkPreferredAuthorRolesProperties (entity) {
