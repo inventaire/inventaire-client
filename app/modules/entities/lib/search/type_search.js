f367f6be7a43dd31eaf229594bf9c1383c086332
maxlath <k@maxlath.eu>
2023-09-14 18:35:50 +0200

    entities search: use /api/search to search languages

--- app/modules/entities/lib/search/type_search.js
+++ app/modules/entities/lib/search/type_search.js
@@ -1,4 +1,3 @@
-import languageSearch from './language_search.js'
 import { getEntityUri, prepareSearchResult } from './entities_uris_results.js'
 import error_ from '#lib/error'
 import { pluralize } from '#entities/lib/types/entities_types'
@@ -17,9 +16,7 @@ export default async function (types, input, limit, offset) {
     if (res) return res
   }
 
-  if (types.includes('languages')) {
-    return languageSearch(input, limit, offset)
-  } else if (types.includes('subjects')) {
+  if (types.includes('subjects')) {
     return wikidataSearch({
       search: input,
       limit,
