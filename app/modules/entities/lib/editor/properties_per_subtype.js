739b6f147e3eb7d53888b102582646183fb5ff9f
maxlath <k@maxlath.eu>
2023-11-03 17:22:35 +0100

    add new author role properties: letterer (wdt:P9191), inker (wdt:P10836), penciller (wdt:P10837)

--- app/modules/entities/lib/editor/properties_per_subtype.js
+++ app/modules/entities/lib/editor/properties_per_subtype.js
@@ -2,24 +2,34 @@ import { isNonEmptyArray } from '#lib/boolean_tests'
 import { getUriNumericId } from '#lib/wikimedia/wikidata'
 import { uniq } from 'underscore'
 
-const classicBook = [ 'wdt:P50' ]
-const illustratedBook = [ 'wdt:P58', 'wdt:P110', 'wdt:P6338' ]
+const mostlyTextWorksRoles = [
+  'wdt:P50', // author
+  'wdt:P110', // illustrator
+]
+const drawedWorksRoles = [
+  'wdt:P58', // scenarist
+  'wdt:P6338', // colorist
+  'wdt:P9191', // letterer
+  'wdt:P10836', // inker
+  'wdt:P10837', // penciller
+]
+
 // const collectiveBook = [ 'wdt:P50', 'wdt:P98' ]
-const allAuthorRoleProperties = classicBook.concat(illustratedBook)
+const allAuthorRoleProperties = mostlyTextWorksRoles.concat(drawedWorksRoles)
 
 const preferredAuthorRolesPropertiesPerWorkType = {
   // works
-  'wd:Q1004': illustratedBook, // comics
-  'wd:Q8274': illustratedBook, // manga
-  'wd:Q562214': illustratedBook, // manhwa
-  'wd:Q725377': illustratedBook, // graphic novel
-  'wd:Q747381': illustratedBook, // light novel
+  'wd:Q1004': drawedWorksRoles, // comics
+  'wd:Q8274': drawedWorksRoles, // manga
+  'wd:Q562214': drawedWorksRoles, // manhwa
+  'wd:Q725377': drawedWorksRoles, // graphic novel
+  'wd:Q747381': drawedWorksRoles, // light novel
 
   // series
-  'wd:Q14406742': illustratedBook, // comic book series
-  'wd:Q21198342': illustratedBook, // manga series
-  'wd:Q74262765': illustratedBook, // manhwa series
-  'wd:Q104213567': illustratedBook, // light novel series
+  'wd:Q14406742': drawedWorksRoles, // comic book series
+  'wd:Q21198342': drawedWorksRoles, // manga series
+  'wd:Q74262765': drawedWorksRoles, // manhwa series
+  'wd:Q104213567': drawedWorksRoles, // light novel series
 }
 
 export function getWorkPreferredAuthorRolesProperties (entity) {
@@ -32,5 +42,5 @@ export function getWorkPreferredAuthorRolesProperties (entity) {
 const byNumericId = (a, b) => getUriNumericId(a) - getUriNumericId(b)
 
 function getP31PreferredAuthorRolesProperties (P31value) {
-  return preferredAuthorRolesPropertiesPerWorkType[P31value] || classicBook
+  return preferredAuthorRolesPropertiesPerWorkType[P31value] || mostlyTextWorksRoles
 }
