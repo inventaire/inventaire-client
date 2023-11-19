2df89d0b06d5b1adf5f74bf6060c01b3016374ac
jums <jums@inventaire.io>
2023-11-19 07:14:19 +0100

    refactor: extract author role in its own component

--- app/modules/entities/components/layouts/authors_info.svelte
+++ app/modules/entities/components/layouts/authors_info.svelte
@@ -1,29 +1,25 @@
 <script>
-  import { i18n } from '#user/lib/i18n'
+  import { pick } from 'underscore'
   import Spinner from '#general/components/spinner.svelte'
-  import { isNonEmptyArray, isNonEmptyPlainObject } from '#lib/boolean_tests'
+  import { isNonEmptyPlainObject } from '#lib/boolean_tests'
   import { getEntitiesAttributesFromClaims } from '#entities/lib/entities'
-  import AuthorDisplay from './author_display.svelte'
+  import AuthorsInfoRole from './authors_info_role.svelte'
   import { propertiesByRoles } from '#entities/components/lib/claims_helpers'
 
   export let claims = {}
 
   let authorsByUris
-
-  let authorProperties = Object.values(propertiesByRoles).flat()
-  let authorsClaims = _.pick(claims, authorProperties)
+  let authorsProperties = Object.values(propertiesByRoles).flat()
+  let authorsClaims = pick(claims, authorsProperties)
 
   const waitingForAuthors = getAuthors()
   async function getAuthors () {
-    // Claims used for getting birth and death years
-    const attributes = [ 'labels', 'claims', 'image' ]
     if (isNonEmptyPlainObject(authorsClaims)) {
+      // Claims attribute is necessary to get birth and death years
+      const attributes = [ 'labels', 'claims', 'image' ]
       authorsByUris = await getEntitiesAttributesFromClaims(authorsClaims, attributes)
     }
   }
-
-  const claimsProperties = Object.keys(claims)
-  const hasPropertiesToDisplay = role => isNonEmptyArray(_.intersection(propertiesByRoles[role], claimsProperties))
 </script>
 
 {#await waitingForAuthors}
@@ -31,26 +27,13 @@
 {:then}
   {#if authorsByUris}
     <div class="authors-info">
-      {#each Object.keys(propertiesByRoles) as role}
-        {#if hasPropertiesToDisplay(role)}
-          <div class="{role} authors-role">
-            <span class="label">{i18n(role)}</span>
-            <div class="authors">
-              {#each propertiesByRoles[role] as prop}
-                {#if authorsClaims[prop]}
-                  {#each authorsClaims[prop] as claimValue}
-                    <div class="author">
-                      <AuthorDisplay
-                        entityData={authorsByUris[claimValue]}
-                        {claimValue}
-                      />
-                    </div>
-                  {/each}
-                {/if}
-              {/each}
-            </div>
-          </div>
-        {/if}
+      {#each Object.keys(propertiesByRoles) as roleLabel}
+        <AuthorsInfoRole
+          {roleLabel}
+          roleProperties={propertiesByRoles[roleLabel]}
+          {claims}
+          {authorsByUris}
+        />
       {/each}
     </div>
   {/if}
@@ -60,19 +43,5 @@
   @import "#general/scss/utils";
   .authors-info{
     @include display-flex(row, flex-end, flex-start, wrap);
-  }
-  .authors-role{
-    @include display-flex(column, flex-start, flex-start, wrap);
-  }
-  .authors{
-    @include display-flex(row, flex-end, flex-start, wrap);
-  }
-  .author{
-    margin-inline-end: 0.5em;
-  }
-  .label{
-    color: $grey;
-    font-size: 0.9rem;
-    margin-block-end: 0.1em;
   }
 </style>
