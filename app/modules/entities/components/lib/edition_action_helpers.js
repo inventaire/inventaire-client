fa81cfae27bb691212adde24e16f64953f02ed35
jums <jums@inventaire.io>
2023-11-26 07:29:18 +0100

    work layout: users count: do not double count same edition items
    unicity was done on editions instead of users ids. This is fixing multiple items of the same owner counting

--- app/modules/entities/components/lib/edition_action_helpers.js
+++ app/modules/entities/components/lib/edition_action_helpers.js
@@ -1,10 +1,15 @@
 import { I18n } from '#user/lib/i18n'
+import { property, uniq } from 'underscore'
+import { isNonEmptyArray } from '#lib/boolean_tests'
 
-export const getCounterText = editionItems => I18n('users_count_have_this_book', { smart_count: getUsersSizePerEdition(editionItems) })
+export const getCounterText = editionItems => I18n('users_count_have_this_book', { smart_count: getOwnersSizePerEdition(editionItems) })
 
-const getUsersSizePerEdition = editionItems => {
-  const notMainUserOwners = editionItems.filter(notMainUserOwner)
-  return _.uniq(notMainUserOwners).length
+const getOwnersSizePerEdition = editionItems => {
+  if (isNonEmptyArray(editionItems)) {
+    const notMainUserEditions = editionItems.filter(notMainUserOwner)
+    const owners = notMainUserEditions.map(property('owner'))
+    return uniq(owners).length
+  }
 }
 
 const notMainUserOwner = doc => doc.owner !== app.user.id
