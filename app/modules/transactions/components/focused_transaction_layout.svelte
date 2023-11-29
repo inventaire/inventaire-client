84753795031ee4cabf360a0913188eb2b9106e2c
maxlath <k@maxlath.eu>
2023-11-29 17:36:57 +0000

    focused_transaction_layout: start rewriting in Svelte

---
+++ app/modules/transactions/components/focused_transaction_layout.svelte
@@ -0,0 +1,56 @@
+<script>
+  import TransactionHeader from '#transactions/components/transaction_header.svelte'
+  import TransactionTimeline from '#transactions/components/transaction_timeline.svelte'
+  import TransactionNextAction from '#transactions/components/transaction_next_action.svelte'
+  import NewMessage from '#transactions/components/new_message.svelte'
+  import CancelTransaction from '#transactions/components/cancel_transaction.svelte'
+  import { attachLinkedDocs } from '#transactions/lib/transactions'
+  import Spinner from '#components/spinner.svelte'
+  import Flash from '#lib/components/flash.svelte'
+
+  export let transaction
+
+  let flash
+  const waiting = attachLinkedDocs(transaction)
+    .catch(err => flash = err)
+</script>
+
+<div class="focused-transaction-layout">
+  <Flash state={flash} />
+  {#await waiting}
+    <Spinner center={true} />
+  {:then}
+    <section>
+      <TransactionHeader {transaction} />
+      <TransactionTimeline {transaction} />
+    </section>
+    <TransactionNextAction bind:transaction />
+    <section class="new-message-section">
+      <NewMessage bind:transaction />
+    </section>
+    <CancelTransaction bind:transaction />
+  {/await}
+</div>
+
+<style lang="scss">
+  @import '#general/scss/utils';
+
+  .focused-transaction-layout{
+    max-width: 40em;
+    margin: 1em auto;
+    :global(section){
+      @include radius;
+      @include shy-border;
+      background-color: white;
+    }
+  }
+  .new-message-section{
+    margin-block-start: 1em;
+  }
+  /* Large screens */
+  @media screen and (min-width: $small-screen){
+    .focused-transaction-layout{
+      padding: 1em;
+    }
+  }
+</style>
