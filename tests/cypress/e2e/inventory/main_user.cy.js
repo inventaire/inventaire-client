9a988132bc524843df129d521ce575708a6aa9c4
jums <jums@inventaire.io>
2023-12-03 10:07:27 +0100

    update Commands with cypress session
    to share state between tests
    
    ie. login only once

--- tests/cypress/e2e/inventory/main_user.cy.js
+++ tests/cypress/e2e/inventory/main_user.cy.js
@@ -1,10 +1,15 @@
 describe('inventory:main user', () => {
-  before(() => {
+  beforeEach(() => {
     cy.login()
   })
 
   it('should navigate to the home screen when logged in', () => {
     cy.visit('/')
     cy.contains('Add books')
+  })
+
+  it('should navigate to book import', () => {
+    cy.visit('/')
+    cy.contains('Add books').click()
   })
 })
--- tests/cypress/support/commands.js
+++ tests/cypress/support/commands.js
@@ -1,11 +1,54 @@
 // Various custom commands and overwrite of existing commands.
 // See https://on.cypress.io/custom-commands
 
-Cypress.Commands.add('login', () => {
-  const username = 'hello' + Math.random().toString().slice(2, 10)
-  cy.request({
-    method: 'post',
-    url: '/api/auth?action=signup',
-    body: { username, email: `${username}@hello.hello`, password: '12345678' },
+const password = '12345678'
+
+Cypress.Commands.add('signup', username => {
+  if (!username) username = 'hello' + Math.random().toString().slice(2, 10)
+  cy.session(username, () => {
+    cy.request({
+      method: 'post',
+      url: '/api/auth?action=signup',
+      body: {
+        username,
+        email: `${username}@hello.hello`,
+        password
+      },
+    })
   })
 })
+
+Cypress.Commands.add('login', (username = 'adamsberg') => {
+  // More about cy.session(): https://docs.cypress.io/api/commands/session#Notes
+  cy.session(username, () => {
+    signUpIfNecessary(username)
+    loginReq(username)
+  })
+})
+
+function signUpIfNecessary (username) {
+  cy.request({
+    method: 'get',
+    url: `/api/auth?action=username-availability&username=${
+      username}`,
+    failOnStatusCode: false,
+  })
+  // .then() is not a JS Promise, but some Cypress stuff
+  // See https://learn.cypress.io/cypress-fundamentals/understanding-the-asynchronous-nature-of-cypress
+  .then(({ body }) => {
+    if (body.status !== 400) {
+      cy.signup(username)
+    }
+  })
+}
+
+function loginReq (username) {
+  cy.request({
+    method: 'post',
+    url: '/api/auth?action=login',
+    body: {
+      username,
+      password
+    },
+  })
+}
