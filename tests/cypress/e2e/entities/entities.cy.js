7c969f5a5b69806cac5a16373032e3227455a99f
jums <jums@inventaire.io>
2023-12-06 12:52:36 +0100

    add a test with a should callback

--- tests/cypress/e2e/entities/entities.cy.js
+++ tests/cypress/e2e/entities/entities.cy.js
@@ -12,8 +12,19 @@ describe('entities layout', () => {
   it('should filter works when typing work title in filter field', () => {
     cy.contains('.works-browser', 'Hernani')
     cy.get('.works-browser-text-filter')
-      .type('Poète{enter}')
+    .type('Poète{enter}')
     cy.contains('.works-browser', 'Le Poète dans les révolutions')
-    cy.get('.works-browser').should('not.include.text', 'Hernani')
+    cy.get('.works-browser')
+    .should('not.include.text', 'Hernani')
+  })
+
+  it('should display works browser sections', () => {
+    // More callbacks at : https://glebbahmutov.com/blog/cypress-should-callback/
+    cy.get('.works-browser-section')
+    .should(div => {
+      expect(div.eq(0)).to.contain('Series')
+      expect(div.eq(1)).to.contain('Works')
+      expect(div.eq(2)).to.contain('Articles')
+    })
   })
 })
