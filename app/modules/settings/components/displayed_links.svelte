741036a9c50f58bda4033441419f687f52435971
maxlath <k@maxlath.eu>
2024-01-08 19:19:31 +0100

    displayed links settings: add whole-category checkboxes
    to make it less tedious to select many

--- app/modules/settings/components/displayed_links.svelte
+++ app/modules/settings/components/displayed_links.svelte
@@ -3,9 +3,15 @@
   import { getPropertiesFromWebsitesNames, getWebsitesNamesFromProperties, websitesByCategoryAndName } from '#entities/lib/entity_links'
   import { onChange } from '#lib/svelte/svelte'
   import Flash from '#lib/components/flash.svelte'
-  import { debounce } from 'underscore'
+  import { debounce, difference, intersection, uniq } from 'underscore'
 
   const { bibliographicDatabases, socialNetworks } = websitesByCategoryAndName
+
+  const bibliographicDatabasesNames = Object.keys(bibliographicDatabases)
+  const socialNetworksNames = Object.keys(socialNetworks)
+
+  let selectedBibliographicDatabasesCount = 0
+  let selectedSocialNetworksCount = 0
 
   let customProperties = app.user.get('customProperties') || []
   let stringifiedSavedCustomProperties = JSON.stringify(customProperties)
@@ -32,45 +38,83 @@
   const lazyUpdate = debounce(updateCustomProperties, 1000)
   function onSelectionChange () {
     customProperties = getPropertiesFromWebsitesNames(selectedWebsites)
+    selectedBibliographicDatabasesCount = intersection(bibliographicDatabasesNames, selectedWebsites).length
+    selectedSocialNetworksCount = intersection(socialNetworksNames, selectedWebsites).length
     lazyUpdate()
   }
   $: onChange(selectedWebsites, onSelectionChange)
+
+  function toggleAllSection ({ names, selectedCount }) {
+    if (selectedCount !== names.length) {
+      selectedWebsites = uniq(selectedWebsites.concat(names))
+    } else {
+      selectedWebsites = difference(selectedWebsites, names)
+    }
+  }
 </script>
 
-<fieldset>
-  <legend>{I18n('bibliographic databases')}</legend>
-  {#each Object.keys(bibliographicDatabases) as websiteName}
-    <label>
-      <input type="checkbox" bind:group={selectedWebsites} value={websiteName} />
-      {websiteName}
-    </label>
-  {/each}
-</fieldset>
-<fieldset>
-  <legend>{I18n('social networks')}</legend>
-  {#each Object.keys(socialNetworks) as websiteName}
-    <label>
-      <input type="checkbox" bind:group={selectedWebsites} value={websiteName} />
-      {websiteName}
-    </label>
-  {/each}
-</fieldset>
+<section>
+  <label class="category">
+    <input
+      type="checkbox"
+      checked={selectedBibliographicDatabasesCount === bibliographicDatabasesNames.length}
+      indeterminate={selectedBibliographicDatabasesCount !== 0 && selectedBibliographicDatabasesCount !== bibliographicDatabasesNames.length}
+      on:click={() => toggleAllSection({ names: bibliographicDatabasesNames, selectedCount: selectedBibliographicDatabasesCount })}
+    />
+    {I18n('bibliographic databases')}
+  </label>
+  <fieldset>
+    {#each bibliographicDatabasesNames as websiteName}
+      <label>
+        <input type="checkbox" bind:group={selectedWebsites} value={websiteName} />
+        {websiteName}
+      </label>
+    {/each}
+  </fieldset>
+</section>
+
+<section>
+  <label class="category">
+    <input
+      type="checkbox"
+      checked={selectedSocialNetworksCount === socialNetworksNames.length}
+      indeterminate={selectedSocialNetworksCount !== 0 && selectedSocialNetworksCount !== socialNetworksNames.length}
+      on:click={() => toggleAllSection({ names: socialNetworksNames, selectedCount: selectedSocialNetworksCount })}
+    />
+    {I18n('social networks')}
+  </label>
+  <fieldset>
+    {#each socialNetworksNames as websiteName}
+      <label>
+        <input type="checkbox" bind:group={selectedWebsites} value={websiteName} />
+        {websiteName}
+      </label>
+    {/each}
+  </fieldset>
+</section>
+
 <Flash bind:state={flash} />
 
 <style lang="scss">
   @import "#general/scss/utils";
+  section{
+    margin-block-end: 1em;
+    @include shy-border;
+    @include radius;
+  }
   fieldset{
     @include display-flex(row, null, null, wrap);
-    margin-block-start: 0.5em;
   }
-  legend{
+  .category{
     font-weight: bold;
+    font-size: 1rem;
+    padding: 0.5em;
+    background-color: $lighter-grey;
   }
-  label{
+  label:not(.category){
+    font-size: 1rem;
+    @include display-flex(row, center);
     width: min(15em, 80vw);
     padding: 0.5em;
-    font-size: 1rem;
-    cursor: pointer;
-    @include display-flex(row, center);
   }
 </style>
