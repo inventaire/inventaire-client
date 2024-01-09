a9a9795986ea9f04ebed6180ab49a0b596a6d508
maxlath <k@maxlath.eu>
2024-01-09 23:08:47 +0100

    category_external_ids: fix chevrons direction for right-to-left languages

--- app/lib/active_languages.ts
+++ app/lib/active_languages.ts
@@ -29,7 +29,7 @@ export type UserLang = typeof langs[number]
 // Generated with https://w.wiki/5cVs
 const rtlLang = new Set([ 'ace', 'ady', 'aeb', 'ar', 'arc', 'arq', 'ary', 'arz', 'az', 'bej', 'bjn', 'bm', 'bqi', 'bsk', 'ckb', 'dar', 'diq', 'dv', 'ett', 'fa', 'ff', 'glk', 'ha', 'hbo', 'he', 'jv', 'kbd', 'khw', 'kk', 'kr', 'ks', 'ku', 'kum', 'ky', 'lad', 'lki', 'lrc', 'luz', 'min', 'ms', 'mzn', 'non', 'nqo', 'ota', 'otk', 'pa', 'pnb', 'ps', 'qya', 'rif', 'sd', 'sdh', 'shi', 'shy', 'sw', 'syc', 'ta', 'tg', 'tk', 'tru', 'tzm', 'ug', 'uz', 'uzs', 'wbl', 'wo', 'xpu' ])
 
-export const getTextDirection = (lang = 'en') => {
+export function getTextDirection (lang = 'en') {
   const languageFamilyCode = lang.split('-')[0]
   return rtlLang.has(languageFamilyCode) ? 'rtl' : 'ltr'
 }
