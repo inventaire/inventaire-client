56bd47b66b9445cbf72adcd1b6ac130726c385c1
maxlath <k@maxlath.eu>
2024-02-21 00:10:38 +0100

    helpers: handle case where initial transactions fetch is slow or failed

--- app/modules/transactions/lib/helpers.js
+++ app/modules/transactions/lib/helpers.js
@@ -1,7 +1,7 @@
 import preq from '#lib/preq'
 import { getTransactions } from '#transactions/lib/get_transactions'
 import error_ from '#lib/error'
-import { serializeTransaction } from '#transactions/lib/transactions'
+import { getActiveTransactionsByItemId, serializeTransaction } from '#transactions/lib/transactions'
 
 let transactions
 export default async function () {
@@ -25,17 +25,30 @@ export async function postTransactionMessage ({ transaction, message }) {
 
 export function hasOngoingTransactionsByItemIdSync (itemId) {
   // Ideally, we would await the transactions, but that wouldn't be a sync function anymore
-  if (!transactions) throw error_.new('called before transactions were initialized', 500)
+  if (!transactions) {
+    error_.report('called before transactions were initialized', {}, 500)
+    return false
+  }
   return transactions.find(transaction => transaction.item === itemId && !transaction.archived) != null
 }
 
 export async function postTransactionRequest ({ itemId, message }) {
-  const { transaction } = await preq.post(app.API.transactions.base, {
-    action: 'request',
-    item: itemId,
-    message,
-  })
-  const serializedTransaction = serializeTransaction(transaction)
-  transactions.unshift(serializedTransaction)
-  return serializedTransaction
+  try {
+    const { transaction } = await preq.post(app.API.transactions.base, {
+      action: 'request',
+      item: itemId,
+      message,
+    })
+    const serializedTransaction = serializeTransaction(transaction)
+    transactions.unshift(serializedTransaction)
+    return serializedTransaction
+  } catch (err) {
+    // This case should normally not be possible, as UI elements should not offer to request
+    // an item already requested, but it might not have worked if hasOngoingTransactionsByItemIdSync was not ready
+    if (err.message === 'user already made a request on this item') {
+      const transactions = await getActiveTransactionsByItemId(itemId)
+      if (transactions[0]) return serializeTransaction(transactions[0])
+    }
+    throw err
+  }
 }
