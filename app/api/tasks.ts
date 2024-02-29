d48bc26f4aade481841c1a9cef56a9078e3d0feb
jums <jums@inventaire.io>
2024-02-29 16:01:49 +0100

    remove unused bySuspectUris URL builder

--- app/api/tasks.ts
+++ app/api/tasks.ts
@@ -1,18 +1,15 @@
 import { forceArray } from '#app/lib/utils'
-import { getEndpointPathBuilders } from './endpoint.ts'
-
-const { action } = getEndpointPathBuilders('tasks')
-const byEntitiesUris = name => uris => {
-  uris = forceArray(uris).join('|')
-  return action(`by-${name}-uris`, { uris })
-}
+import endpoint from './endpoint.js'
+const { action } = endpoint('tasks')
 
 export default {
   byIds (ids) { return action('by-ids', { ids }) },
   byScore (limit, offset) { return action('by-score', { limit, offset }) },
   byEntitiesType (params) { return action('by-entities-type', params) },
-  bySuspectUris: byEntitiesUris('suspect'),
-  bySuggestionUris: byEntitiesUris('suggestion'),
+  bySuggestionUris (uris) {
+    uris = forceArray(uris).join('|')
+    return action('by-suggestion-uris', { uris })
+  },
   deduplicateWorks: action('deduplicate-works'),
   update: action('update'),
 }
