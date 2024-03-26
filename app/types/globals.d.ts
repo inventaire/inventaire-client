8313fe9882b6be87c7089bc4685e3a26370f961c
maxlath <k@maxlath.eu>
2024-03-26 19:41:19 +0100

    import global libs types

---
+++ app/types/globals.d.ts
@@ -0,0 +1,6 @@
+// This is the result of trial and errors, rather than a clear understanding of how to declare global object types
+// There might be a better/more idomatic way
+declare module jQuery {}
+declare module Backbone {}
+declare module Marionette {}
+declare module L {}
