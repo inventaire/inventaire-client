6d5cafab7bdef676393709429090acc180c23f4b
maxlath <k@maxlath.eu>
2024-04-12 15:21:56 +0200

    boolean_tests: add type assertions

--- app/lib/boolean_tests.ts
+++ app/lib/boolean_tests.ts
@@ -1,14 +1,22 @@
 import { isString, isArray } from 'underscore'
 import * as regex_ from '#lib/regex'
 import typeOf from '#lib/type_of'
-// Keep in sync with server/lib/boolean_tests
+import type { AbsoluteUrl } from '#server/types/common'
+import type { CouchUuid } from '#server/types/couchdb'
+import type { EntityUri, PropertyUri, WdEntityUri, WdPropertyUri } from '#server/types/entity'
+import type { AssetImagePath, GroupImagePath, ImageHash, ImagePath, UserImagePath } from '#server/types/image'
+import type { Email, Username } from '#server/types/user'
+import type { ItemId, PropertyId } from 'wikibase-sdk'
 
-export const isCouchUuid = regex_.CouchUuid.test.bind(regex_.CouchUuid)
-const bindedTest = regexName => regex_[regexName].test.bind(regex_[regexName])
+function bindedTest <T> (regexName) {
+  return function (str): str is T {
+    return regex_[regexName].test(str)
+  }
+}
 
 export const isNonEmptyString = str => isString(str) && (str.length > 0)
 
-export const isUrl = url => {
+export function isUrl (url: string): url is AbsoluteUrl {
   try {
     const { protocol, username, password } = new URL(url)
     if (!(protocol === 'http:' || protocol === 'https:')) return false
@@ -23,24 +31,25 @@ export const isUrl = url => {
   return true
 }
 
-export const isImageHash = bindedTest('ImageHash')
-export const isLocalImg = bindedTest('LocalImg')
-export const isAssetImg = bindedTest('AssetImg')
-export const isUserImg = bindedTest('UserImg')
-export const isGroupImg = bindedTest('GroupImg')
+export const isCouchUuid = bindedTest<CouchUuid>('CouchUuid')
+export const isImageHash = bindedTest<ImageHash>('ImageHash')
+export const isLocalImg = bindedTest<ImagePath>('LocalImg')
+export const isAssetImg = bindedTest<AssetImagePath>('AssetImg')
+export const isUserImg = bindedTest<UserImagePath>('UserImg')
+export const isGroupImg = bindedTest<GroupImagePath>('GroupImg')
 export const isInvEntityId = isCouchUuid
-export const isEmail = bindedTest('Email')
+export const isEmail = bindedTest<Email>('Email')
 export const isUserId = isCouchUuid
 export const isGroupId = isCouchUuid
 export const isItemId = isCouchUuid
 export const isShelfId = isCouchUuid
-export const isUsername = bindedTest('Username')
-export const isEntityUri = bindedTest('EntityUri')
-export const isPropertyUri = bindedTest('PropertyUri')
-export const isWikidataItemId = bindedTest('WikidataItemId')
-export const isWikidataPropertyId = bindedTest('WikidataPropertyId')
-export const isWikidataItemUri = bindedTest('WikidataItemUri')
-export const isWikidataPropertyUri = bindedTest('WikidataPropertyUri')
+export const isUsername = bindedTest<Username>('Username')
+export const isEntityUri = bindedTest<EntityUri>('EntityUri')
+export const isPropertyUri = bindedTest<PropertyUri>('PropertyUri')
+export const isWikidataItemId = bindedTest<ItemId>('WikidataItemId')
+export const isWikidataPropertyId = bindedTest<PropertyId>('WikidataPropertyId')
+export const isWikidataItemUri = bindedTest<WdEntityUri>('WikidataItemUri')
+export const isWikidataPropertyUri = bindedTest<WdPropertyUri>('WikidataPropertyUri')
 
 export const isNonNull = obj => obj != null
 
@@ -63,4 +72,6 @@ export const isDateString = dateString => {
 
 // As Svelte components are the only known users of CustomEvent
 // in the stack, it doesn't not need to be more specific than that for now
-export const isComponentEvent = obj => obj instanceof CustomEvent
+export function isComponentEvent (obj): obj is CustomEvent {
+  return obj instanceof CustomEvent
+}
