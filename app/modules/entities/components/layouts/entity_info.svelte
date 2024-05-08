d1c78c159df1fd91886680064aa028e39797f15e
jums <jums@inventaire.io>
2024-05-08 09:10:12 +0200

    add modal to show element data
    this commit felt like harvesting seeds from entity layout to be reusable quite easily
    
    prefer modal to a fancy row display, to keep it simple and more future
    proofed since more information can be displayed more easily

---
+++ app/modules/entities/components/layouts/entity_info.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import AuthorsInfo from '#entities/components/layouts/authors_info.svelte'
+  import Ebooks from '#entities/components/layouts/ebooks.svelte'
+  import EntityTitle from '#entities/components/layouts/entity_title.svelte'
+  import Infobox from '#entities/components/layouts/infobox.svelte'
+  import Summary from '#entities/components/layouts/summary.svelte'
+
+  export let entity, shortlistOnly, hasLinkTitle
+
+  const { type } = entity
+  $: claims = entity.claims
+</script>
+
+<EntityTitle
+  {entity}
+  {hasLinkTitle}
+/>
+<AuthorsInfo {claims} />
+<Infobox
+  {claims}
+  entityType={type}
+  {shortlistOnly}
+/>
+<Ebooks {entity} />
+<Summary {entity} />
