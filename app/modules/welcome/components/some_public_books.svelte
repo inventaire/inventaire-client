inventaire/inventaire-client

commit c38afff2892beb95b2b7aa2c6068b3601b36620f

welcome page: public books section: when no public items are found, do not throw and do not display the section
laying ground for decentralisation, as admin might create an instance without any public items available

app/modules/welcome/components/some_public_books.svelte
@@ -3,6 +3,7 @@
   import Spinner from '#components/spinner.svelte'
   import ItemsCascade from '#inventory/components/items_cascade.svelte'
   import ItemsTable from '#inventory/components/items_table.svelte'
+  import { isNonEmptyArray } from '#lib/boolean_tests'
   import { screen } from '#lib/components/stores/screen'
   import { I18n } from '#user/lib/i18n'
 
@@ -13,21 +14,26 @@
   }
 
   const waiting = app.request('items:getRecentPublic', params)
+    .catch(err => {
+      if (err.message !== 'no item found') throw err
+    })
 </script>
 
-<section>
-  <h3>{I18n('some of the last books listed')}</h3>
-  {#await waiting}
-    <Spinner center={true} />
-  {:then}
-    {#if $screen.isSmallerThan('$smaller-screen')}
-      <ItemsTable items={params.items} {waiting} haveSeveralOwners={true} />
-    {:else}
-      <ItemsCascade items={params.items} {waiting} />
-    {/if}
-  {/await}
-  <div class="fade-out" />
-</section>
+{#if isNonEmptyArray(params.items)}
+  <section>
+    <h3>{I18n('some of the last books listed')}</h3>
+    {#await waiting}
+      <Spinner center={true} />
+    {:then}
+      {#if $screen.isSmallerThan('$smaller-screen')}
+        <ItemsTable items={params.items} {waiting} haveSeveralOwners={true} />
+      {:else}
+        <ItemsCascade items={params.items} {waiting} />
+      {/if}
+    {/await}
+    <div class="fade-out" />
+  </section>
+{/if}
 
 <style lang="scss">
   @import "#welcome/scss/welcome_layout_commons";

commit 14a09d18b0da4121bf96a87d21fab8b3217634f4

welcome: recover some_public_books list when the items database is not empty
Fix regression introduced by c38afff

app/modules/welcome/components/some_public_books.svelte
@@ -19,21 +19,25 @@
     })
 </script>
 
-{#if isNonEmptyArray(params.items)}
-  <section>
-    <h3>{I18n('some of the last books listed')}</h3>
-    {#await waiting}
-      <Spinner center={true} />
-    {:then}
-      {#if $screen.isSmallerThan('$smaller-screen')}
-        <ItemsTable items={params.items} {waiting} haveSeveralOwners={true} />
-      {:else}
-        <ItemsCascade items={params.items} {waiting} />
-      {/if}
-    {/await}
-    <div class="fade-out" />
-  </section>
-{/if}
+{#await waiting}
+  <Spinner />
+{:then}
+  {#if isNonEmptyArray(params.items)}
+    <section>
+      <h3>{I18n('some of the last books listed')}</h3>
+      {#await waiting}
+        <Spinner center={true} />
+      {:then}
+        {#if $screen.isSmallerThan('$smaller-screen')}
+          <ItemsTable items={params.items} {waiting} haveSeveralOwners={true} />
+        {:else}
+          <ItemsCascade items={params.items} {waiting} />
+        {/if}
+      {/await}
+      <div class="fade-out" />
+    </section>
+  {/if}
+{/await}
 
 <style lang="scss">
   @import "#welcome/scss/welcome_layout_commons";