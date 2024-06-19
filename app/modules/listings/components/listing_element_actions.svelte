163c7d25d8b54531019dc11efa7872f063f2edec
jums <jums@inventaire.io>
2024-06-19 12:31:19 +0200

    refactor: refresh elements when updating paginatedElements

--- app/modules/listings/components/listing_element_actions.svelte
+++ app/modules/listings/components/listing_element_actions.svelte
@@ -6,11 +6,11 @@
   import { removeElement, updateElement } from '#listings/lib/listings'
   import { i18n } from '#user/lib/i18n'
 
-  export let isReordering, element, elements, paginatedElements, listingId, flash
+  export let isReordering, element, paginatedElements, listingId, flash
 
   let isLoading
   const { _id, uri } = element
-  $: hasSeveralElements = elements.length > 1
+  $: hasSeveralElements = paginatedElements.length > 1
   $: index = paginatedElements.findIndex(obj => obj.uri === uri)
 
   async function _removeElement () {
@@ -19,7 +19,6 @@
         // Enhancement: after remove, have an "undo" button
         paginatedElements.splice(index, 1)
         paginatedElements = paginatedElements
-        elements.splice(index, 1)
       })
       .catch(err => flash = err)
   }
@@ -28,8 +27,7 @@
     isReordering = isLoading = true
     flash = null
     const oldElements = clone(paginatedElements)
-    moveArrayElement(paginatedElements, index, newIndex)
-    paginatedElements = paginatedElements
+    paginatedElements = moveArrayElement(paginatedElements, index, newIndex)
     const exclusiveOrdinal = newIndex + 1
 
     await updateElement({
@@ -76,7 +74,7 @@
           {index + 1}
         {/if}
       </div>
-      {#if index !== elements.length - 1}
+      {#if index !== paginatedElements.length - 1}
         <button
           disabled={isReordering}
           on:click={() => reorder(index + 1)}
