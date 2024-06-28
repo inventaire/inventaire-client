928b089055897884d7e55befa22715910d8a1cc6
jums <jums@inventaire.io>
2024-06-28 15:48:39 +0200

    add elements search: taylor entities search according to listing type

---
+++ app/modules/listings/lib/entities_typing.ts
@@ -0,0 +1,27 @@
+import { typesBySection } from '#search/lib/search_sections'
+
+const { work, serie, author, publisher } = typesBySection.entity
+
+const searchTypesByListingType = {
+  work: [ work, serie ],
+  author: [ author ],
+  publisher: [ publisher ],
+}
+
+export const getSearchType = listingType => searchTypesByListingType[listingType]
+
+const i18nTypesKeys = {
+  work: 'works and series',
+  author: 'authors',
+  publisher: 'publishers',
+}
+
+export const getI18nTypeKey = listingType => i18nTypesKeys[listingType]
+
+const i18nSearchPlaceholderKeys = {
+  work: 'Search for works or series',
+  author: 'Search for authors',
+  publisher: 'Search for publishers',
+}
+
+export const getI18nSearchPlaceholder = listingType => i18nSearchPlaceholderKeys[listingType]
