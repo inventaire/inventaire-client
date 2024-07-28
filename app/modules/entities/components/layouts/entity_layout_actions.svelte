34de8a003238ab6441518dfe3bab54d19282a9d1
maxlath <k@maxlath.eu>
2024-07-28 18:09:26 +0200

    entity_layout_actions: always link to local history page
    - required to see local entity layers
    - but including remote entities without local layers for behavior consistency

--- app/modules/entities/components/layouts/entity_layout_actions.svelte
+++ app/modules/entities/components/layouts/entity_layout_actions.svelte
@@ -2,7 +2,7 @@
   import app from '#app/app'
   import Link from '#app/lib/components/link.svelte'
   import { icon as iconFn } from '#app/lib/handlebars_helpers/icons'
-  import { getWikidataUrl, getWikidataHistoryUrl, type SerializedEntity } from '#entities/lib/entities'
+  import { getWikidataUrl, type SerializedEntity } from '#entities/lib/entities'
   import { startRefreshTimeSpan } from '#entities/lib/entity_refresh'
   import Spinner from '#general/components/spinner.svelte'
   import { i18n, I18n } from '#user/lib/i18n'
@@ -21,7 +21,6 @@
   }
 
   const wikidataUrl = wdUri ? getWikidataUrl(wdUri) : null
-  const wikidataHistoryUrl = wdUri ? getWikidataHistoryUrl(wdUri) : null
 </script>
 
 {#if showEntityEditButtons}
@@ -55,13 +54,6 @@
       <span class="button-text">{I18n('refresh Wikidata data')}</span>
     </button>
   </li>
-  <li>
-    <Link
-      url={wikidataHistoryUrl}
-      text={I18n('entity history')}
-      icon="history"
-    />
-  </li>
 {:else}
   {#if claims['wdt:P212'] && !wdUri}
     <li>
@@ -78,14 +70,14 @@
       </button>
     </li>
   {/if}
-  <li>
-    <Link
-      url={`/entity/${uri}/history`}
-      text={I18n('entity history')}
-      icon="history"
-    />
-  </li>
 {/if}
+<li>
+  <Link
+    url={`/entity/${uri}/history`}
+    text={I18n('entity history')}
+    icon="history"
+  />
+</li>
 
 {#if app.user.hasDataadminAccess && showEntityEditButtons}
   {#if type === 'human'}
