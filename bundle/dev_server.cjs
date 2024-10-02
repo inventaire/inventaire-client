1f0d9036ad60daef8643fa0c960e943bffd60658
maxlath <k@maxlath.eu>
2024-10-02 14:59:10 +0200

    dependencies: update webpack* dependencies

--- bundle/dev_server.cjs
+++ bundle/dev_server.cjs
@@ -6,13 +6,18 @@ module.exports = {
   port: webpackDevServer.port,
   hot: true,
   // See https://webpack.js.org/configuration/dev-server/#devserverproxy
-  proxy: {
-    '/api': inventaireServerHost,
-    '/public': inventaireServerHost,
-    '/img': inventaireServerHost,
-    '/local': inventaireServerHost,
-    '/**/*.(json|xml)': inventaireServerHost,
-  },
+  proxy: [
+    {
+      context: [
+        '/api',
+        '/public',
+        '/img',
+        '/local',
+        '/**/*.(json|xml)',
+      ],
+      target: inventaireServerHost,
+    },
+  ],
   historyApiFallback: {
     rewrites: [
       { from: /./, to: '/public/dist/index.html' },
