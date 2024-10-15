164a42415aec1c8691df5b66dfa93836e91c603d
jums <jums@inventaire.io>
2024-10-15 10:30:49 +0200

    allow to click on edition cover to enlarge it
    a long wanting feature (imo) to allow close inspection of a cover when solving an edition task
    
    by extenstion this is also adding the feature to edition modal

--- app/modules/entities/components/entity_image.svelte
+++ app/modules/entities/components/entity_image.svelte
@@ -2,6 +2,7 @@
   import Link from '#app/lib/components/link.svelte'
   import { loadInternalLink } from '#app/lib/utils'
   import ImageDiv from '#components/image_div.svelte'
+  import Modal from '#components/modal.svelte'
   import type { SerializedEntity } from '#entities/lib/entities'
   import { i18n } from '#user/lib/i18n'
 
@@ -9,18 +10,42 @@
   export let size = 300
   export let withLink = false
   export let noImageCredits = false
+  export let enlargeInModal = false
 
   const { image, uri } = entity
   const { url } = image
   let creditsUrl, creditsText
+  let showModal = false
 
   if ('credits' in image) {
     creditsUrl = image.credits.url
     creditsText = image.credits.text
   }
+  if (enlargeInModal) {
+    withLink = true
+  }
+
+  function onImageClick (e) {
+    if (enlargeInModal) {
+      showModal = true
+    } else {
+      loadInternalLink(e)
+    }
+  }
 
   const entityUrl = `/entity/${uri}`
 </script>
+{#if showModal}
+  <Modal
+    on:closeModal={() => showModal = false}
+    closeOnClick={true}
+  >
+    <ImageDiv
+      url={image.url}
+      size={1000}
+    />
+  </Modal>
+{/if}
 <div
   class="entity-image"
   style:max-width="{size}px"
@@ -28,7 +53,7 @@
   {#if withLink}
     <a
       href={entityUrl}
-      on:click={loadInternalLink}
+      on:click={e => onImageClick(e)}
     >
       <ImageDiv {url} {size} />
     </a>
--- app/modules/tasks/components/task_entity.svelte
+++ app/modules/tasks/components/task_entity.svelte
@@ -44,6 +44,7 @@
             {entity}
             size={96}
             noImageCredits={true}
+            enlargeInModal={true}
           />
         </div>
       {/if}
