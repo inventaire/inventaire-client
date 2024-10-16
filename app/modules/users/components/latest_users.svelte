eb8f2d4ff22fbe5f5d9ac25311a0b79c04f76fb0
maxlath <k@maxlath.eu>
2024-10-16 17:24:23 +0200

    latest_users: display abuse reports

--- app/modules/users/components/latest_users.svelte
+++ app/modules/users/components/latest_users.svelte
@@ -34,20 +34,35 @@
 <ul>
   {#each users as user (user._id)}
     <li>
-      <img src={imgSrc(user.picture, 64)} alt="" loading="lazy" />
-      <div class="info">
-        <div class="header">
-          <span class="username">{user.username}</span>
-          <span class="id">{user._id}</span>
-          <span class="created">{getSimpleTime(user.created)}</span>
+      <div class="top">
+        <img src={imgSrc(user.picture, 64)} alt="" loading="lazy" />
+        <div class="info">
+          <div class="header">
+            <span class="username">{user.username}</span>
+            <span class="id">{user._id}</span>
+            <span class="created">{getSimpleTime(user.created)}</span>
+          </div>
+          <p class="bio">{user.bio || ''}</p>
+          <p class="links">
+            <a class="link" on:click={loadInternalLink} href={user.pathname}>{I18n('profile')}</a> -
+            <a class="link" target="_blank" href={`${user.pathname}.json`}>JSON</a> -
+            <a class="link" on:click={loadInternalLink} href={user.contributionsPathname}>{I18n('contributions')}</a>
+          </p>
         </div>
-        <p class="bio">{user.bio || ''}</p>
-        <p class="links">
-          <a class="link" on:click={loadInternalLink} href={user.pathname}>{I18n('profile')}</a> -
-          <a class="link" target="_blank" href={`${user.pathname}.json`}>JSON</a> -
-          <a class="link" on:click={loadInternalLink} href={user.contributionsPathname}>{I18n('contributions')}</a>
-        </p>
       </div>
+      {#if user.reports?.length > 0}
+        <div class="reports">
+          <h4>{i18n('Abuse reports')}</h4>
+          <ul>
+            {#each user.reports as { type, text }}
+              <li>
+                <span class="report-type">{type}</span>
+                <span class="report-text">{text}</span>
+              </li>
+            {/each}
+          </ul>
+        </div>
+      {/if}
     </li>
   {/each}
 </ul>
@@ -73,14 +88,13 @@
     justify-content: center;
   }
   li{
-    display: flex;
-    flex-direction: row;
-    align-items: flex-start;
-    justify-content: flex-start;
     max-width: 50em;
     background-color: $light-grey;
     padding: 0.5em;
     margin: 0.5em auto;
+  }
+  .top{
+    @include display-flex(row, flex-start, flex-start);
   }
   .info{
     position: relative;
@@ -121,5 +135,37 @@
   .controls{
     height: 5em;
     @include display-flex(row, center, center);
+  }
+  .reports{
+    background-color: #ddd;
+    padding: 0.2rem;
+    margin-block-start: 0.5rem;
+    h4{
+      font-size: 1rem;
+      @include sans-serif;
+      font-weight: bold;
+      line-height: 1rem;
+      margin: 0.2rem;
+    }
+    li{
+      padding: 0.5rem;
+      margin: 0.2rem 0;
+      @include radius;
+      background-color: $yellow;
+    }
+  }
+  .report-type{
+    background-color: $grey;
+    font-weight: bold;
+    color: white;
+    padding: 0.2rem 0.4rem;
+    margin-inline-end: 0.2rem;
+    @include radius;
+  }
+  .report-text{
+    font-style: italic;
+    &::before, &::after{
+      content: '"'
+    }
   }
 </style>
