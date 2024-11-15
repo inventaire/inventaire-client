57fdb9ebc1ebb6adeb47c8a3545669262f68f329
jums <jums@inventaire.io>
2024-11-15 10:08:48 +0100

    split ListingsLayout from EntityListingsLayout

--- app/modules/listings/components/entity_listings_layout.svelte
+++ app/modules/listings/components/entity_listings_layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import Spinner from '#general/components/spinner.svelte'
   import { getListingsByEntityUri } from '#listings/lib/listings'
-  import ListingsLayout from '#modules/listings/components/listings_layout.svelte'
+  import EntityListingComment from '#modules/listings/components/entity_listing_comment.svelte'
   import { i18n, I18n } from '#user/lib/i18n'
 
   export let entity
@@ -23,16 +23,24 @@
   $: emptyListings = listings.length === 0
 </script>
 
-{#if !noListingsFound}
-  <div class="listings-layout">
-    <h5>{i18n('Lists containing this work')}</h5>
-    {#await waitingForListings}
-      <p class="loading">{I18n('loading')}<Spinner /></p>
-    {:then}
-      <ListingsLayout {listings} {uri} />
-    {/await}
-  </div>
-{/if}
+<div class="listings-layout">
+  <h5>{i18n('Lists containing this work')}</h5>
+  {#await waitingForListings}
+    <p class="loading">{I18n('loading')}<Spinner /></p>
+  {:then}
+    {#if noListingsFound}
+      <div class="empty">
+        {i18n('There is nothing here')}
+      </div>
+    {:else}
+      <ul class="listings-layout">
+        {#each listings as listing (listing._id)}
+          <EntityListingComment {listing} {uri} />
+        {/each}
+      </ul>
+    {/if}
+  {/await}
+</div>
 
 <style lang="scss">
   @import "#general/scss/utils";
@@ -40,7 +48,9 @@
     @include display-flex(column, center);
     background-color: $off-white;
     padding: 1em 0.5em;
-    margin: 1em 0;
+    margin: auto;
+    inline-size: 100%;
+    max-width: 50em;
   }
   h5{
     @include sans-serif;
@@ -48,5 +58,9 @@
   }
   .loading{
     @include display-flex(column, center);
+  }
+  .empty{
+    color: $grey;
+    text-align: center;
   }
 </style>
