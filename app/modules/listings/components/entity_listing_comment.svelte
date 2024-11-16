6d79b349189208522cdcd650e8f886ec42421301
jums <jums@inventaire.io>
2024-11-16 07:31:08 +0100

    add links and make it beautiful

--- app/modules/listings/components/entity_listing_comment.svelte
+++ app/modules/listings/components/entity_listing_comment.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import app from '#app/app'
+  import { loadInternalLink } from '#app/lib/utils'
   import ImagesCollage from '#components/images_collage.svelte'
   import { getElementsImages } from '#listings/lib/listings'
   import { i18n } from '#user/lib/i18n'
@@ -8,7 +9,7 @@
 
   const { name, creator, elements } = listing
 
-  const { comment } = elements?.find(el => el.uri === uri)
+  const { comment, _id: elementId } = elements?.find(el => el.uri === uri)
 
   let username
   const imagesLimit = 6
@@ -22,10 +23,15 @@
 </script>
 
 {#if comment}
-  <div class="entity-listing-comment-layout">
-    <div class="first-row">
+  <a
+    href={`/lists/${listing._id}/element/${elementId}`}
+    class="entity-listing-comment-layout"
+    on:click={loadInternalLink}
+  >
+    <div class="comment-row">
       <div class="element-comment">
-        {comment}
+        {comment.slice(0, 150)}
+        {#if comment.length > 150}…{/if}
       </div>
       <div class="creator-info" aria-label={i18n('list_created_by', { username })}>
         {#await waitingForUserdata then}
@@ -34,9 +40,16 @@
       </div>
     </div>
 
-    <div class="last-row">
-      <div class="listing-name">
-        {name}
+    <a
+      href={`/lists/${listing._id}`}
+      class="listing-row"
+      on:click|stopPropagation={loadInternalLink}
+    >
+      <div class="listing-name-section">
+        <div class="listing-name-label">
+          {i18n('In list:')}
+        </div>
+        <div>{name}</div>
       </div>
       {#await waitingForImages then imagesUrls}
         <ImagesCollage
@@ -45,8 +58,8 @@
           imageSize={128}
         />
       {/await}
-    </div>
-  </div>
+    </a>
+  </a>
 {/if}
 
 <style lang="scss">
@@ -55,13 +68,46 @@
     @include display-flex(column);
     width: 100%;
     overflow-y: auto;
+    padding: 1em;
+    background-color: white;
+    &:not(:last-child){
+      margin-block-end: 1em;
+    }
+    &:hover, &:focus{
+      background-color: darken(white, 1%);
+    }
   }
-  .first-row{
-    @include display-flex(row, center, space-between);
+  .element-comment{
+    font-size: 1.1em;
+    padding-inline-end: 1em;
   }
-  .last-row{
+  .comment-row{
+    @include display-flex(column);
+    padding-block-end: 0.5em;
+  }
+  .creator-info{
+    align-self: end;
+  }
+  .listing-name-section{
+    padding-block-end: 0.5em;
+    padding-inline-end: 0.5em;
+    flex: 3;
+  }
+  .listing-name-label{
+    opacity: 0.6;
+    padding-block-start: 0.5em;
+  }
+  .listing-row{
+    @include display-flex(row, center);
+    padding-inline-start: 1em;
+    background-color: $light-grey;
     :global(.images-collage){
-      block-size: 7em;
+      flex: 2;
+      block-size: 5em;
+    }
+    &:hover, &:focus{
+      opacity: 0.8;
+      @include transition(opacity);
     }
   }
 </style>
--- app/modules/listings/components/entity_listings_layout.svelte
+++ app/modules/listings/components/entity_listings_layout.svelte
@@ -5,35 +5,30 @@
   import { i18n, I18n } from '#user/lib/i18n'
 
   export let entity
-  export let emptyListings = false
 
   let listings = []
-  let noListingsFound
   const { uri } = entity
 
   const getListings = async () => {
     if (uri) {
       listings = await getListingsByEntityUri(uri) || []
     }
-    noListingsFound = (listings.length === 0)
   }
 
   const waitingForListings = getListings()
-
-  $: emptyListings = listings.length === 0
 </script>
 
-<div class="listings-layout">
-  <h5>{i18n('Lists containing this work')}</h5>
-  {#await waitingForListings}
-    <p class="loading">{I18n('loading')}<Spinner /></p>
-  {:then}
-    {#if noListingsFound}
+{#await waitingForListings}
+  <p class="loading">{I18n('loading')}<Spinner /></p>
+{:then}
+  <div class="listings-layout listings-comments-layout">
+    <h5>{i18n('Comments from lists containing this work')}</h5>
+    {#if listingsWithEntityComment.length === 0}
       <div class="empty">
         {i18n('There is nothing here')}
       </div>
     {:else}
-      <ul class="listings-layout">
+      <ul class="listings-list">
         {#each listings as listing (listing._id)}
           <EntityListingComment {listing} {uri} />
         {/each}
@@ -44,17 +39,20 @@
 
 <style lang="scss">
   @import "#general/scss/utils";
+  .listings-list{
+    width: 100%;
+  }
   .listings-layout{
     @include display-flex(column, center);
     background-color: $off-white;
-    padding: 1em 0.5em;
+    padding: 0.5em;
     margin: auto;
     inline-size: 100%;
-    max-width: 50em;
+    max-width: 40em;
   }
   h5{
     @include sans-serif;
-    margin-block-end: 0.5em;
+    margin-block-end: 0;
   }
   .loading{
     @include display-flex(column, center);
