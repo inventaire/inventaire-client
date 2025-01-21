b615c1a344f3ab644088a7b806b76ffc9ac2a766
maxlath <k@maxlath.eu>
2025-01-21 18:40:50 +0100

    img api: request entities images on remote entities origin

--- app/api/img.ts
+++ app/api/img.ts
@@ -1,10 +1,14 @@
+import { config } from '#app/config'
 import { isNonEmptyString, isEntityUri, isAssetImg, isLocalImg, isImageHash } from '#app/lib/boolean_tests'
 import { buildPath } from '#app/lib/location'
 import { fixedEncodeURIComponent, hashCode } from '#app/lib/utils'
 import { thumbnail } from '#app/lib/wikimedia/commons'
-import type { RelativeUrl, Url } from '#server/types/common'
+import type { Url } from '#server/types/common'
 import type { EntityUri } from '#server/types/entity'
 import type { ImagePath } from '#server/types/image'
+
+const { remoteEntitiesOrigin } = config
+const entitiesImagesOrigin = remoteEntitiesOrigin || ''
 
 // Keep in sync with server/lib/emails/app_api
 export default function (path: ImagePath | Url | EntityUri, width = 1600, height = 1600) {
@@ -13,7 +17,8 @@ export default function (path: ImagePath | Url | EntityUri, width = 1600, height
   // Converting image hashes to a full URL
   if (isLocalImg(path) || isAssetImg(path)) {
     const [ container, filename ] = path.split('/').slice(2)
-    return `/img/${container}/${width}x${height}/${filename}` as RelativeUrl
+    const origin = container === 'entities' ? entitiesImagesOrigin : ''
+    return `${origin}/img/${container}/${width}x${height}/${filename}` as Url
   }
 
   // The server may return images path on upload.wikimedia.org
@@ -25,12 +30,13 @@ export default function (path: ImagePath | Url | EntityUri, width = 1600, height
   if (path.startsWith('http')) {
     const key = hashCode(path)
     const href = fixedEncodeURIComponent(path)
-    return `/img/remote/${width}x${height}/${key}?href=${href}` as RelativeUrl
+    return `${origin}/img/remote/${width}x${height}/${key}?href=${href}` as Url
   }
 
   // Current use-case: app/modules/search/lib/search_results.ts subjects
   if (isEntityUri(path)) {
-    return buildPath('/api/entities', {
+    const url = `${entitiesImagesOrigin}/api/entities` as Url
+    return buildPath(url, {
       action: 'images',
       uris: path,
       redirect: true,
