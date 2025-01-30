6355ddbed4ad4e7f474d7e82c435331a775232e3
maxlath <k@maxlath.eu>
2025-01-30 19:41:40 +0100

    config: add types

--- app/config.ts
+++ app/config.ts
@@ -1,7 +1,8 @@
 import { getEndpointBase } from '#app/api/endpoint'
 import preq from '#app/lib/preq'
+import type { ClientConfig } from '#server/controllers/config'
 
 // Do not use API.config to avoid a circular dependency
 const configEndpoint = getEndpointBase('config')
 
-export const config = await preq.get(configEndpoint)
+export const config: ClientConfig = await preq.get(configEndpoint)
