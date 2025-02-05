3e8416c7c8fdd23d5577b7c6f600bed18b653b1d
maxlath <k@maxlath.eu>
2025-02-05 20:50:18 +0100

    serie_cleanup: recover editions and their work switcher

--- app/modules/entities/components/cleanup/lib/serie_cleanup_helpers.ts
+++ app/modules/entities/components/cleanup/lib/serie_cleanup_helpers.ts
@@ -1,6 +1,9 @@
 import { pluck, range } from 'underscore'
+import { newError } from '#app/lib/error'
 import type { SeriePartPlaceholder } from '#app/modules/entities/views/cleanup/lib/fill_gaps'
 import { type SerializedEntity } from '#entities/lib/entities'
+
+export type WorkWithEditions = SerializedEntity & { editions?: SerializedEntity[] }
 
 export function getPossibleOrdinals (worksWithOrdinal: (SerializedEntity | SeriePartPlaceholder)[]) {
   const nonPlaceholdersWorksWithOrdinals = worksWithOrdinal.filter(workIsNotPlaceholder)
@@ -16,4 +19,12 @@ export function workIsPlaceholder (work: SerializedEntity | SeriePartPlaceholder
 
 export function workIsNotPlaceholder (work: SerializedEntity | SeriePartPlaceholder): work is SerializedEntity {
   return !workIsPlaceholder(work)
+}
+
+export function assertWorkIsNotPlaceholder (work: SerializedEntity | SeriePartPlaceholder): asserts work is SerializedEntity {
+  if (workIsNotPlaceholder(work)) throw newError('expected an existing work')
+}
+
+export function sortByLabel (a: SerializedEntity, b: SerializedEntity) {
+  return a.label > b.label ? 1 : -1
 }
