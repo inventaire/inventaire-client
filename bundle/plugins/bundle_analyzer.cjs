4166afe238c9d2087835d3acf8589cf9ebd47f45
maxlath <k@maxlath.eu>
2025-02-10 19:12:34 +0100

    bundle stats: disable json stats
    as we don't have a use for it presently, and those files take a lot of space

--- bundle/plugins/bundle_analyzer.cjs
+++ bundle/plugins/bundle_analyzer.cjs
@@ -3,7 +3,6 @@ const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 module.exports = new BundleAnalyzerPlugin({
   analyzerMode: 'static',
   reportFilename: 'bundle_report.html',
-  generateStatsFile: true,
-  statsFilename: 'bundle_stats.json',
+  generateStatsFile: false,
   openAnalyzer: false,
 })
