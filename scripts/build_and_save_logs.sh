29938cd833b263b5ca5d0abadc40125dbcdc0003
maxlath <k@maxlath.eu>
2025-02-26 17:15:16 +0100

    scripts: build: force exit with 0 code

--- scripts/build_and_save_logs.sh
+++ scripts/build_and_save_logs.sh
@@ -3,4 +3,10 @@
 # as it triggers "sh: 1: set: Illegal option -o pipefail"
 # which is even worse that what is described here https://github.com/npm/npm/issues/18517
 set -o pipefail
+
+echo -e "\e[0;32mbuild and save logs\e[0m"
 ./scripts/build | tee ./logs/build.log
+echo -e "\e[0;32mbuild and save logs: done\e[0m"
+
+# Exit with code 0 (apparently required for Docker image build to succeed)
+true
