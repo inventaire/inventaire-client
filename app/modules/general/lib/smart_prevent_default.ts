2a4c35cf069d10bc05ecaaf272c302f90c0f5161
maxlath <k@maxlath.eu>
2025-03-28 12:29:34 +0100

    app layout: recover smart_prevent_default behavior

--- app/modules/general/lib/smart_prevent_default.ts
+++ app/modules/general/lib/smart_prevent_default.ts
@@ -1,39 +1,30 @@
 import { isOpenedOutside } from '#app/lib/utils'
-// Prevent default click event behavior selectively
 
-export default function (e) {
+const { origin } = location
+
+// Prevent default click event behavior selectively
+export function preventAnchorDefault (e) {
   // largely inspired by
   // https://github.com/jmeas/backbone.intercept/blob/master/src/backbone.intercept.js
 
   // Only intercept left-clicks
-  let href
   if (e.which !== 1) return
 
-  // Don't intercept if ctrlKey is pressed
-  // it should open the targeted anchor href in a new tab/window
-  // Prevent the normal handler to also fire:
-  // `unless isOpenedOutside(e) then handler()
-  // Ignore missing href though, as this behavior is applied to some anchors
-  // purposedly without href. Ex: visibility dropdown menus
-  if (isOpenedOutside(e, true)) return
+  const anchorEl = e.target.closest('a')
+  if (!anchorEl) return
 
-  // If there is no href on current target, then the default
-  // behaviour is to do nothing.
-  // Therefore, there is no reason to preventDefault.
-  if (e.currentTarget != null) {
-    const $link = $(e.currentTarget)
-    // Get the href; stop processing if there isn't one
-    href = $link.attr('href')
-    if (!href) return
-  }
+  const { href } = anchorEl
+  if (!href) return
+
+  if (isOpenedOutside(e, true)) return
 
   // Return if the URL is absolute (thus with ://)
   // or if the protocol is mailto or javascript
-  if (/^#|javascript:|mailto:|(?:\w+:)?\/\//.test(href)) return
+  if (/^#|javascript:|mailto:|(?:\w+:)?\/\//.test(href) && !href.startsWith(origin)) return
 
   // Return if the URL is an API call
   // Ex: Wikidata Oauth, data export, etc.
-  if (/^\/api\//.test(href)) return
+  if (href.startsWith(`${origin}/api/`)) return
 
   // If we haven't been stopped yet, then we prevent the default action
   e.preventDefault()
