a277498af481580e23152df82f542ab265054dab
maxlath <k@maxlath.eu>
2025-03-29 11:09:26 +0100

    document_headers: integrate other <head> tags

--- app/modules/general/components/document_headers.svelte
+++ app/modules/general/components/document_headers.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
+  import { config } from '#app/config'
   import { getTextDirection, langs, regionify } from '#app/lib/active_languages'
   import { setQuerystring } from '#app/lib/location'
   import { locationStore } from '#app/lib/location_store'
+  import { metadataStore, prerenderStatusStore } from '#app/lib/metadata/update'
   import { onChange } from '#app/lib/svelte/svelte'
   import { objectEntries } from '#app/lib/utils'
   import { localLang } from '#modules/user/lib/i18n'
   import type { AbsoluteUrl } from '#server/types/common'
 
   const { origin } = location
+  const { instanceName } = config
 
   function updateDocumentLang () {
     if (typeof $localLang === 'string') {
@@ -18,9 +21,22 @@
 
   $: onChange($localLang, updateDocumentLang)
   $: baseHref = `${origin}/${$locationStore.route}` as AbsoluteUrl
+
+  let url, title, description, image, rss, twitterCard
+  $: {
+    if ($metadataStore) {
+      ;({ url, title, description, image, rss, 'twitter:card': twitterCard } = $metadataStore)
+    }
+  }
+
+  let statusCode, header
+  $: ({ statusCode, header } = $prerenderStatusStore)
 </script>
 
 <svelte:head>
+  <title>{title}</title>
+  <link rel="canonical" href={url} />
+
   <!-- The default lang - en - doesnt need a lang querystring to be set.
   It could have one, but search engines need to know that the default url
   they got matches this languages hreflang -->
@@ -40,4 +56,26 @@
       <meta property="og:locale:alternate" content={territory} />
     {/if}
   {/each}
+
+  <link rel="alternate" type="application/rss+xml" href={rss} />
+
+  <meta name="twitter:title" content={title} />
+  <meta name="twitter:card" content={twitterCard} />
+  <meta name="twitter:description" content={description} />
+  <meta name="twitter:image" content={image} />
+
+  <meta property="og:site_name" content={instanceName} />
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content={url} />
+  <meta property="og:title" content={title} />
+  <meta name="description" property="og:description" content={description} />
+  <!-- limiting to one og:image to be sure it's the one selected. cf https://stackoverflow.com/questions/13424780/facebook-multiple-ogimage-tags-which-is-default -->
+  <meta property="og:image" content={image} />
+
+  {#if statusCode}
+    <meta name="prerender-status-code" content={statusCode} />
+    {#if header}
+      <meta name="prerender-header" content={header} />
+    {/if}
+  {/if}
 </svelte:head>
