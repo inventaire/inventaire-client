7e31c157c6a8a6b9a7d596787749f3636ebeac6e
maxlath <k@maxlath.eu>
2025-04-01 17:20:49 +0200

    paginated items: improve types

--- app/modules/users/components/paginated_section_items.svelte
+++ app/modules/users/components/paginated_section_items.svelte
@@ -2,11 +2,13 @@
   import { screen } from '#app/lib/components/stores/screen'
   import PaginatedItems from '#inventory/components/paginated_items.svelte'
   import { getPaginationParameters } from '#inventory/lib/paginated_items'
+  import type { ItemsPageRequestFn } from '#inventory/lib/queries'
 
-  export let sectionRequestName, showDistance = false
+  export let sectionRequest: ItemsPageRequestFn
+  export let showDistance = false
 
   const pagination = getPaginationParameters({
-    requestName: sectionRequestName,
+    request: sectionRequest,
     limit: 20,
     allowMore: true,
   })
