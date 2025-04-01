68c11d0c29f6284dc44ae534f7164874a4961545
maxlath <k@maxlath.eu>
2025-04-01 22:34:53 +0200

    router: use a routeToRegExp implementation closer to backbone
    as some routes were not found with the routerrouter implementation

--- app/lib/router.ts
+++ app/lib/router.ts
@@ -7,7 +7,7 @@ export type RouteAction = (...args: string[]) => void | Promise<void>
 export type RouteControllers = Record<string, RouteAction>
 
 export function addRoutes (newRouteControllerNames: RouteControllerNames, controllers: RouteControllers) {
-  for (const [ route, controllerName ] of objectEntries(newRouteControllerNames)) {
+  for (const [ route, controllerName ] of objectEntries(newRouteControllerNames).reverse()) {
     const action = controllers[controllerName]
     setRoute(route, action)
   }
@@ -22,39 +22,38 @@ function setRoute (route: string | RegExp, action: RouteAction) {
   handlers.push([ route, action ])
 }
 
-// Given a RegExp'ed `route` and a `path`, return the array of extracted
-// and decoded parameters. Empty or unmatched parameters will be treated
-// as `null` to normalize cross-browser behavior.
 function extractParameters (routeRegex: RegExp, pathname: string) {
   return routeRegex.exec(pathname).slice(1).map(parameter => {
     return parameter ? decodeURIComponent(parameter) : null
   })
 }
 
-// Convert a route string into a regular expression suitable for matching
-// against the current location's `pathname`.
+// Adapted from https://github.com/jashkenas/backbone/blob/8f0285f/backbone.js#L1755-L1762
 function routeToRegExp (route: string) {
   route = route
-    // escape RegExp reserved characters
-    .replaceAll(/[$.|]+/g, '\\$&')
-    // replace optional parameters with RegExp
-    .replaceAll(/\((.+?)\)/g, '(?:$1)?')
-    // replace named parameters with RegExp
-    .replaceAll(/:\w+/g, '([^/]+)')
-    // replace wildcard parameters with RegExp
-    .replaceAll(/\*(\w+)?/g, '(.+?)')
-
-  return new RegExp(`^${route}$`)
+    // Escape
+    .replace(/[-{}[\]+?.,\\^$|#\s]/g, '\\$&')
+    // Optional  parameter
+    .replace(/\((.*?)\)/g, '(?:$1)?')
+    // Named parameter
+    .replace(/(\(\?)?:\w+/g, (match, optional) => optional ? match : '([^/?]+)')
+    // Splat parameter
+    .replace(/\*\w+/g, '([^?]*?)')
+  return new RegExp('^' + route + '(?:\\?([\\s\\S]*))?$')
 }
 
 export function loadUrl (pathname: string) {
   pathname = decodeURIComponent(pathname)
   for (const [ routeRegex, action ] of handlers) {
-    if (routeRegex.test(pathname)) return action(...extractParameters(routeRegex, pathname))
+    if (routeRegex.test(pathname)) {
+      return action(...extractParameters(routeRegex, pathname))
+    }
   }
   throw new Error('route not found')
 }
 
 export function startRouter () {
+  // Reverse to let loadUrl iterate handlers by priority order
+  handlers.reverse()
   loadUrl(location.pathname)
 }
