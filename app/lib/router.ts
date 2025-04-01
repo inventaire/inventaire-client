ef81d4b89058c9008fb5a5a4585b8863937564c3
maxlath <k@maxlath.eu>
2025-04-01 14:37:43 +0200

    router: add loadUrl function
    This required to internalize and adapt @jgarber/routerrouter to have access to the handlers

--- app/lib/router.ts
+++ app/lib/router.ts
@@ -1,19 +1,60 @@
-import RouterRouter from '@jgarber/routerrouter'
 import { objectEntries } from './utils'
 
-export type RouteControllerNames = Record<string, string>
-export type RouteControllers = Record<string, ((paramA?: string, paramB?: string) => void | Promise<void>)>
+// Router logic borrowing from Backbone.Router and @jgarber/routerrouter
 
-const routes: RouteControllers = {}
+export type RouteControllerNames = Record<string, string>
+export type RouteAction = (...args: string[]) => void | Promise<void>
+export type RouteControllers = Record<string, RouteAction>
 
 export function addRoutes (newRouteControllerNames: RouteControllerNames, controllers: RouteControllers) {
-  const newRoutes = {}
   for (const [ route, controllerName ] of objectEntries(newRouteControllerNames)) {
-    newRoutes[route] = controllers[controllerName]
+    const action = controllers[controllerName]
+    setRoute(route, action)
   }
-  Object.assign(routes, newRoutes)
 }
 
-export function start () {
-  new RouterRouter(routes)
+const handlers: [ RegExp, RouteAction ][] = []
+
+function setRoute (route: string | RegExp, action: RouteAction) {
+  if (!(route instanceof RegExp)) {
+    route = routeToRegExp(route)
+  }
+  handlers.push([ route, action ])
+}
+
+// Given a RegExp'ed `route` and a `path`, return the array of extracted
+// and decoded parameters. Empty or unmatched parameters will be treated
+// as `null` to normalize cross-browser behavior.
+function extractParameters (routeRegex: RegExp, pathname: string) {
+  return routeRegex.exec(pathname).slice(1).map(parameter => {
+    return parameter ? decodeURIComponent(parameter) : null
+  })
+}
+
+// Convert a route string into a regular expression suitable for matching
+// against the current location's `pathname`.
+function routeToRegExp (route: string) {
+  route = route
+    // escape RegExp reserved characters
+    .replaceAll(/[$.|]+/g, '\\$&')
+    // replace optional parameters with RegExp
+    .replaceAll(/\((.+?)\)/g, '(?:$1)?')
+    // replace named parameters with RegExp
+    .replaceAll(/:\w+/g, '([^/]+)')
+    // replace wildcard parameters with RegExp
+    .replaceAll(/\*(\w+)?/g, '(.+?)')
+
+  return new RegExp(`^${route}$`)
+}
+
+export function loadUrl (pathname: string) {
+  pathname = decodeURIComponent(pathname)
+  for (const [ routeRegex, action ] of handlers) {
+    if (routeRegex.test(pathname)) return action(...extractParameters(routeRegex, pathname))
+  }
+  throw new Error('route not found')
+}
+
+export function startRouter () {
+  loadUrl(location.pathname)
 }
